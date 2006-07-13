7457026a184011809dbfaf39fd45983784fda20d
lloyd <lloyd@randombit.net>
2006-07-13 14:35:57 +0000

    Version bump

--- misc/config/mkconfig.pl
+++ misc/config/mkconfig.pl
@@ -11,7 +11,7 @@ my $PROJECT_NAME = 'Botan';
 # Version numbers
 my $MAJOR = 1;
 my $MINOR = 5;
-my $PATCH = 9;
+my $PATCH = 10;
 
 #############################################################################
 # Configuration options
