25e64ec2aad7824fb1918d7722b1628386498a07
lloyd <lloyd@randombit.net>
2006-08-13 15:24:29 +0000

    Add an x86 assembly MD5 implementation; works, but needs optimization

---
+++ modules/alg_ia32/md5.cpp
@@ -0,0 +1,43 @@
+/*************************************************
+* MD5 Source File                                *
+* (C) 1999-2006 The Botan Project                *
+*************************************************/
+
+#include <botan/md5.h>
+#include <botan/bit_ops.h>
+
+namespace Botan {
+
+extern "C" void md5_core(u32bit[4], const byte[64], u32bit[16]);
+
+/*************************************************
+* MD5 Compression Function                       *
+*************************************************/
+void MD5::hash(const byte input[])
+   {
+   md5_core(digest, input, M);
+   }
+
+/*************************************************
+* Copy out the digest                            *
+*************************************************/
+void MD5::copy_out(byte output[])
+   {
+   for(u32bit j = 0; j != OUTPUT_LENGTH; ++j)
+      output[j] = get_byte(3 - (j % 4), digest[j/4]);
+   }
+
+/*************************************************
+* Clear memory of sensitive data                 *
+*************************************************/
+void MD5::clear() throw()
+   {
+   MDx_HashFunction::clear();
+   M.clear();
+   digest[0] = 0x67452301;
+   digest[1] = 0xEFCDAB89;
+   digest[2] = 0x98BADCFE;
+   digest[3] = 0x10325476;
+   }
+
+}
