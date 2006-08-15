9878bca6ac9ea94c3cba87a06144e93e12fd933b
lloyd <lloyd@randombit.net>
2006-08-15 11:36:16 +0000

    Remove unused variable
    Collect the external functions into a single extern "C" block

--- modules/alg_ia32/serpent.cpp
+++ modules/alg_ia32/serpent.cpp
@@ -8,9 +8,13 @@
 
 namespace Botan {
 
-extern "C" void serpent_encrypt(const byte[16], byte[16], const u32bit[132]);
-extern "C" void serpent_decrypt(const byte[16], byte[16], const u32bit[132]);
-extern "C" void serpent_key_schedule(u32bit[140]);
+extern "C" {
+
+void serpent_encrypt(const byte[16], byte[16], const u32bit[132]);
+void serpent_decrypt(const byte[16], byte[16], const u32bit[132]);
+void serpent_key_schedule(u32bit[140]);
+
+}
 
 /*************************************************
 * Serpent Encryption                             *
@@ -33,8 +37,6 @@ void Serpent::dec(const byte in[], byte out[]) const
 *************************************************/
 void Serpent::key(const byte key[], u32bit length)
    {
-   const u32bit PHI = 0x9E3779B9;
-
    SecureBuffer<u32bit, 140> W;
    for(u32bit j = 0; j != length / 4; ++j)
       W[j] = make_u32bit(key[4*j+3], key[4*j+2], key[4*j+1], key[4*j]);
