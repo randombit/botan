b12eda23a0ed5c516dda166b990975fa53c8ff8c
lloyd <lloyd@randombit.net>
2006-08-23 09:49:07 +0000

    Clean up and finish off the Python version of the NIST X.509 test suite. Expected results are read from a file (results.txt), and pass/fails are reported. Also change validate to return the result normally, rather than throwing an exception; while it did save some code in validate() itself, with the addition of all the code to support it, it was a bit longer, as well as being somewhat hard to follow.

--- misc/python/nisttest.py
+++ misc/python/nisttest.py
@@ -3,43 +3,59 @@
 import sys, os, botan
 from os.path import join;
 
-class TestResult(Exception):
-    def __init__(self, r):
-        self.result = r
-    def __str__(self):
-        return repr(self.result).replace('botan._botan.verify_result.', '')
-
-def raise_unless_ok(r):
-    if r != botan.verify_result.verified:
-        raise TestResult(r)
-
 def validate(ca_certs, certs, crls, ee_certs):
     store = botan.X509_Store()
-
     for cert in certs:
         if cert not in ee_certs:
             store.add_cert(botan.X509_Certificate(cert), cert in ca_certs)
 
     for crl in crls:
-        raise_unless_ok(store.add_crl(botan.X509_CRL(crl)))
+        r = store.add_crl(botan.X509_CRL(crl))
+        if r != botan.verify_result.verified:
+            return r
 
     for ee in ee_certs:
-        raise_unless_ok(store.validate(botan.X509_Certificate(ee)))
+        r = store.validate(botan.X509_Certificate(ee))
+        if r != botan.verify_result.verified:
+            return r
 
-    raise TestResult(botan.verify_result.verified)
+    return botan.verify_result.verified
+
+def run_test(files, rootdir, testname, expected):
+    crls = [join(rootdir,x) for x in files if x.endswith(".crl")]
+    certs = [join(rootdir,x) for x in files if x.endswith(".crt")]
+    end_entity = [x for x in certs if x.find("End Cert") != -1]
+    ca_certs = [x for x in certs if x.find("Trust Anchor") != -1]
+
+    print "Running", testname, "...",
+
+    result = validate(ca_certs, certs, crls, end_entity)
+    result = repr(result).replace('botan._botan.verify_result.', '')
+    
+    if result != expected:
+        print "FAILED: got", result, "expected", expected
+    else:
+        print "passed"
 
 def main():
+    def load_results(file):
+        results = {}
+        for line in open(file, 'r'):
+            line = line[0:line.find('#')].strip()
+            if line:
+                test,result = line.split(' ')
+                results[test] = result
+        return results
+
+    results = load_results('results.txt')
+
     for root, dirs, files in os.walk('../nist_tests/tests'):
         if files:
-            crls = [join(root,x) for x in files if x.endswith(".crl")]
-            certs = [join(root,x) for x in files if x.endswith(".crt")]
-            end_entity = [x for x in certs if x.find("End Cert") != -1]
-            ca_certs = [x for x in certs if x.find("Trust Anchor") != -1]
-
-            try:
-                validate(ca_certs, certs, crls, end_entity)
-            except TestResult, result:
-                print result
+            thistest = root[root.rfind('/')+1:]
+            if thistest in results:
+                run_test(files, root, thistest, results[thistest])
+            else:
+                print "Skipping", thistest, "- no expected result set"
         
 if __name__ == "__main__":
     sys.exit(main())
