74a8b86758a4a0b1c8a6471a13ebcf94fe24343e
lloyd <lloyd@randombit.net>
2007-03-08 00:28:01 +0000

    Move the version query code out of base.cpp and into version.cpp; I have a feeling I may want to perform automated source changes to the version strings (eg, to insert a monotone revision ID), but I'd just as soon perform such operations on as small a file as possible to limit any damage that might occur due to a source code rewriting script gone awry.

--- src/base.cpp
+++ src/base.cpp
@@ -5,7 +5,6 @@
 
 #include <botan/base.h>
 #include <botan/version.h>
-#include <botan/parsing.h>
 #include <botan/util.h>
 #include <botan/config.h>
 
@@ -242,22 +241,5 @@ u32bit RandomNumberGenerator::add_entropy(EntropySource& source,
 
    return entropy_estimate(buffer, bytes_gathered);
    }
-
-/*************************************************
-* Return the version as a string                 *
-*************************************************/
-std::string version_string()
-   {
-   return "Botan " + to_string(version_major()) + "." +
-                     to_string(version_minor()) + "." +
-                     to_string(version_patch());
-   }
-
-/*************************************************
-* Return parts of the version as integers        *
-*************************************************/
-u32bit version_major() { return BOTAN_VERSION_MAJOR; }
-u32bit version_minor() { return BOTAN_VERSION_MINOR; }
-u32bit version_patch() { return BOTAN_VERSION_PATCH; }
 
 }
---
+++ src/version.cpp
@@ -0,0 +1,28 @@
+/*************************************************
+* Version Information Source File                *
+* (C) 1999-2007 The Botan Project                *
+*************************************************/
+
+#include <botan/version.h>
+#include <botan/parsing.h>
+
+namespace Botan {
+
+/*************************************************
+* Return the version as a string                 *
+*************************************************/
+std::string version_string()
+   {
+   return "Botan " + to_string(version_major()) + "." +
+                     to_string(version_minor()) + "." +
+                     to_string(version_patch());
+   }
+
+/*************************************************
+* Return parts of the version as integers        *
+*************************************************/
+u32bit version_major() { return BOTAN_VERSION_MAJOR; }
+u32bit version_minor() { return BOTAN_VERSION_MINOR; }
+u32bit version_patch() { return BOTAN_VERSION_PATCH; }
+
+}
