68a0f2fad07192a98369226216adee57ce0579b3
lloyd <lloyd@randombit.net>
2007-07-26 13:56:17 +0000

    If counter overflows, truncate the output of KDF2. That effectively limits the output to just a bit under 2^32 bytes, which is the maximum you can request anyway.

--- src/kdf.cpp
+++ src/kdf.cpp
@@ -101,7 +101,7 @@ SecureVector<byte> KDF2::derive(u32bit out_len,
    u32bit counter = 1;
 
    std::auto_ptr<HashFunction> hash(get_hash(hash_name));
-   while(out_len)
+   while(out_len && counter)
       {
       hash->update(secret, secret_len);
       for(u32bit j = 0; j != 4; ++j)
