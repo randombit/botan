ec9c0d09255d237bc6796b4b1f0836c46ea49c6a
lloyd <lloyd@randombit.net>
2008-09-11 18:23:43 +0000

    Handle configuration of what kind of clock to use for benchmarking. Linux defaults to clock_gettime, FreeBSD defaults to gettimeofday, everything else defaults to clock (though it is easy to a timer on with -DUSE_<CLOCK_NAME>=1, eg -DUSE_GETTIMEOFDAY=1 or -DUSE_TIMES=1

--- checks/clock.cpp
+++ checks/clock.cpp
@@ -1,10 +1,37 @@
 #include "timer.h"
 #include <botan/build.h>
 
-#define USE_CLOCK_GETTIME 1
-#define USE_GETTIMEOFDAY 0
-#define USE_TIMES 0
-#define USE_CLOCK 0
+/*************************************************
+* Platform specific settings for which clock type to use
+*/
+
+#if !defined(USE_CLOCK_GETTIME) && defined(BOTAN_TARGET_OS_IS_LINUX)
+  #define USE_CLOCK_GETTIME 1
+#endif
+
+#if !defined(USE_GETTIMEOFDAY) && defined(BOTAN_TARGET_OS_IS_FREEBSD)
+  #define USE_GETTIMEOFDAY 1
+#endif
+
+/*************************************************
+  Set the default, which is clock
+*/
+#ifndef USE_CLOCK_GETTIME
+  #define USE_CLOCK_GETTIME 0
+#endif
+
+#ifndef USE_GETTIMEOFDAY
+  #define USE_GETTIMEOFDAY 0
+#endif
+
+#ifndef USE_TIMES
+  #define USE_TIMES 0
+#endif
+
+/* the default: ANSI/ISO clock */
+#ifndef USE_CLOCKeemacs
+  #define USE_CLOCK 1
+#endif
 
 #if USE_CLOCK_GETTIME
   #include <time.h>
@@ -17,18 +44,23 @@
   #include <time.h>
 #endif
 
+/* The implementation: */
 u64bit Timer::get_clock()
    {
    static const u64bit billion = 1000000000;
 
 #if USE_CLOCK_GETTIME
+
    struct timespec tv;
    clock_gettime(CLOCK_REALTIME, &tv);
    return (billion * tv.tv_sec + tv.tv_nsec);
+
 #elif USE_GETTIMEOFDAY
+
    struct timeval tv;
    gettimeofday(&tv, 0);
    return (billion * tv.tv_sec + 1000 * tv.tv_usec);
+
 #elif USE_TIMES
 
    struct tms tms;
@@ -38,10 +70,13 @@ u64bit Timer::get_clock()
       (billion / sysconf(_SC_CLK_TCK));
 
    return (tms.tms_utime * clocks_to_nanoseconds);
+
 #elif USE_CLOCK
+
    static const u64bit clocks_to_nanoseconds =
       (billion / CLOCKS_PER_SEC);
 
    return clock() * clocks_to_nanoseconds;
+
 #endif
    }
