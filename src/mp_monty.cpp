ef2e555e450068c50e8bc974a464e6de7b1f2922
lloyd <lloyd@randombit.net>
2008-09-11 18:59:44 +0000

    Fix loop variable naming (i in outermost scope, then j, remove unneeded k)

--- src/mp_monty.cpp
+++ src/mp_monty.cpp
@@ -20,40 +20,40 @@ void bigint_monty_redc(word z[], u32bit z_size,
    {
    const u32bit blocks_of_8 = x_size - (x_size % 8);
 
-   for(u32bit j = 0; j != x_size; ++j)
+   for(u32bit i = 0; i != x_size; ++i)
       {
-      word* z_j = z + j;
+      word* z_i = z + i;
 
-      const word y = z_j[0] * u;
+      const word y = z_i[0] * u;
 
       word carry = 0;
 
-      for(u32bit i = 0; i != blocks_of_8; i += 8)
-         carry = word8_madd3(z_j + i, x + i, y, carry);
+      for(u32bit j = 0; j != blocks_of_8; j += 8)
+         carry = word8_madd3(z_i + j, x + j, y, carry);
 
-      for(u32bit i = blocks_of_8; i != x_size; ++i)
-         z_j[i] = word_madd3(x[i], y, z_j[i], &carry);
+      for(u32bit j = blocks_of_8; j != x_size; ++j)
+         z_i[j] = word_madd3(x[j], y, z_i[j], &carry);
 
-      word z_sum = z_j[x_size] + carry;
-      carry = (z_sum < z_j[x_size]);
-      z_j[x_size] = z_sum;
+      word z_sum = z_i[x_size] + carry;
+      carry = (z_sum < z_i[x_size]);
+      z_i[x_size] = z_sum;
 
-      for(u32bit k = x_size + 1; carry && k != z_size - j; ++k)
+      for(u32bit j = x_size + 1; carry && j != z_size - i; ++j)
          {
-         ++z_j[k];
-         carry = !z_j[k];
+         ++z_i[j];
+         carry = !z_i[j];
          }
       }
 
    // Check if z[x_size...x_size+1] >= x[0...x_size] using bigint_cmp (inlined)
    if(!z[x_size + x_size])
       {
-      for(u32bit j = x_size; j > 0; --j)
+      for(u32bit i = x_size; i > 0; --i)
          {
-         if(z[x_size + j - 1] > x[j-1])
+         if(z[x_size + i - 1] > x[i-1])
             break;
 
-         if(z[x_size + j - 1] < x[j-1])
+         if(z[x_size + i - 1] < x[i-1])
             return;
          }
       }
@@ -61,11 +61,11 @@ void bigint_monty_redc(word z[], u32bit z_size,
    // If the compare above is true, subtract using bigint_sub2 (inlined)
    word carry = 0;
 
-   for(u32bit j = 0; j != blocks_of_8; j += 8)
-      carry = word8_sub2(z + x_size + j, x + j, carry);
+   for(u32bit i = 0; i != blocks_of_8; i += 8)
+      carry = word8_sub2(z + x_size + i, x + i, carry);
 
-   for(u32bit j = blocks_of_8; j != x_size; ++j)
-      z[x_size + j] = word_sub(z[x_size + j], x[j], &carry);
+   for(u32bit i = blocks_of_8; i != x_size; ++i)
+      z[x_size + i] = word_sub(z[x_size + i], x[i], &carry);
 
    if(carry)
       --z[x_size+x_size];
