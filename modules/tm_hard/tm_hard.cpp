0995e820972bb74ed5488b6206f7ab5b92e127ed
lloyd <lloyd@randombit.net>
2008-09-18 01:12:39 +0000

    Add asm to access high res timer on IA-64, HP-PA, S390x

--- modules/tm_hard/tm_hard.cpp
+++ modules/tm_hard/tm_hard.cpp
@@ -18,14 +18,27 @@ u64bit Hardware_Timer::clock() const
    u32bit rtc_low = 0, rtc_high = 0;
    asm volatile("rdtsc" : "=d" (rtc_high), "=a" (rtc_low));
    rtc = (static_cast<u64bit>(rtc_high) << 32) | rtc_low;
+
 #elif defined(BOTAN_TARGET_ARCH_IS_PPC) || defined(BOTAN_TARGET_ARCH_IS_PPC64)
    u32bit rtc_low = 0, rtc_high = 0;
    asm volatile("mftbu %0; mftb %1" : "=r" (rtc_high), "=r" (rtc_low));
    rtc = (static_cast<u64bit>(rtc_high) << 32) | rtc_low;
+
 #elif defined(BOTAN_TARGET_ARCH_IS_ALPHA)
    asm volatile("rpcc %0" : "=r" (rtc));
+
 #elif defined(BOTAN_TARGET_ARCH_IS_SPARC64)
    asm volatile("rd %%tick, %0" : "=r" (rtc));
+
+#elif defined(BOTAN_TARGET_ARCH_IS_IA64)
+   asm volatile("mov %0=ar.itc" : "=r" (rtc));
+
+#elif defined(BOTAN_TARGET_ARCH_IS_S390X)
+   asm volatile("stck 0(%0)" : : "a" (&rtc) : "memory", "cc");
+
+#elif defined(BOTAN_TARGET_ARCH_IS_HPPA)
+   asm volatile("mfctl 16,%0" : "=r" (rtc)); // 64-bit only?
+
 #else
    #error "Unsure how to access hardware timer on this system"
 #endif
