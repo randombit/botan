b2243c339e9cb15a7af017eae8c6341354802613
lloyd <lloyd@randombit.net>
2008-09-28 21:29:04 +0000

    Add missing PBE header and modinfo files

---
+++ src/pbe/pbes2/pbes2.h
@@ -0,0 +1,45 @@
+/*************************************************
+* PKCS #5 v2.0 PBE Header File                   *
+* (C) 1999-2007 Jack Lloyd                       *
+*************************************************/
+
+#ifndef BOTAN_PBE_PKCS_v20_H__
+#define BOTAN_PBE_PKCS_v20_H__
+
+#include <botan/pbe.h>
+#include <botan/pipe.h>
+#include <botan/enums.h>
+
+namespace Botan {
+
+/*************************************************
+* PKCS#5 v2.0 PBE                                *
+*************************************************/
+class BOTAN_DLL PBE_PKCS5v20 : public PBE
+   {
+   public:
+      void write(const byte[], u32bit);
+      void start_msg();
+      void end_msg();
+      PBE_PKCS5v20(DataSource&);
+      PBE_PKCS5v20(const std::string&, const std::string&);
+   private:
+      void set_key(const std::string&);
+      void new_params(RandomNumberGenerator& rng);
+      MemoryVector<byte> encode_params() const;
+      void decode_params(DataSource&);
+      OID get_oid() const;
+
+      void flush_pipe(bool);
+      bool known_cipher(const std::string&) const;
+
+      const Cipher_Dir direction;
+      std::string digest, cipher, cipher_algo;
+      SecureVector<byte> salt, key, iv;
+      u32bit iterations, key_length;
+      Pipe pipe;
+   };
+
+}
+
+#endif
