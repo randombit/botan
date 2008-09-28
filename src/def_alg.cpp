ae478c48af11cf4e148dddfb4b748d56d311ca67
lloyd <lloyd@randombit.net>
2008-09-28 16:45:45 +0000

    Modularize all ciphers

--- src/def_alg.cpp
+++ src/def_alg.cpp
@@ -7,35 +7,114 @@
 #include <botan/libstate.h>
 #include <botan/parsing.h>
 
-#include <botan/aes.h>
-#include <botan/blowfish.h>
-#include <botan/cast128.h>
-#include <botan/cast256.h>
-#include <botan/des.h>
-#include <botan/gost.h>
-#include <botan/idea.h>
-#include <botan/kasumi.h>
-#include <botan/lion.h>
-#include <botan/lubyrack.h>
-#include <botan/mars.h>
-#include <botan/misty1.h>
-#include <botan/noekeon.h>
-#include <botan/rc2.h>
-#include <botan/rc5.h>
-#include <botan/rc6.h>
-#include <botan/safer_sk.h>
-#include <botan/seed.h>
-#include <botan/serpent.h>
-#include <botan/skipjack.h>
-#include <botan/square.h>
-#include <botan/tea.h>
-#include <botan/twofish.h>
-#include <botan/xtea.h>
+#ifdef BOTAN_HAS_AES
+   #include <botan/aes.h>
+#endif
 
-#include <botan/arc4.h>
-#include <botan/salsa20.h>
-#include <botan/turing.h>
-#include <botan/wid_wake.h>
+#ifdef BOTAN_HAS_BLOWFISH
+   #include <botan/blowfish.h>
+#endif
+
+#ifdef BOTAN_HAS_CAST
+   #include <botan/cast128.h>
+   #include <botan/cast256.h>
+#endif
+
+#ifdef BOTAN_HAS_DES
+   #include <botan/des.h>
+#endif
+
+#ifdef BOTAN_HAS_GOST
+   #include <botan/gost.h>
+#endif
+
+#ifdef BOTAN_HAS_IDEA
+   #include <botan/idea.h>
+#endif
+
+#ifdef BOTAN_HAS_KASUMI
+   #include <botan/kasumi.h>
+#endif
+
+#ifdef BOTAN_HAS_LION
+   #include <botan/lion.h>
+#endif
+
+#ifdef BOTAN_HAS_LUBYRACK
+   #include <botan/lubyrack.h>
+#endif
+
+#ifdef BOTAN_HAS_MARS
+   #include <botan/mars.h>
+#endif
+
+#ifdef BOTAN_HAS_MISTY1
+   #include <botan/misty1.h>
+#endif
+
+#ifdef BOTAN_HAS_NOEKEON
+   #include <botan/noekeon.h>
+#endif
+
+#ifdef BOTAN_HAS_RC2
+   #include <botan/rc2.h>
+#endif
+
+#ifdef BOTAN_HAS_RC5
+   #include <botan/rc5.h>
+#endif
+
+#ifdef BOTAN_HAS_RC6
+   #include <botan/rc6.h>
+#endif
+
+#ifdef BOTAN_HAS_SAFER
+   #include <botan/safer_sk.h>
+#endif
+
+#ifdef BOTAN_HAS_SEED
+   #include <botan/seed.h>
+#endif
+
+#ifdef BOTAN_HAS_SERPENT
+   #include <botan/serpent.h>
+#endif
+
+#ifdef BOTAN_HAS_SKIPJACK
+   #include <botan/skipjack.h>
+#endif
+
+#ifdef BOTAN_HAS_SQUARE
+   #include <botan/square.h>
+#endif
+
+#ifdef BOTAN_HAS_TEA
+   #include <botan/tea.h>
+#endif
+
+#ifdef BOTAN_HAS_TWOFISH
+   #include <botan/twofish.h>
+#endif
+
+#ifdef BOTAN_HAS_XTEA
+   #include <botan/xtea.h>
+#endif
+
+#ifdef BOTAN_HAS_ARC4
+   #include <botan/arc4.h>
+#endif
+
+#ifdef BOTAN_HAS_SALSA20
+   #include <botan/salsa20.h>
+#endif
+
+#ifdef BOTAN_HAS_TURING
+   #include <botan/turing.h>
+#endif
+
+#ifdef BOTAN_HAS_WID_WAKE
+   #include <botan/wid_wake.h>
+#endif
 
 #ifdef BOTAN_HAS_ADLER32
   #include <botan/adler32.h>
@@ -121,47 +200,114 @@ Default_Engine::find_block_cipher(const std::string& algo_spec) const
       return 0;
    const std::string algo_name = global_state().deref_alias(name[0]);
 
+#ifdef BOTAN_HAS_AES
    HANDLE_TYPE_NO_ARGS("AES", AES);
    HANDLE_TYPE_NO_ARGS("AES-128", AES_128);
    HANDLE_TYPE_NO_ARGS("AES-192", AES_192);
    HANDLE_TYPE_NO_ARGS("AES-256", AES_256);
+#endif
+
+#ifdef BOTAN_HAS_BLOWFISH
    HANDLE_TYPE_NO_ARGS("Blowfish", Blowfish);
+#endif
+
+#ifdef BOTAN_HAS_CAST
    HANDLE_TYPE_NO_ARGS("CAST-128", CAST_128);
    HANDLE_TYPE_NO_ARGS("CAST-256", CAST_256);
+#endif
+
+#ifdef BOTAN_HAS_DES
    HANDLE_TYPE_NO_ARGS("DES", DES);
    HANDLE_TYPE_NO_ARGS("DESX", DESX);
    HANDLE_TYPE_NO_ARGS("TripleDES", TripleDES);
-   HANDLE_TYPE_NO_ARGS("GOST", GOST);
-   HANDLE_TYPE_NO_ARGS("IDEA", IDEA);
-   HANDLE_TYPE_NO_ARGS("KASUMI", KASUMI);
-   HANDLE_TYPE_NO_ARGS("MARS", MARS);
-   HANDLE_TYPE_ONE_U32BIT("MISTY1", MISTY1, 8);
-   HANDLE_TYPE_NO_ARGS("Noekeon", Noekeon);
-   HANDLE_TYPE_NO_ARGS("RC2", RC2);
-   HANDLE_TYPE_ONE_U32BIT("RC5", RC5, 12);
-   HANDLE_TYPE_NO_ARGS("RC6", RC6);
-   HANDLE_TYPE_ONE_U32BIT("SAFER-SK", SAFER_SK, 10);
-   HANDLE_TYPE_NO_ARGS("SEED", SEED);
-   HANDLE_TYPE_NO_ARGS("Serpent", Serpent);
-   HANDLE_TYPE_NO_ARGS("Skipjack", Skipjack);
-   HANDLE_TYPE_NO_ARGS("Square", Square);
-   HANDLE_TYPE_NO_ARGS("TEA", TEA);
-   HANDLE_TYPE_NO_ARGS("Twofish", Twofish);
-   HANDLE_TYPE_NO_ARGS("XTEA", XTEA);
+#endif
 
+#ifdef BOTAN_HAS_GOST
+   HANDLE_TYPE_NO_ARGS("GOST", GOST);
+#endif
+
+#ifdef BOTAN_HAS_IDEA
+   HANDLE_TYPE_NO_ARGS("IDEA", IDEA);
+#endif
+
+#ifdef BOTAN_HAS_KASUMI
+   HANDLE_TYPE_NO_ARGS("KASUMI", KASUMI);
+#endif
+
+#ifdef BOTAN_HAS_MARS
+   HANDLE_TYPE_NO_ARGS("MARS", MARS);
+#endif
+
+#ifdef BOTAN_HAS_MISTY1
+   HANDLE_TYPE_ONE_U32BIT("MISTY1", MISTY1, 8);
+#endif
+
+#ifdef BOTAN_HAS_NOEKEON
+   HANDLE_TYPE_NO_ARGS("Noekeon", Noekeon);
+#endif
+
+#ifdef BOTAN_HAS_RC2
+   HANDLE_TYPE_NO_ARGS("RC2", RC2);
+#endif
+
+#ifdef BOTAN_HAS_RC5
+   HANDLE_TYPE_ONE_U32BIT("RC5", RC5, 12);
+#endif
+
+#ifdef BOTAN_HAS_RC6
+   HANDLE_TYPE_NO_ARGS("RC6", RC6);
+#endif
+
+#ifdef BOTAN_HAS_SAFER
+   HANDLE_TYPE_ONE_U32BIT("SAFER-SK", SAFER_SK, 10);
+#endif
+
+#ifdef BOTAN_HAS_SEED
+   HANDLE_TYPE_NO_ARGS("SEED", SEED);
+#endif
+
+#ifdef BOTAN_HAS_SERPENT
+   HANDLE_TYPE_NO_ARGS("Serpent", Serpent);
+#endif
+
+#ifdef BOTAN_HAS_SKIPJACK
+   HANDLE_TYPE_NO_ARGS("Skipjack", Skipjack);
+#endif
+
+#ifdef BOTAN_HAS_SQUARE
+   HANDLE_TYPE_NO_ARGS("Square", Square);
+#endif
+
+#ifdef BOTAN_HAS_TEA
+   HANDLE_TYPE_NO_ARGS("TEA", TEA);
+#endif
+
+#ifdef BOTAN_HAS_TWOFISH
+   HANDLE_TYPE_NO_ARGS("Twofish", Twofish);
+#endif
+
+#ifdef BOTAN_HAS_XTEA
+   HANDLE_TYPE_NO_ARGS("XTEA", XTEA);
+#endif
+
+#ifdef BOTAN_HAS_LUBY_RACKOFF
    if(algo_name == "Luby-Rackoff" && name.size() >= 2)
       {
       HashFunction* hash = find_hash(name[1]);
       if(hash)
          return new LubyRackoff(hash);
       }
+#endif
 
+#ifdef BOTAN_HAS_LION
    if(algo_name == "Lion")
       {
       if(name.size() != 4)
          throw Invalid_Algorithm_Name(algo_spec);
       return new Lion(name[1], name[2], to_u32bit(name[3]));
       }
+#endif
+
    return 0;
    }
 
@@ -176,11 +322,22 @@ Default_Engine::find_stream_cipher(const std::string& algo_spec) const
       return 0;
    const std::string algo_name = global_state().deref_alias(name[0]);
 
+#ifdef BOTAN_HAS_ARC4
    HANDLE_TYPE_ONE_U32BIT("ARC4", ARC4, 0);
    HANDLE_TYPE_ONE_U32BIT("RC4_drop", ARC4, 768);
+#endif
+
+#ifdef BOTAN_HAS_SALSA20
    HANDLE_TYPE_NO_ARGS("Salsa20", Salsa20);
+#endif
+
+#ifdef BOTAN_HAS_TURING
    HANDLE_TYPE_NO_ARGS("Turing", Turing);
+#endif
+
+#ifdef BOTAN_HAS_WID_WAKE
    HANDLE_TYPE_NO_ARGS("WiderWake4+1-BE", WiderWake_41_BE);
+#endif
 
    return 0;
    }
