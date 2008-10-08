60e7a9bea4bca8d4c7905129bd98b643da895ed0
lloyd <lloyd@randombit.net>
2008-10-08 17:06:18 +0000

    Split OpenSSL public key implementations into algorithm specific files, which allows it to be used while still disabling some (or all) public key algorithms.

--- src/engine/openssl/eng_ossl.h
+++ src/engine/openssl/eng_ossl.h
@@ -17,15 +17,29 @@ namespace Botan {
 class OpenSSL_Engine : public Engine
    {
    public:
+#if defined(BOTAN_HAS_IF_PUBLIC_KEY_FAMILY)
       IF_Operation* if_op(const BigInt&, const BigInt&, const BigInt&,
                           const BigInt&, const BigInt&, const BigInt&,
                           const BigInt&, const BigInt&) const;
+#endif
+
+#if defined(BOTAN_HAS_DSA)
       DSA_Operation* dsa_op(const DL_Group&, const BigInt&,
                             const BigInt&) const;
+#endif
+
+#if defined(BOTAN_HAS_NYBERG_RUEPPEL)
       NR_Operation* nr_op(const DL_Group&, const BigInt&, const BigInt&) const;
+#endif
+
+#if defined(BOTAN_HAS_ELGAMAL)
       ELG_Operation* elg_op(const DL_Group&, const BigInt&,
                             const BigInt&) const;
+#endif
+
+#if defined(BOTAN_HAS_DIFFIE_HELLMAN)
       DH_Operation* dh_op(const DL_Group&, const BigInt&) const;
+#endif
 
       Modular_Exponentiator* mod_exp(const BigInt&,
                                      Power_Mod::Usage_Hints) const;
