98588c8fc66440781a65630d51d3920bedb2a2d0
lloyd <lloyd@randombit.net>
2008-10-11 18:04:36 +0000

    Correct OID for ECDSA param

--- doc/examples/ecdsa.cpp
+++ doc/examples/ecdsa.cpp
@@ -13,7 +13,7 @@ int main()
       std::auto_ptr<RandomNumberGenerator> rng(
          RandomNumberGenerator::make_rng());
 
-      EC_Domain_Params params = get_EC_Dom_Pars_by_oid("1.3.132.8");
+      EC_Domain_Params params = get_EC_Dom_Pars_by_oid("1.3.132.0.8");
 
       std::cout << params.get_curve().get_p() << "\n";
       std::cout << params.get_order() << "\n";
