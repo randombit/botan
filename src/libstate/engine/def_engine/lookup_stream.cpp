a4be9296531e66b3d40b5ff195bfd9ba9b9c070f
lloyd <lloyd@randombit.net>
2008-11-10 02:22:54 +0000

    Split lookup_cipher.cpp into lookup_{block,stream,bc_pad}.cpp

---
+++ src/libstate/engine/def_engine/lookup_stream.cpp
@@ -0,0 +1,85 @@
+/*************************************************
+* Stream Cipher Lookup                           *
+* (C) 1999-2007 Jack Lloyd                       *
+*************************************************/
+
+#include <botan/def_eng.h>
+#include <botan/scan_name.h>
+
+#if defined(BOTAN_HAS_ARC4)
+  #include <botan/arc4.h>
+#endif
+
+#if defined(BOTAN_HAS_SALSA20)
+  #include <botan/salsa20.h>
+#endif
+
+#if defined(BOTAN_HAS_TURING)
+  #include <botan/turing.h>
+#endif
+
+#if defined(BOTAN_HAS_WID_WAKE)
+  #include <botan/wid_wake.h>
+#endif
+
+namespace Botan {
+
+/*************************************************
+* Look for an algorithm with this name           *
+*************************************************/
+StreamCipher*
+Default_Engine::find_stream_cipher(const std::string& algo_spec) const
+   {
+   SCAN_Name request(algo_spec);
+
+#if defined(BOTAN_HAS_ARC4)
+   if(request.algo_name() == "ARC4")
+      return new ARC4(request.argument_as_u32bit(0, 0));
+   if(request.algo_name() == "RC4_drop")
+      return new ARC4(768);
+#endif
+
+#if defined(BOTAN_HAS_SALSA20)
+   if(request.algo_name() == "Salsa20")
+      return new Salsa20;
+#endif
+
+#if defined(BOTAN_HAS_TURING)
+   if(request.algo_name() == "Turing")
+      return new Turing;
+#endif
+
+#if defined(BOTAN_HAS_WID_WAKE)
+   if(request.algo_name() == "WiderWake4+1-BE")
+      return new WiderWake_41_BE;
+#endif
+
+   return 0;
+   }
+
+/*************************************************
+* Look for an algorithm with this name           *
+*************************************************/
+BlockCipherModePaddingMethod*
+Default_Engine::find_bc_pad(const std::string& algo_spec) const
+   {
+   SCAN_Name request(algo_spec);
+
+#if defined(BOTAN_HAS_CIPHER_MODE_PADDING)
+   if(request.algo_name() == "PKCS7")
+      return new PKCS7_Padding;
+
+   if(request.algo_name() == "OneAndZeros")
+      return new OneAndZeros_Padding;
+
+   if(request.algo_name() == "X9.23")
+      return new ANSI_X923_Padding;
+
+   if(request.algo_name() == "NoPadding")
+      return new Null_Padding;
+#endif
+
+   return 0;
+   }
+
+}
