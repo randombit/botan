72a829e04030906bd7dbc90437980cc9adbe5620
lloyd <lloyd@randombit.net>
2008-11-10 02:24:29 +0000

    Remove find_bc_pad from lookup_stream.cpp

--- src/libstate/engine/def_engine/lookup_stream.cpp
+++ src/libstate/engine/def_engine/lookup_stream.cpp
@@ -57,29 +57,4 @@ Default_Engine::find_stream_cipher(const std::string& algo_spec) const
    return 0;
    }
 
-/*************************************************
-* Look for an algorithm with this name           *
-*************************************************/
-BlockCipherModePaddingMethod*
-Default_Engine::find_bc_pad(const std::string& algo_spec) const
-   {
-   SCAN_Name request(algo_spec);
-
-#if defined(BOTAN_HAS_CIPHER_MODE_PADDING)
-   if(request.algo_name() == "PKCS7")
-      return new PKCS7_Padding;
-
-   if(request.algo_name() == "OneAndZeros")
-      return new OneAndZeros_Padding;
-
-   if(request.algo_name() == "X9.23")
-      return new ANSI_X923_Padding;
-
-   if(request.algo_name() == "NoPadding")
-      return new Null_Padding;
-#endif
-
-   return 0;
-   }
-
 }
