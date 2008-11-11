80c0504a2e60061f8a0424fdf697d3c8429af918
lloyd <lloyd@randombit.net>
2008-11-11 21:49:05 +0000

    Mention an idea for wall clock sync in Hardware_Timer

--- src/timer/cpu_counter/tm_hard.h
+++ src/timer/cpu_counter/tm_hard.h
@@ -16,6 +16,12 @@ namespace Botan {
 class BOTAN_DLL Hardware_Timer : public Timer
    {
    public:
+      /*
+      @todo: Add sync(Timer& wall_clock, bool milliseconds) which busy
+      loops using wall_clock and tries to guess the tick rate of the
+      hardware counter, allowing it to be used for benchmarks, etc
+      */
+
       std::string name() const { return "Hardware Timer"; }
       u64bit clock() const;
    };
