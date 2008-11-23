b203af2842453ff2e4509840d71232b415f5ce67
lloyd <lloyd@randombit.net>
2008-11-23 04:07:06 +0000

    Dean Gaudet's original version of the SHA-1 SSE2 code supported multiple blocks as input (and can overlap computations from one block to another - very nice). Reimport that original version and use it.

--- src/hash/sha1_sse2/sha1_sse2.cpp
+++ src/hash/sha1_sse2/sha1_sse2.cpp
@@ -12,11 +12,7 @@ namespace Botan {
 *************************************************/
 void SHA_160_SSE2::compress_n(const byte input[], u32bit blocks)
    {
-   for(u32bit i = 0; i != blocks; ++i)
-      {
-      botan_sha1_sse2_compress(digest, reinterpret_cast<const u32bit*>(input));
-      input += HASH_BLOCK_SIZE;
-      }
+   botan_sha1_sse2_compress(digest, reinterpret_cast<const u32bit*>(input), blocks);
    }
 
 }
