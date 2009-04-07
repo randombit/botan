bbef5652aa760beb83ef3116ecec3bd5eb76af35
lloyd <lloyd@randombit.net>
2009-04-07 19:55:46 +0000

    Make the member variables of MDx_HashFunction private instead of protected - no subclass needs access to any of these variables.

--- src/hash/mdx_hash/mdx_hash.h
+++ src/hash/mdx_hash/mdx_hash.h
@@ -21,18 +21,17 @@ class BOTAN_DLL MDx_HashFunction : public HashFunction
       MDx_HashFunction(u32bit, u32bit, bool, bool, u32bit = 8);
       virtual ~MDx_HashFunction() {}
    protected:
+      void add_data(const byte[], u32bit);
+      void final_result(byte output[]);
+      virtual void compress_n(const byte block[], u32bit block_n) = 0;
+
       void clear() throw();
+      virtual void copy_out(byte[]) = 0;
+      virtual void write_count(byte[]);
+   private:
       SecureVector<byte> buffer;
       u64bit count;
       u32bit position;
-   private:
-      void add_data(const byte[], u32bit);
-      void final_result(byte output[]);
-
-      virtual void compress_n(const byte block[], u32bit block_n) = 0;
-
-      virtual void copy_out(byte[]) = 0;
-      virtual void write_count(byte[]);
 
       const bool BIG_BYTE_ENDIAN, BIG_BIT_ENDIAN;
       const u32bit COUNT_SIZE;
