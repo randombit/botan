90394a98550551d882fb4d9610b9b8bfa7271c76
lloyd <lloyd@randombit.net>
2009-12-24 22:21:25 +0000

    Add/fix doxygen comments

--- src/utils/time.h
+++ src/utils/time.h
@@ -13,6 +13,9 @@
 
 namespace Botan {
 
+/**
+* Struct representing a particular date and time
+*/
 struct BOTAN_DLL calendar_point
    {
    u32bit year;
@@ -26,15 +29,19 @@ struct BOTAN_DLL calendar_point
       year(y), month(mon), day(d), hour(h), minutes(min), seconds(sec) {}
    };
 
-/*
-* Time Access/Conversion Functions
+/**
+* @param time_point a time point from the system clock
+* @returns calendar_point object representing this time point
+*/
+BOTAN_DLL calendar_point calendar_value(u64bit time_point);
+
+/**
+* @return seconds resolution timestamp, unknown epoch
 */
 BOTAN_DLL u64bit system_time();
 
-BOTAN_DLL calendar_point calendar_value(u64bit a_time_t);
-
 /**
-@return nanoseconds resolution timestamp, unknown epoch
+* @return nanoseconds resolution timestamp, unknown epoch
 */
 BOTAN_DLL u64bit get_nanoseconds_clock();
 
