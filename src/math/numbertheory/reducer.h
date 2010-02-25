23c82b7a5a17ee39dc69159592a7d7e1e8d03740
lloyd <lloyd@randombit.net>
2010-02-25 19:23:43 +0000

    Name vars in Modular_Reducer decl

--- src/math/numbertheory/reducer.h
+++ src/math/numbertheory/reducer.h
@@ -18,14 +18,14 @@ namespace Botan {
 class BOTAN_DLL Modular_Reducer
    {
    public:
-      BigInt multiply(const BigInt&, const BigInt&) const;
-      BigInt square(const BigInt&) const;
-      BigInt reduce(const BigInt&) const;
+      BigInt multiply(const BigInt& x, const BigInt& y) const;
+      BigInt square(const BigInt& x) const;
+      BigInt reduce(const BigInt& x) const;
 
       bool initialized() const { return (mod_words != 0); }
 
       Modular_Reducer() { mod_words = 0; }
-      Modular_Reducer(const BigInt&);
+      Modular_Reducer(const BigInt& mod);
    private:
       BigInt modulus, modulus_2, mu;
       u32bit mod_words, mod2_words, mu_words;
