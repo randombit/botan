fde718068a630858f152485b6b19cd8c30fd1e03
lloyd <lloyd@randombit.net>
2010-04-20 00:07:38 +0000

    Compile fix

--- src/ssl/tls_suites.cpp
+++ src/ssl/tls_suites.cpp
@@ -13,7 +13,7 @@ namespace Botan {
 /**
 * Convert an SSL/TLS ciphersuite to algorithm fields
 */
-TLS_Ciphersuite_Algos TLS_Suites::lookup_ciphersuite(u16bit suite)
+TLS_Ciphersuite_Algos CipherSuite::lookup_ciphersuite(u16bit suite)
    {
    if(suite == TLS_RSA_WITH_RC4_128_MD5)
       return TLS_Ciphersuite_Algos(TLS_ALGO_SIGNER_RSA |
