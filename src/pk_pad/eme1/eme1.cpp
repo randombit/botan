7c5877d0e2712b9c1bd24e8190f3ca3e433e3570
lloyd <lloyd@randombit.net>
2010-05-25 17:32:04 +0000

    Initial partial protection against timing attacks in OAEP decoding. The loop that scans for the delim byte is still input-dependent, though.

--- src/pk_pad/eme1/eme1.cpp
+++ src/pk_pad/eme1/eme1.cpp
@@ -53,8 +53,11 @@ SecureVector<byte> EME1::unpad(const byte in[], u32bit in_length,
    */
 
    key_length /= 8;
+
+   // Invalid input: truncate to zero length input, causing later
+   // checks to fail
    if(in_length > key_length)
-      throw Decoding_Error("Invalid EME1 encoding");
+      in_length = 0;
 
    SecureVector<byte> tmp(key_length);
    tmp.copy(key_length - in_length, in, in_length);
@@ -62,20 +65,29 @@ SecureVector<byte> EME1::unpad(const byte in[], u32bit in_length,
    mgf->mask(tmp + HASH_LENGTH, tmp.size() - HASH_LENGTH, tmp, HASH_LENGTH);
    mgf->mask(tmp, HASH_LENGTH, tmp + HASH_LENGTH, tmp.size() - HASH_LENGTH);
 
-   for(u32bit j = 0; j != Phash.size(); ++j)
-      if(tmp[j+HASH_LENGTH] != Phash[j])
-         throw Decoding_Error("Invalid EME1 encoding");
+   const bool phash_ok = same_mem(&tmp[HASH_LENGTH], &Phash[0], Phash.size());
 
-   for(u32bit j = HASH_LENGTH + Phash.size(); j != tmp.size(); ++j)
+   bool delim_ok = true;
+   u32bit delim_idx = 0;
+
+   // Is this vulnerable to timing attacks?
+   for(u32bit i = HASH_LENGTH + Phash.size(); i != tmp.size(); ++i)
       {
-      if(tmp[j] && tmp[j] != 0x01)
-         throw Decoding_Error("Invalid EME1 encoding");
-      if(tmp[j] && tmp[j] == 0x01)
+      if(tmp[i] && !delim_idx)
          {
-         SecureVector<byte> retval(tmp + j + 1, tmp.size() - j - 1);
-         return retval;
+         if(tmp[i] == 0x01)
+            delim_idx = i;
+         else
+            delim_ok = false;
          }
       }
+
+   if(delim_idx && delim_ok && phash_ok)
+      {
+      return SecureVector<byte>(tmp + delim_idx + 1,
+                                tmp.size() - delim_idx - 1);
+      }
+
    throw Decoding_Error("Invalid EME1 encoding");
    }
 
