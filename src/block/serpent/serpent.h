a62eaa2f7447d930db84f966986736d30ef522bb
lloyd <lloyd@randombit.net>
2010-06-21 14:21:37 +0000

    Make Serpent's key_schedule and actual round keys private. Add protected accessor functions for get and set. Set is needed by the x86 version since it implements the key schedule directly.

--- src/block/serpent/serpent.h
+++ src/block/serpent/serpent.h
@@ -26,8 +26,22 @@ class BOTAN_DLL Serpent : public BlockCipher
       BlockCipher* clone() const { return new Serpent; }
       Serpent() : BlockCipher(16, 16, 32, 8) {}
    protected:
-      void key_schedule(const byte key[], u32bit length);
+      /**
+      * For use by subclasses using SIMD, asm, etc
+      * @return const reference to the key schedule
+      */
+      const SecureVector<u32bit, 132>& get_round_keys() const
+         { return round_key; }
 
+      /**
+      * For use by subclasses that implement the key schedule
+      * @param ks is the new key schedule value to set
+      */
+      void set_round_keys(const u32bit ks[132])
+         { round_key.set(ks, 132); }
+
+   private:
+      void key_schedule(const byte key[], u32bit length);
       SecureVector<u32bit, 132> round_key;
    };
 
