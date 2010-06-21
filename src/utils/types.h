fe47c1cb19d6f3d1fb54f488cf08eb1830dda1e5
lloyd <lloyd@randombit.net>
2010-06-21 14:42:27 +0000

    Doxygen

--- src/utils/types.h
+++ src/utils/types.h
@@ -15,12 +15,29 @@
 */
 namespace Botan {
 
+/**
+* Typedef representing an unsigned 8-bit quantity
+*/
 typedef unsigned char byte;
+
+/**
+* Typedef representing an unsigned 16-bit quantity
+*/
 typedef unsigned short u16bit;
+
+/**
+* Typedef representing an unsigned 32-bit quantity
+*/
 typedef unsigned int u32bit;
 
+/**
+* Typedef representing a signed 32-bit quantity
+*/
 typedef signed int s32bit;
 
+/**
+* Typedef representing an unsigned 64-bit quantity
+*/
 #if defined(_MSC_VER) || defined(__BORLANDC__)
    typedef unsigned __int64 u64bit;
 #elif defined(__KCC)
@@ -31,6 +48,9 @@ typedef signed int s32bit;
    typedef unsigned long long u64bit;
 #endif
 
+/**
+* A default buffer size; typically a memory page
+*/
 static const u32bit DEFAULT_BUFFERSIZE = BOTAN_DEFAULT_BUFFER_SIZE;
 
 }
