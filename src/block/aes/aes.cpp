1cfe5f6ec5435461862e42873fb3e3e338739eb1
lloyd <lloyd@randombit.net>
2010-08-20 20:10:33 +0000

    Use small tables in the first round of AES. See notes from rev 7bb73327a01fa6022adcfffcd3faef03ed42ab96 for details.

--- src/block/aes/aes.cpp
+++ src/block/aes/aes.cpp
@@ -25,15 +25,25 @@ void AES::enc(const byte in[], byte out[]) const
    u32bit T2 = load_be<u32bit>(in, 2) ^ EK[2];
    u32bit T3 = load_be<u32bit>(in, 3) ^ EK[3];
 
-   u32bit B0, B1, B2, B3;
-   B0 = TE0[get_byte(0, T0)] ^ TE1[get_byte(1, T1)] ^
-        TE2[get_byte(2, T2)] ^ TE3[get_byte(3, T3)] ^ EK[4];
-   B1 = TE0[get_byte(0, T1)] ^ TE1[get_byte(1, T2)] ^
-        TE2[get_byte(2, T3)] ^ TE3[get_byte(3, T0)] ^ EK[5];
-   B2 = TE0[get_byte(0, T2)] ^ TE1[get_byte(1, T3)] ^
-        TE2[get_byte(2, T0)] ^ TE3[get_byte(3, T1)] ^ EK[6];
-   B3 = TE0[get_byte(0, T3)] ^ TE1[get_byte(1, T0)] ^
-        TE2[get_byte(2, T1)] ^ TE3[get_byte(3, T2)] ^ EK[7];
+   u32bit B0 = TE[get_byte(0, T0)] ^
+               rotate_right(TE[get_byte(1, T1)],  8) ^
+               rotate_right(TE[get_byte(2, T2)], 16) ^
+               rotate_right(TE[get_byte(3, T3)], 24) ^ EK[4];
+
+   u32bit B1 = TE[get_byte(0, T1)] ^
+               rotate_right(TE[get_byte(1, T2)],  8) ^
+               rotate_right(TE[get_byte(2, T3)], 16) ^
+               rotate_right(TE[get_byte(3, T0)], 24) ^ EK[5];
+
+   u32bit B2 = TE[get_byte(0, T2)] ^
+               rotate_right(TE[get_byte(1, T3)],  8) ^
+               rotate_right(TE[get_byte(2, T0)], 16) ^
+               rotate_right(TE[get_byte(3, T1)], 24) ^ EK[6];
+
+   u32bit B3 = TE[get_byte(0, T3)] ^
+               rotate_right(TE[get_byte(1, T0)],  8) ^
+               rotate_right(TE[get_byte(2, T1)], 16) ^
+               rotate_right(TE[get_byte(3, T2)], 24) ^ EK[7];
 
    for(u32bit j = 2; j != ROUNDS; j += 2)
       {
@@ -113,15 +123,25 @@ void AES::dec(const byte in[], byte out[]) const
    u32bit T2 = load_be<u32bit>(in, 2) ^ DK[2];
    u32bit T3 = load_be<u32bit>(in, 3) ^ DK[3];
 
-   u32bit B0, B1, B2, B3;
-   B0 = TD0[get_byte(0, T0)] ^ TD1[get_byte(1, T3)] ^
-        TD2[get_byte(2, T2)] ^ TD3[get_byte(3, T1)] ^ DK[4];
-   B1 = TD0[get_byte(0, T1)] ^ TD1[get_byte(1, T0)] ^
-        TD2[get_byte(2, T3)] ^ TD3[get_byte(3, T2)] ^ DK[5];
-   B2 = TD0[get_byte(0, T2)] ^ TD1[get_byte(1, T1)] ^
-        TD2[get_byte(2, T0)] ^ TD3[get_byte(3, T3)] ^ DK[6];
-   B3 = TD0[get_byte(0, T3)] ^ TD1[get_byte(1, T2)] ^
-        TD2[get_byte(2, T1)] ^ TD3[get_byte(3, T0)] ^ DK[7];
+   u32bit B0 = TD[get_byte(0, T0)] ^
+               rotate_right(TD[get_byte(1, T3)],  8) ^
+               rotate_right(TD[get_byte(2, T2)], 16) ^
+               rotate_right(TD[get_byte(3, T1)], 24) ^ DK[4];
+
+   u32bit B1 = TD[get_byte(0, T1)] ^
+               rotate_right(TD[get_byte(1, T0)],  8) ^
+               rotate_right(TD[get_byte(2, T3)], 16) ^
+               rotate_right(TD[get_byte(3, T2)], 24) ^ DK[5];
+
+   u32bit B2 = TD[get_byte(0, T2)] ^
+               rotate_right(TD[get_byte(1, T1)],  8) ^
+               rotate_right(TD[get_byte(2, T0)], 16) ^
+               rotate_right(TD[get_byte(3, T3)], 24) ^ DK[6];
+
+   u32bit B3 = TD[get_byte(0, T3)] ^
+               rotate_right(TD[get_byte(1, T2)],  8) ^
+               rotate_right(TD[get_byte(2, T1)], 16) ^
+               rotate_right(TD[get_byte(3, T0)], 24) ^ DK[7];
 
    for(u32bit j = 2; j != ROUNDS; j += 2)
       {
