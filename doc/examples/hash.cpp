d6c39e385e105da61797695c0583dd796e5af965
lloyd <lloyd@randombit.net>
2010-09-08 17:02:37 +0000

    Read input as binary

--- doc/examples/hash.cpp
+++ doc/examples/hash.cpp
@@ -36,7 +36,7 @@ int main(int argc, char* argv[])
       int skipped = 0;
       for(int j = 2; argv[j] != 0; j++)
          {
-         std::ifstream file(argv[j]);
+         std::ifstream file(argv[j], std::ios::binary);
          if(!file)
             {
             std::cout << "ERROR: could not open " << argv[j] << std::endl;
