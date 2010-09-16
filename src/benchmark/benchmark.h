c037226de0af018187d03e7caaf6acb754fe1039
lloyd <lloyd@randombit.net>
2010-09-16 16:44:41 +0000

    I had this older version of algorithm_benchmark included for compatability with 1.8, but actually the signature is completely different anyway because that version took a Timer object, which doesn't exist at all anymore.
    I suppose I could add an empty Timer class plus subclasses, let
    someone instantiate it and pass it in, ignoring it, but I'm not
    feeling this is worth the effort. It would make more sense to add a
    version with this signature to 1.8, which creates a
    Default_Benchmark_Timer and uses it.

--- src/benchmark/benchmark.h
+++ src/benchmark/benchmark.h
@@ -31,24 +31,6 @@ BOTAN_DLL algorithm_benchmark(const std::string& name,
                               u32bit milliseconds,
                               u32bit buf_size);
 
-/**
-* Algorithm benchmark (1.8 compatability version)
-* @deprecated Use variant taking buf_size defined above
-* @param name the name of the algorithm to test (cipher, hash, or MAC)
-* @param milliseconds total time for the benchmark to run
-* @param rng the rng to use to generate random inputs
-* @param af the algorithm factory used to create objects
-* @return results a map from provider to speed in mebibytes per second
-*/
-std::map<std::string, double>
-inline algorithm_benchmark(const std::string& name,
-                           u32bit milliseconds,
-                           RandomNumberGenerator& rng,
-                           Algorithm_Factory& af)
-   {
-   return algorithm_benchmark(name, af, rng, milliseconds, 16);
-   }
-
 }
 
 #endif
