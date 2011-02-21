08e5c8bd3e792f309f3a3b627ed545946b55d5e7
lloyd <lloyd@randombit.net>
2011-02-21 17:57:19 +0000

    Tick to 1.8.12-dev

--- configure.py
+++ configure.py
@@ -37,9 +37,9 @@ class BuildConfigurationInformation(object):
     """
     version_major = 1
     version_minor = 8
-    version_patch = 11
-    version_so_patch = 2
-    version_suffix = ''
+    version_patch = 12
+    version_so_patch = 12
+    version_suffix = '-dev'
 
     version_string = '%d.%d.%d%s' % (
         version_major, version_minor, version_patch, version_suffix)
