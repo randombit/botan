fc62f7f284387a180e42402e8706965a666efba7
lloyd <lloyd@randombit.net>
2011-04-08 14:57:49 +0000

    More pubkey doc updates

--- doc/examples/dh.cpp
+++ doc/examples/dh.cpp
@@ -1,9 +1,3 @@
-/*
-* (C) 2009-2010 Jack Lloyd
-*
-* Distributed under the terms of the Botan license
-*/
-
 #include <botan/botan.h>
 #include <botan/dh.h>
 #include <botan/pubkey.h>
@@ -14,41 +8,36 @@ using namespace Botan;
 
 int main()
    {
-   Botan::LibraryInitializer init;
-
    try
       {
+      LibraryInitializer init;
+
       AutoSeeded_RNG rng;
 
       // Alice and Bob agree on a DH domain to use
-      DL_Group shared_domain("modp/ietf/1024");
+      DL_Group shared_domain("modp/ietf/2048");
 
-      // Alice creates a DH key and sends (the public part) to Bob
+      // Alice creates a DH key
       DH_PrivateKey private_a(rng, shared_domain);
-
-      // Alice sends to Bob her public key:
-      MemoryVector<byte> public_a = private_a.public_value();
 
       // Bob creates a key with a matching group
       DH_PrivateKey private_b(rng, shared_domain);
 
+      // Alice sends to Bob her public key and a session parameter
+      MemoryVector<byte> public_a = private_a.public_value();
+      const std::string session_param =
+         "Alice and Bob's shared session parameter";
+
       // Bob sends his public key to Alice
       MemoryVector<byte> public_b = private_b.public_value();
 
-      PK_Key_Agreement ka1(private_a, "KDF2(SHA-1)");
-      PK_Key_Agreement ka2(private_b, "KDF2(SHA-1)");
+      // Now Alice performs the key agreement operation
+      PK_Key_Agreement ka_alice(private_a, "KDF2(SHA-256)");
+      SymmetricKey alice_key = ka1.derive_key(32, public_b, session_param);
 
-      /*
-      * Preferably, include some salt or parameter that binds this key
-      * generation to the current session (for instance a session
-      * identifier, if guaranteed unique, would be a good choice).  Or
-      * anything else that both sides can agree on that will never
-      * repeat.
-      */
-      const std::string ka_salt = "alice and bob agree on a key";
-
-      SymmetricKey alice_key = ka1.derive_key(32, public_b, ka_salt);
-      SymmetricKey bob_key = ka2.derive_key(32, public_a, ka_salt);
+      // Bob does the same:
+      PK_Key_Agreement ka_bob(private_b, "KDF2(SHA-256)");
+      SymmetricKey bob_key = ka2.derive_key(32, public_a, session_param);
 
       if(alice_key == bob_key)
          {
