8566e2784dc83c6d8c02de42cf2448cb408100f1
lloyd <lloyd@randombit.net>
2012-01-22 00:54:37 +0000

    Reorder for readability

--- src/tls/tls_suites.cpp
+++ src/tls/tls_suites.cpp
@@ -19,23 +19,11 @@ TLS_Ciphersuite TLS_Ciphersuite::lookup_ciphersuite(u16bit suite)
       {
       // RSA ciphersuites
 
-      case TLS_RSA_WITH_RC4_128_MD5:
-         return TLS_Ciphersuite("RSA", "", "MD5", "ARC4", 16);
-
-      case TLS_RSA_WITH_RC4_128_SHA:
-         return TLS_Ciphersuite("RSA", "", "SHA-1", "ARC4", 16);
-
-      case TLS_RSA_WITH_3DES_EDE_CBC_SHA:
-         return TLS_Ciphersuite("RSA", "", "SHA-1", "TripleDES", 24);
-
       case TLS_RSA_WITH_AES_128_CBC_SHA:
          return TLS_Ciphersuite("RSA", "", "SHA-1", "AES-128", 16);
 
       case TLS_RSA_WITH_AES_256_CBC_SHA:
          return TLS_Ciphersuite("RSA", "", "SHA-1", "AES-256", 32);
-
-      case TLS_RSA_WITH_SEED_CBC_SHA:
-         return TLS_Ciphersuite("RSA", "", "SHA-1", "SEED", 16);
 
       case TLS_RSA_WITH_AES_128_CBC_SHA256:
          return TLS_Ciphersuite("RSA", "", "SHA-256", "AES-128", 16);
@@ -43,19 +31,22 @@ TLS_Ciphersuite TLS_Ciphersuite::lookup_ciphersuite(u16bit suite)
       case TLS_RSA_WITH_AES_256_CBC_SHA256:
          return TLS_Ciphersuite("RSA", "", "SHA-256", "AES-256", 32);
 
-      // DH/DSS ciphersuites
+      case TLS_RSA_WITH_3DES_EDE_CBC_SHA:
+         return TLS_Ciphersuite("RSA", "", "SHA-1", "TripleDES", 24);
 
-      case TLS_DHE_DSS_WITH_3DES_EDE_CBC_SHA:
-         return TLS_Ciphersuite("DSA", "DH", "SHA-1", "TripleDES", 24);
+      case TLS_RSA_WITH_RC4_128_SHA:
+         return TLS_Ciphersuite("RSA", "", "SHA-1", "ARC4", 16);
+
+      case TLS_RSA_WITH_RC4_128_MD5:
+         return TLS_Ciphersuite("RSA", "", "MD5", "ARC4", 16);
+
+      case TLS_RSA_WITH_SEED_CBC_SHA:
+         return TLS_Ciphersuite("RSA", "", "SHA-1", "SEED", 16);
+
+      // DH/DSS ciphersuites
 
       case TLS_DHE_DSS_WITH_AES_128_CBC_SHA:
          return TLS_Ciphersuite("DSA", "DH", "SHA-1", "AES-128", 16);
-
-      case TLS_DHE_DSS_WITH_SEED_CBC_SHA:
-         return TLS_Ciphersuite("DSA", "DH", "SHA-1", "SEED", 16);
-
-      case TLS_DHE_DSS_WITH_RC4_128_SHA:
-         return TLS_Ciphersuite("DSA", "DH", "SHA-1", "ARC4", 16);
 
       case TLS_DHE_DSS_WITH_AES_256_CBC_SHA:
          return TLS_Ciphersuite("DSA", "DH", "SHA-1", "AES-256", 32);
@@ -66,16 +57,19 @@ TLS_Ciphersuite TLS_Ciphersuite::lookup_ciphersuite(u16bit suite)
       case TLS_DHE_DSS_WITH_AES_256_CBC_SHA256:
          return TLS_Ciphersuite("DSA", "DH", "SHA-256", "AES-256", 32);
 
-      // DH/RSA ciphersuites
+      case TLS_DHE_DSS_WITH_3DES_EDE_CBC_SHA:
+         return TLS_Ciphersuite("DSA", "DH", "SHA-1", "TripleDES", 24);
 
-      case TLS_DHE_RSA_WITH_3DES_EDE_CBC_SHA:
-         return TLS_Ciphersuite("RSA", "DH", "SHA-1", "TripleDES", 24);
+      case TLS_DHE_DSS_WITH_RC4_128_SHA:
+         return TLS_Ciphersuite("DSA", "DH", "SHA-1", "ARC4", 16);
+
+      case TLS_DHE_DSS_WITH_SEED_CBC_SHA:
+         return TLS_Ciphersuite("DSA", "DH", "SHA-1", "SEED", 16);
+
+      // DH/RSA ciphersuites
 
       case TLS_DHE_RSA_WITH_AES_128_CBC_SHA:
          return TLS_Ciphersuite("RSA", "DH", "SHA-1", "AES-128", 16);
-
-      case TLS_DHE_RSA_WITH_SEED_CBC_SHA:
-         return TLS_Ciphersuite("RSA", "DH", "SHA-1", "SEED", 16);
 
       case TLS_DHE_RSA_WITH_AES_256_CBC_SHA:
          return TLS_Ciphersuite("RSA", "DH", "SHA-1", "AES-256", 32);
@@ -86,46 +80,48 @@ TLS_Ciphersuite TLS_Ciphersuite::lookup_ciphersuite(u16bit suite)
       case TLS_DHE_RSA_WITH_AES_256_CBC_SHA256:
          return TLS_Ciphersuite("RSA", "DH", "SHA-256", "AES-256", 32);
 
+      case TLS_DHE_RSA_WITH_3DES_EDE_CBC_SHA:
+         return TLS_Ciphersuite("RSA", "DH", "SHA-1", "TripleDES", 24);
+
+      case TLS_DHE_RSA_WITH_SEED_CBC_SHA:
+         return TLS_Ciphersuite("RSA", "DH", "SHA-1", "SEED", 16);
+
       // ECDH/RSA ciphersuites
-      case TLS_ECDHE_RSA_WITH_RC4_128_SHA:
-         return TLS_Ciphersuite("RSA", "ECDH", "SHA-1", "ARC4", 16);
-
-      case TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA:
-         return TLS_Ciphersuite("RSA", "ECDH", "SHA-1", "TripleDES", 24);
-
       case TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA:
          return TLS_Ciphersuite("RSA", "ECDH", "SHA-1", "AES-128", 16);
 
       case TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA:
          return TLS_Ciphersuite("RSA", "ECDH", "SHA-1", "AES-256", 32);
 
-      // SRP ciphersuites
+      case TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA:
+         return TLS_Ciphersuite("RSA", "ECDH", "SHA-1", "TripleDES", 24);
 
-      case TLS_SRP_SHA_RSA_WITH_3DES_EDE_SHA:
-         return TLS_Ciphersuite("RSA", "SRP", "SHA-1", "TripleDES", 24);
+      case TLS_ECDHE_RSA_WITH_RC4_128_SHA:
+         return TLS_Ciphersuite("RSA", "ECDH", "SHA-1", "ARC4", 16);
 
-      case TLS_SRP_SHA_DSS_WITH_3DES_EDE_SHA:
-         return TLS_Ciphersuite("DSA", "SRP", "SHA-1", "TripleDES", 24);
+      // SRP/RSA ciphersuites
 
       case TLS_SRP_SHA_RSA_WITH_AES_128_SHA:
          return TLS_Ciphersuite("RSA", "SRP", "SHA-1", "AES-128", 16);
 
-      case TLS_SRP_SHA_DSS_WITH_AES_128_SHA:
-         return TLS_Ciphersuite("DSA", "SRP", "SHA-1", "AES-128", 16);
-
       case TLS_SRP_SHA_RSA_WITH_AES_256_SHA:
          return TLS_Ciphersuite("RSA", "SRP", "SHA-1", "AES-256", 32);
+
+      case TLS_SRP_SHA_RSA_WITH_3DES_EDE_SHA:
+         return TLS_Ciphersuite("RSA", "SRP", "SHA-1", "TripleDES", 24);
+
+      // SRP/DSA ciphersuites
+
+      case TLS_SRP_SHA_DSS_WITH_AES_128_SHA:
+         return TLS_Ciphersuite("DSA", "SRP", "SHA-1", "AES-128", 16);
 
       case TLS_SRP_SHA_DSS_WITH_AES_256_SHA:
          return TLS_Ciphersuite("DSA", "SRP", "SHA-1", "AES-256", 32);
 
+      case TLS_SRP_SHA_DSS_WITH_3DES_EDE_SHA:
+         return TLS_Ciphersuite("DSA", "SRP", "SHA-1", "TripleDES", 24);
+
       // ECDH/ECDSA ciphersuites
-
-      case TLS_ECDHE_ECDSA_WITH_RC4_128_SHA:
-         return TLS_Ciphersuite("ECDSA", "ECDH", "SHA-1", "ARC4", 16);
-
-      case TLS_ECDHE_ECDSA_WITH_3DES_EDE_CBC_SHA:
-         return TLS_Ciphersuite("ECDSA", "ECDH", "SHA-1", "TripleDES", 24);
 
       case TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA:
          return TLS_Ciphersuite("ECDSA", "ECDH", "SHA-1", "AES-128", 16);
@@ -144,6 +140,12 @@ TLS_Ciphersuite TLS_Ciphersuite::lookup_ciphersuite(u16bit suite)
 
       case TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384:
          return TLS_Ciphersuite("ECDSA", "ECDH", "SHA-384", "AES-256", 32);
+
+      case TLS_ECDHE_ECDSA_WITH_RC4_128_SHA:
+         return TLS_Ciphersuite("ECDSA", "ECDH", "SHA-1", "ARC4", 16);
+
+      case TLS_ECDHE_ECDSA_WITH_3DES_EDE_CBC_SHA:
+         return TLS_Ciphersuite("ECDSA", "ECDH", "SHA-1", "TripleDES", 24);
 
       default:
          return TLS_Ciphersuite(); // some unknown ciphersuite
