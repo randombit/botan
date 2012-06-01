f0baffd392e08344c968e8697c66978e1a61baeb
lloyd <lloyd@randombit.net>
2012-06-01 14:34:07 +0000

    Missing source file from d8021f3e5aa8812a2843d6afd27bbe56d04af734

---
+++ src/pbkdf/pbkdf.cpp
@@ -0,0 +1,44 @@
+/*
+* PBKDF
+* (C) 2012 Jack Lloyd
+*
+* Distributed under the terms of the Botan license
+*/
+
+#include <botan/pbkdf.h>
+#include <botan/internal/assert.h>
+
+namespace Botan {
+
+OctetString PBKDF::derive_key(size_t output_len,
+                              const std::string& passphrase,
+                              const byte salt[], size_t salt_len,
+                              size_t iterations) const
+   {
+   if(iterations == 0)
+      throw std::invalid_argument(name() + ": Invalid iteration count");
+
+   auto derived = key_derivation(output_len, passphrase,
+                                 salt, salt_len, iterations,
+                                 std::chrono::milliseconds(0));
+
+   BOTAN_ASSERT(derived.first == iterations,
+                "PBKDF used the correct number of iterations");
+
+   return derived.second;
+   }
+
+OctetString PBKDF::derive_key(size_t output_len,
+                              const std::string& passphrase,
+                              const byte salt[], size_t salt_len,
+                              std::chrono::milliseconds ms,
+                              size_t& iterations) const
+   {
+   auto derived = key_derivation(output_len, passphrase, salt, salt_len, 0, ms);
+
+   iterations = derived.first;
+
+   return derived.second;
+   }
+
+}
