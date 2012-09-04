7f888f4bbcf8d11ff5916b1056ab181962da5b90
lloyd <lloyd@randombit.net>
2012-09-04 15:17:20 +0000

    Avoid putting dword typedef in global namespace

--- src/math/mp/mp_generic/mp_asm.h
+++ src/math/mp/mp_generic/mp_asm.h
@@ -11,19 +11,19 @@
 
 #include <botan/mp_types.h>
 
+namespace Botan {
+
 #if (BOTAN_MP_WORD_BITS == 8)
-  typedef Botan::u16bit dword;
+  typedef u16bit dword;
 #elif (BOTAN_MP_WORD_BITS == 16)
-  typedef Botan::u32bit dword;
+  typedef u32bit dword;
 #elif (BOTAN_MP_WORD_BITS == 32)
-  typedef Botan::u64bit dword;
+  typedef u64bit dword;
 #elif (BOTAN_MP_WORD_BITS == 64)
   #error BOTAN_MP_WORD_BITS can be 64 only with assembly support
 #else
   #error BOTAN_MP_WORD_BITS must be 8, 16, 32, or 64
 #endif
-
-namespace Botan {
 
 extern "C" {
 
