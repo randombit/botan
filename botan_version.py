6e8a66df33cfa059cb5faab59a4b10fa0a96d6f1
lloyd <lloyd@randombit.net>
2013-02-27 20:23:34 +0000

    Notes for 1.10.5

--- botan_version.py
+++ botan_version.py
@@ -1,9 +1,9 @@
 
 release_major = 1
 release_minor = 10
-release_patch = 4
+release_patch = 5
 
 release_vc_rev = None
 release_so_abi_rev = 0
 
-release_datestamp = 20120107
+release_datestamp = 0
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-Botan 1.10.4, 2013-01-07
+Botan 1.10.5, Not Yet Released
 http://botan.randombit.net/
 
 Botan is a C++ class library for performing a wide variety of
