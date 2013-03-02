d95cbfcbd54d1a80ce921866274c66622fa06c3b
lloyd <lloyd@randombit.net>
2013-03-02 23:52:28 +0000

    Port dist script changes from mainline back to 1.10

--- botan_version.py
+++ botan_version.py
@@ -3,7 +3,8 @@ release_major = 1
 release_minor = 10
 release_patch = 5
 
-release_vc_rev = None
 release_so_abi_rev = 0
 
+# These are set by the distribution script
+release_vc_rev = None
 release_datestamp = 0
