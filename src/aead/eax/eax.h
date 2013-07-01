d2b4514152e606cc34cbb3239546ab2ebb0f2d71
lloyd <lloyd@randombit.net>
2013-07-01 15:07:11 +0000

    Namespace AEAD header guards

--- src/aead/eax/eax.h
+++ src/aead/eax/eax.h
@@ -5,8 +5,8 @@
 * Distributed under the terms of the Botan license
 */
 
-#ifndef BOTAN_EAX_H__
-#define BOTAN_EAX_H__
+#ifndef BOTAN_AEAD_EAX_H__
+#define BOTAN_AEAD_EAX_H__
 
 #include <botan/aead.h>
 #include <botan/block_cipher.h>
