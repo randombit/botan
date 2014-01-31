9332870c160d7a02f1bef6f249fa1baae196dc51
lloyd <lloyd@randombit.net>
2014-01-31 22:28:19 +0000

    Add header guard and fix amalgamation. Github issue 10.

--- src/lib/modes/aead/gcm/clmul/clmul.h
+++ src/lib/modes/aead/gcm/clmul/clmul.h
@@ -5,6 +5,9 @@
 * Distributed under the terms of the Botan license
 */
 
+#ifndef BOTAN_GCM_CLMUL_H__
+#define BOTAN_GCM_CLMUL_H__
+
 #include <botan/types.h>
 
 namespace Botan {
@@ -12,3 +15,5 @@ namespace Botan {
 void gcm_multiply_clmul(byte x[16], const byte H[16]);
 
 }
+
+#endif
