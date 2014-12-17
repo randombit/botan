fe6a46ad89788436d17736b286662b793f776885
lloyd <lloyd@randombit.net>
2014-12-17 03:24:35 +0000

    In OpenSSL engine drop support for public key operations. These PK ops just call BN directly and so don't get the benefit of side channel protections in either OpenSSL's or Botan's implementations of the algorithms.

--- src/lib/engine/openssl/openssl_engine.h
+++ src/lib/engine/openssl/openssl_engine.h
@@ -20,22 +20,6 @@ class OpenSSL_Engine : public Engine
    public:
       std::string provider_name() const override { return "openssl"; }
 
-      PK_Ops::Key_Agreement*
-         get_key_agreement_op(const Private_Key& key, RandomNumberGenerator& rng) const override;
-
-      PK_Ops::Signature* get_signature_op(const Private_Key& key, const std::string& emsa,
-                                          RandomNumberGenerator& rng) const override;
-
-      PK_Ops::Verification* get_verify_op(const Public_Key& key, const std::string& emsa,
-                                          RandomNumberGenerator& rng) const override;
-
-      PK_Ops::Encryption* get_encryption_op(const Public_Key& key, RandomNumberGenerator& rng) const override;
-
-      PK_Ops::Decryption* get_decryption_op(const Private_Key& key, RandomNumberGenerator& rng) const override;
-
-      Modular_Exponentiator* mod_exp(const BigInt&,
-                                     Power_Mod::Usage_Hints) const override;
-
       BlockCipher* find_block_cipher(const SCAN_Name&,
                                      Algorithm_Factory&) const override;
 
