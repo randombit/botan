2d5669770a5723d258703384f905e1d13d6b8696
lloyd <lloyd@randombit.net>
2015-03-23 02:15:45 +0000

    Avoid putting very small values in mlock memory

--- src/lib/alloc/locking_allocator/locking_allocator.cpp
+++ src/lib/alloc/locking_allocator/locking_allocator.cpp
@@ -103,7 +103,9 @@ void* mlock_allocator::allocate(size_t num_elems, size_t elem_size)
    if(n / elem_size != num_elems)
       return nullptr; // overflow!
 
-   if(n > m_poolsize || n > BOTAN_MLOCK_ALLOCATOR_MAX_ALLOCATION)
+   if(n > m_poolsize)
+      return nullptr;
+   if(n < BOTAN_MLOCK_ALLOCATOR_MIN_ALLOCATION || n > BOTAN_MLOCK_ALLOCATOR_MAX_ALLOCATION)
       return nullptr;
 
    std::lock_guard<std::mutex> lock(m_mutex);
