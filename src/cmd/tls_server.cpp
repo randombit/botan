c56d3442e605d508dc372b2d38aeac84f273a43c
Jack Lloyd <lloyd@randombit.net>
2015-07-05 20:19:55 -0400

    Avoid building tls_server on MinGW. GH #39

--- src/cmd/tls_server.cpp
+++ src/cmd/tls_server.cpp
@@ -7,9 +7,9 @@
 
 #include "apps.h"
 
-#if defined(BOTAN_HAS_TLS) \
-  && defined(BOTAN_HAS_DSA) \
-  && !defined(BOTAN_TARGET_OS_IS_WINDOWS)
+#if defined(BOTAN_HAS_TLS) && defined(BOTAN_HAS_DSA) \
+  && !defined(BOTAN_TARGET_OS_IS_WINDOWS) \
+  && !defined(BOTAN_TARGET_OS_IS_MINGW)
 
 #include <botan/tls_server.h>
 #include <botan/hex.h>
