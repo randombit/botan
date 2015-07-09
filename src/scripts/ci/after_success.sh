5f70b0f85bf4946c5e1f2c2736b406b37c36ac5e
Simon Warta <simon@warta.it>
2015-07-09 13:01:35 +0200

    Assume that $COVERALLS_REPO_TOKEN might not be set (e.g. pull requests)

--- src/scripts/ci/after_success.sh
+++ src/scripts/ci/after_success.sh
@@ -6,5 +6,6 @@ if [ "$BUILD_MODE" = "coverage" ]; then
   lcov --directory . --capture --output-file coverage.info
   lcov --remove coverage.info 'tests/*' '/usr/*' --output-file coverage.info
   lcov --list coverage.info
-  coveralls-lcov -t $COVERALLS_REPO_TOKEN coverage.info
+  # Assume that $COVERALLS_REPO_TOKEN might not be set (e.g. pull requests)
+  coveralls-lcov --repo-token="$COVERALLS_REPO_TOKEN" coverage.info
 fi
