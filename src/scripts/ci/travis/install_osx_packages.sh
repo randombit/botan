02c18eb420f695494395dffd5c13ef6f41617fc4
Simon Warta <simon@warta.it>
2015-09-02 21:08:09 +0200

    Update travis configuration for iOS build
    Closes #188

---
+++ src/scripts/ci/travis/install_osx_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -ev
+which shellcheck > /dev/null && shellcheck "$0" # Run shellcheck on this if available
+
+# Workaround for https://github.com/Homebrew/homebrew/issues/42553
+brew update || brew update
+
+brew install xz
