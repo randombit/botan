92c0a35106abb4115e8f5bb8bb0fc6f4f20f0691
Jack Lloyd <lloyd@randombit.net>
2015-10-29 12:14:29 -0400

    Blind attempt to set up up Linux ARM/EABI builds on Travis
    Clean up the travis build script a bit and rename the targets.

--- src/scripts/ci/travis/build.sh
+++ src/scripts/ci/travis/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -ev
 which shellcheck > /dev/null && shellcheck "$0" # Run shellcheck on this if available
 
@@ -13,7 +14,7 @@ elif [ "$BUILD_MODE" = "sanitizer" ]; then
 fi
 
 if [ "$MODULES" = "min" ]; then
-    CFG_FLAGS+=(--minimized-build --enable-modules=base)
+    CFG_FLAGS+=(--minimized-build)
 fi
 
 # Workaround for missing update-alternatives
@@ -30,41 +31,63 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     export CXX="ccache $CXX"
 fi
 
-# configure
-if [ "$TARGETOS" = "ios32" ]; then
-    ./configure.py "${CFG_FLAGS[@]}" --cpu=armv7 --cc=clang \
-        --cc-abi-flags="-arch armv7 -arch armv7s -stdlib=libc++" \
-        --prefix=/tmp/botan-installation
+# choose configure flags
 
-elif [ "$TARGETOS" = "ios64" ]; then
-    ./configure.py "${CFG_FLAGS[@]}" --cpu=armv8-a --cc=clang \
-        --cc-abi-flags="-arch arm64 -stdlib=libc++" \
-        --prefix=/tmp/botan-installation
+MAKE_COMMAND="make -j 2"
+INSTALL_DIR="/tmp/botan-installation"
+CFG="./configure.py ${CFG_FLAGS[@]} --prefix=$INSTALL_DIR"
 
-else
+if [ $TARGET = "native" ]; then
     $CXX --version
-    ./configure.py "${CFG_FLAGS[@]}" --cc="$CC" --cc-bin="$CXX" \
-        --with-bzip2 --with-lzma --with-openssl --with-sqlite --with-zlib \
-        --prefix=/tmp/botan-installation
+    CFG="$CFG --cc=\"$CC\" --cc-bin=\"$CXX\" --with-bzip2 --with-lzma --with-openssl --with-sqlite --with-zlib"
+else
+
+    if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+
+        # On OS X we target iOS/ARM
+        MAKE_COMMAND="xcrun --sdk=iphoneos $MAKE_COMMAND"
+
+        if [ "$TARGET" = "arm32" ]; then
+            CFG="$CFG --cpu=armv7 --cc=clang --cc-abi-flags=\"-arch armv7 -arch armv7s -stdlib=libc++\""
+        elif [ "$TARGET" = "arm64" ]; then
+            CFG="$CFG --cpu=armv8-a --cc=clang --cc-abi-flags=\"-arch arm64 -stdlib=libc++\""
+        else
+            echo "Unknown target $TARGET"
+        fi
+
+    else
+
+        # On Linux we target ARM EABI
+        if [ "$TARGET" = "arm32" ]; then
+            CFG="$CFG --cpu=armv7 --cc-bin=arm-linux-gnueabi-gcc"
+        else
+            echo "Unknown target $TARGET"
+        fi
+    fi
 fi
+
+# run configure script
+eval $CFG
 
 # build
-if [ "${TARGETOS:0:3}" = "ios" ]; then
-    xcrun --sdk iphoneos make -j 2
-else
-    make -j 2
+eval $MAKE_COMMAND
+
+# run tests when possible
+if [ "$TARGET" = "native" ]; then
+
+   # TODO run arm-eabi tests under qemu
+   ./botan-test
+
+   if [ "$MODULES" != "min" ] && [ "$BUILD_MODE" = "shared" ]; then
+       python2 --version
+       python3 --version
+       LD_LIBRARY_PATH=. python2 src/python/botan.py
+       LD_LIBRARY_PATH=. python3 src/python/botan.py
+   fi
+
 fi
 
-if [ "$MODULES" != "min" ] && [ "${TARGETOS:0:3}" != "ios" ]; then
-    ./botan-test
-fi
-
-if [ "$MODULES" != "min" ] && [ "$BUILD_MODE" = "shared" ] && [ "$TARGETOS" = "desktop" ]
-then
-    python2 --version
-    python3 --version
-    LD_LIBRARY_PATH=. python2 src/python/botan.py
-    LD_LIBRARY_PATH=. python3 src/python/botan.py
-fi
-
+# test installation process
 make install
+
+ls -lR $INSTALL_DIR
