ea46993ee082d69b03e85ef4135d46316c75fe44
Jack Lloyd <lloyd@randombit.net>
2015-12-11 10:30:48 -0500

    Disable asio serial port support to avoid Darwin braindamage. GH #350

--- src/lib/utils/http_util/http_util.cpp
+++ src/lib/utils/http_util/http_util.cpp
@@ -12,7 +12,15 @@
 #include <sstream>
 
 #if defined(BOTAN_HAS_BOOST_ASIO)
-#include <boost/asio.hpp>
+
+  /*
+  * We don't need serial port support anyway, and asking for it
+  * causes macro conflicts with Darwin's termios.h when this
+  * file is included in the amalgamation. GH #350
+  */
+  #define BOOST_ASIO_DISABLE_SERIAL_PORT
+  #include <boost/asio.hpp>
+
 #endif
 
 namespace Botan {
