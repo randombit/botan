64caa9abd8699f3221977a6c4bcc21a5ee8c9040
Jack Lloyd <lloyd@randombit.net>
2016-01-01 10:06:16 -0500

    Inline SEED's sbox tables
    Simpler, and a bit faster also it seems (but not fast)

--- src/lib/block/seed/seed.h
+++ src/lib/block/seed/seed.h
@@ -27,14 +27,6 @@ class BOTAN_DLL SEED : public Block_Cipher_Fixed_Params<16, 16>
    private:
       void key_schedule(const byte[], size_t) override;
 
-      class G_FUNC
-         {
-         public:
-            u32bit operator()(u32bit) const;
-         private:
-            static const u32bit S0[256], S1[256], S2[256], S3[256];
-         };
-
       secure_vector<u32bit> K;
    };
 
