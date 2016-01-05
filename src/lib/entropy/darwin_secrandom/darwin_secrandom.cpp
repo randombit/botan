49ed7e1562a13bb3162500b42e4c35f3afb49366
Simon Warta <simon@kullo.net>
2016-01-05 17:38:32 +0100

    Check return value of SecRandomCopyBytes

--- src/lib/entropy/darwin_secrandom/darwin_secrandom.cpp
+++ src/lib/entropy/darwin_secrandom/darwin_secrandom.cpp
@@ -1,10 +1,12 @@
 /*
 * Darwin SecRandomCopyBytes EntropySource
 * (C) 2015 Daniel Seither (Kullo GmbH)
+* (C) 2016 Simon Warta (Kullo GmbH)
 *
 * Botan is released under the Simplified BSD License (see license.txt)
 */
 
+#include <botan/exceptn.h>
 #include <botan/internal/darwin_secrandom.h>
 #include <Security/Security.h>
 
@@ -17,9 +19,15 @@ void Darwin_SecRandom::poll(Entropy_Accumulator& accum)
    {
    secure_vector<byte>& buf = accum.get_io_buf(BOTAN_SYSTEM_RNG_POLL_REQUEST);
 
-   if(0 == SecRandomCopyBytes(kSecRandomDefault, buf.size(), buf.data()))
+   // kSecRandomDefault refers to a "cryptographically secure random number generator"
+   // See http://www.opensource.apple.com/source/libsecurity_keychain/libsecurity_keychain-55050.9/lib/SecRandom.h
+   if(0 == ::SecRandomCopyBytes(::kSecRandomDefault, buf.size(), buf.data()))
       {
       accum.add(buf.data(), buf.size(), BOTAN_ENTROPY_ESTIMATE_STRONG_RNG);
+      }
+   else
+      {
+      throw Exception("SecRandomCopyBytes failed with errno = " + std::to_string(errno));
       }
    }
 
