967fc669820cec66a1569bb458f6aca390f790b6
Jack Lloyd <lloyd@randombit.net>
2016-02-17 01:00:11 -0500

    Split up BigInt tests
    Removes a fair number of the power mod test cases for size and test time.
    Would be better to add a randomized or fuzzer-based test. Otherwise no
    change.

--- src/tests/test_bigint.cpp
+++ src/tests/test_bigint.cpp
@@ -1,5 +1,5 @@
 /*
-* (C) 2009,2015 Jack Lloyd
+* (C) 2009,2015,2016 Jack Lloyd
 *
 * Botan is released under the Simplified BSD License (see license.txt)
 */
@@ -17,6 +17,8 @@ namespace Botan_Tests {
 namespace {
 
 #if defined(BOTAN_HAS_BIGINT)
+
+using Botan::BigInt;
 
 class BigInt_Unit_Tests : public Test
    {
@@ -144,169 +146,288 @@ class BigInt_Unit_Tests : public Test
 
 BOTAN_REGISTER_TEST("bigint_unit", BigInt_Unit_Tests);
 
-class BigInt_KAT_Tests : public Text_Based_Test
+class BigInt_Add_Test : public Text_Based_Test
    {
    public:
-      BigInt_KAT_Tests() : Text_Based_Test("bigint.vec",
-                                           std::vector<std::string>{"Output"},
-                                           {"In1","In2","Input","Shift","Modulus","Value","Base","Exponent","IsPrime"})
-         {}
+      BigInt_Add_Test() : Text_Based_Test("bn/add.vec", {"In1","In2","Output"}) {}
 
-      Test::Result run_one_test(const std::string& algo, const VarMap& vars) override
+      Test::Result run_one_test(const std::string&, const VarMap& vars) override
          {
-         Test::Result result("BigInt " + algo);
+         Test::Result result("BigInt Addition");
 
          using Botan::BigInt;
 
-         if(algo == "Addition")
-            {
-            const BigInt a = get_req_bn(vars, "In1");
-            const BigInt b = get_req_bn(vars, "In2");
-            const BigInt c = get_req_bn(vars, "Output");
-            BigInt d = a + b;
+         const BigInt a = get_req_bn(vars, "In1");
+         const BigInt b = get_req_bn(vars, "In2");
+         const BigInt c = get_req_bn(vars, "Output");
+         BigInt d = a + b;
 
-            result.test_eq("a + b", a + b, c);
-            result.test_eq("b + a", b + a, c);
+         result.test_eq("a + b", a + b, c);
+         result.test_eq("b + a", b + a, c);
 
-            BigInt e = a;
-            e += b;
-            result.test_eq("a += b", e, c);
+         BigInt e = a;
+         e += b;
+         result.test_eq("a += b", e, c);
 
-            e = b;
-            e += a;
-            result.test_eq("b += a", e, c);
-            }
-         else if(algo == "Subtraction")
-            {
-            const BigInt a = get_req_bn(vars, "In1");
-            const BigInt b = get_req_bn(vars, "In2");
-            const BigInt c = get_req_bn(vars, "Output");
-            BigInt d = a - b;
-
-            result.test_eq("a - b", a - b, c);
-
-            BigInt e = a;
-            e -= b;
-            result.test_eq("a -= b", e, c);
-            }
-         else if(algo == "Multiplication")
-            {
-            const BigInt a = get_req_bn(vars, "In1");
-            const BigInt b = get_req_bn(vars, "In2");
-            const BigInt c = get_req_bn(vars, "Output");
-
-            result.test_eq("a * b", a * b, c);
-            result.test_eq("b * a", b * a, c);
-
-            BigInt e = a;
-            e *= b;
-            result.test_eq("a *= b", e, c);
-
-            e = b;
-            e *= a;
-            result.test_eq("b *= a", e, c);
-            }
-         else if(algo == "Square")
-            {
-            const BigInt a = get_req_bn(vars, "Input");
-            const BigInt c = get_req_bn(vars, "Output");
-
-            result.test_eq("a * a", a * a, c);
-            result.test_eq("sqr(a)", square(a), c);
-            }
-         else if(algo == "Division")
-            {
-            const BigInt a = get_req_bn(vars, "In1");
-            const BigInt b = get_req_bn(vars, "In2");
-            const BigInt c = get_req_bn(vars, "Output");
-
-            result.test_eq("a / b", a / b, c);
-
-            BigInt e = a;
-            e /= b;
-            result.test_eq("a /= b", e, c);
-            }
-         else if(algo == "Modulo")
-            {
-            const BigInt a = get_req_bn(vars, "In1");
-            const BigInt b = get_req_bn(vars, "In2");
-            const BigInt c = get_req_bn(vars, "Output");
-
-            result.test_eq("a % b", a % b, c);
-
-            BigInt e = a;
-            e %= b;
-            result.test_eq("a %= b", e, c);
-            }
-         else if(algo == "LeftShift")
-            {
-            const BigInt value = get_req_bn(vars, "Value");
-            const size_t shift = get_req_bn(vars, "Shift").to_u32bit();
-            const BigInt output = get_req_bn(vars, "Output");
-
-            result.test_eq("a << s", value << shift, output);
-
-            BigInt e = value;
-            e <<= shift;
-            result.test_eq("a <<= s", e, output);
-            }
-         else if(algo == "RightShift")
-            {
-            const BigInt value = get_req_bn(vars, "Value");
-            const size_t shift = get_req_bn(vars, "Shift").to_u32bit();
-            const BigInt output = get_req_bn(vars, "Output");
-
-            result.test_eq("a >> s", value >> shift, output);
-
-            BigInt e = value;
-            e >>= shift;
-            result.test_eq("a >>= s", e, output);
-            }
-         else if(algo == "ModExp")
-            {
-            const BigInt value = get_req_bn(vars, "Base");
-            const BigInt exponent = get_req_bn(vars, "Exponent");
-            const BigInt modulus = get_req_bn(vars, "Modulus");
-            const BigInt output = get_req_bn(vars, "Output");
-
-            result.test_eq("power_mod", Botan::power_mod(value, exponent, modulus), output);
-            }
-         else if(algo == "PrimeTest")
-            {
-            const BigInt value = get_req_bn(vars, "Value");
-            const bool v_is_prime = get_req_sz(vars, "IsPrime") > 0;
-
-            result.test_eq("value", Botan::is_prime(value, Test::rng()), v_is_prime);
-            }
-         else if(algo == "RESSOL")
-            {
-            const Botan::BigInt a = get_req_bn(vars, "Input");
-            const Botan::BigInt p = get_req_bn(vars, "Modulus");
-            const Botan::BigInt exp = get_req_bn(vars, "Output");
-
-            const Botan::BigInt a_sqrt = Botan::ressol(a, p);
-
-            result.test_eq("result", a_sqrt, exp);
-
-            if(a_sqrt > 1)
-               {
-               const Botan::BigInt a_sqrt2 = (a_sqrt*a_sqrt) % p;
-               result.test_eq("square correct", a_sqrt2, a);
-               }
-
-            return result;
-            }
-         else
-            {
-            result.test_failure("Unknown BigInt algorithm " + algo);
-            }
+         e = b;
+         e += a;
+         result.test_eq("b += a", e, c);
 
          return result;
          }
 
    };
 
-BOTAN_REGISTER_TEST("bigint_kat", BigInt_KAT_Tests);
+BOTAN_REGISTER_TEST("bn_add", BigInt_Add_Test);
+
+class BigInt_Sub_Test : public Text_Based_Test
+   {
+   public:
+      BigInt_Sub_Test() : Text_Based_Test("bn/sub.vec", {"In1","In2","Output"}) {}
+
+      Test::Result run_one_test(const std::string&, const VarMap& vars) override
+         {
+         Test::Result result("BigInt Subtraction");
+
+         const BigInt a = get_req_bn(vars, "In1");
+         const BigInt b = get_req_bn(vars, "In2");
+         const BigInt c = get_req_bn(vars, "Output");
+
+         BigInt d = a - b;
+
+         result.test_eq("a - b", a - b, c);
+
+         BigInt e = a;
+         e -= b;
+         result.test_eq("a -= b", e, c);
+
+         return result;
+         }
+   };
+
+BOTAN_REGISTER_TEST("bn_sub", BigInt_Sub_Test);
+
+class BigInt_Mul_Test : public Text_Based_Test
+   {
+   public:
+      BigInt_Mul_Test() : Text_Based_Test("bn/mul.vec", {"In1","In2","Output"}) {}
+
+      Test::Result run_one_test(const std::string&, const VarMap& vars) override
+         {
+         Test::Result result("BigInt Multiply");
+
+         const BigInt a = get_req_bn(vars, "In1");
+         const BigInt b = get_req_bn(vars, "In2");
+         const BigInt c = get_req_bn(vars, "Output");
+
+         result.test_eq("a * b", a * b, c);
+         result.test_eq("b * a", b * a, c);
+
+         BigInt e = a;
+         e *= b;
+         result.test_eq("a *= b", e, c);
+
+         e = b;
+         e *= a;
+         result.test_eq("b *= a", e, c);
+
+         return result;
+         }
+   };
+
+BOTAN_REGISTER_TEST("bn_mul", BigInt_Mul_Test);
+
+class BigInt_Sqr_Test : public Text_Based_Test
+   {
+   public:
+      BigInt_Sqr_Test() : Text_Based_Test("bn/sqr.vec", {"Input","Output"}) {}
+
+      Test::Result run_one_test(const std::string&, const VarMap& vars) override
+         {
+         Test::Result result("BigInt Square");
+
+         const BigInt input = get_req_bn(vars, "Input");
+         const BigInt output = get_req_bn(vars, "Output");
+
+         result.test_eq("a * a", input * input, output);
+         result.test_eq("sqr(a)", square(input), output);
+
+         return result;
+         }
+   };
+
+BOTAN_REGISTER_TEST("bn_sqr", BigInt_Sqr_Test);
+
+class BigInt_Div_Test : public Text_Based_Test
+   {
+   public:
+      BigInt_Div_Test() : Text_Based_Test("bn/divide.vec", {"In1","In2","Output"}) {}
+
+      Test::Result run_one_test(const std::string&, const VarMap& vars) override
+         {
+         Test::Result result("BigInt Divide");
+
+         const BigInt a = get_req_bn(vars, "In1");
+         const BigInt b = get_req_bn(vars, "In2");
+         const BigInt c = get_req_bn(vars, "Output");
+
+         result.test_eq("a / b", a / b, c);
+
+         BigInt e = a;
+         e /= b;
+         result.test_eq("a /= b", e, c);
+
+         return result;
+         }
+   };
+
+BOTAN_REGISTER_TEST("bn_div", BigInt_Div_Test);
+
+class BigInt_Mod_Test : public Text_Based_Test
+   {
+   public:
+      BigInt_Mod_Test() : Text_Based_Test("bn/mod.vec", {"In1","In2","Output"}) {}
+
+      Test::Result run_one_test(const std::string&, const VarMap& vars) override
+         {
+         Test::Result result("BigInt Mod");
+
+         const BigInt a = get_req_bn(vars, "In1");
+         const BigInt b = get_req_bn(vars, "In2");
+         const BigInt c = get_req_bn(vars, "Output");
+
+         result.test_eq("a % b", a % b, c);
+
+         BigInt e = a;
+         e %= b;
+         result.test_eq("a %= b", e, c);
+
+         return result;
+         }
+   };
+
+BOTAN_REGISTER_TEST("bn_mod", BigInt_Mod_Test);
+
+class BigInt_Lshift_Test : public Text_Based_Test
+   {
+   public:
+      BigInt_Lshift_Test() : Text_Based_Test("bn/lshift.vec", {"Value","Shift","Output"}) {}
+
+      Test::Result run_one_test(const std::string&, const VarMap& vars) override
+         {
+         Test::Result result("BigInt Lshift");
+
+         const BigInt value = get_req_bn(vars, "Value");
+         const size_t shift = get_req_bn(vars, "Shift").to_u32bit();
+         const BigInt output = get_req_bn(vars, "Output");
+
+         result.test_eq("a << s", value << shift, output);
+
+         BigInt e = value;
+         e <<= shift;
+         result.test_eq("a <<= s", e, output);
+
+         return result;
+         }
+   };
+
+BOTAN_REGISTER_TEST("bn_lshift", BigInt_Lshift_Test);
+
+class BigInt_Rshift_Test : public Text_Based_Test
+   {
+   public:
+      BigInt_Rshift_Test() : Text_Based_Test("bn/rshift.vec", {"Value","Shift","Output"}) {}
+
+      Test::Result run_one_test(const std::string&, const VarMap& vars) override
+         {
+         Test::Result result("BigInt Rshift");
+
+         const BigInt value = get_req_bn(vars, "Value");
+         const size_t shift = get_req_bn(vars, "Shift").to_u32bit();
+         const BigInt output = get_req_bn(vars, "Output");
+
+         result.test_eq("a >> s", value >> shift, output);
+
+         BigInt e = value;
+         e >>= shift;
+         result.test_eq("a >>= s", e, output);
+
+         return result;
+         }
+   };
+
+BOTAN_REGISTER_TEST("bn_rshift", BigInt_Rshift_Test);
+
+class BigInt_Powmod_Test : public Text_Based_Test
+   {
+   public:
+      BigInt_Powmod_Test() : Text_Based_Test("bn/powmod.vec", {"Base","Exponent","Modulus","Output"}) {}
+
+      Test::Result run_one_test(const std::string&, const VarMap& vars) override
+         {
+         Test::Result result("BigInt Powmod");
+
+         const BigInt value = get_req_bn(vars, "Base");
+         const BigInt exponent = get_req_bn(vars, "Exponent");
+         const BigInt modulus = get_req_bn(vars, "Modulus");
+         const BigInt output = get_req_bn(vars, "Output");
+
+         result.test_eq("power_mod", Botan::power_mod(value, exponent, modulus), output);
+
+         return result;
+         }
+   };
+
+BOTAN_REGISTER_TEST("bn_powmod", BigInt_Powmod_Test);
+
+class BigInt_IsPrime_Test : public Text_Based_Test
+   {
+   public:
+      BigInt_IsPrime_Test() : Text_Based_Test("bn/isprime.vec", {"Value","IsPrime"}) {}
+
+      Test::Result run_one_test(const std::string&, const VarMap& vars) override
+         {
+         Test::Result result("BigInt IsPrime");
+
+         const BigInt value = get_req_bn(vars, "Value");
+         const bool v_is_prime = get_req_sz(vars, "IsPrime") > 0;
+
+         result.test_eq("value", Botan::is_prime(value, Test::rng()), v_is_prime);
+
+         return result;
+         }
+   };
+
+BOTAN_REGISTER_TEST("bn_isprime", BigInt_IsPrime_Test);
+
+class BigInt_Ressol_Test : public Text_Based_Test
+   {
+   public:
+      BigInt_Ressol_Test() : Text_Based_Test("bn/ressol.vec", {"Input","Modulus","Output"}) {}
+
+      Test::Result run_one_test(const std::string&, const VarMap& vars) override
+         {
+         Test::Result result("BigInt Ressol");
+
+         const Botan::BigInt a = get_req_bn(vars, "Input");
+         const Botan::BigInt p = get_req_bn(vars, "Modulus");
+         const Botan::BigInt exp = get_req_bn(vars, "Output");
+
+         const Botan::BigInt a_sqrt = Botan::ressol(a, p);
+
+         result.test_eq("result", a_sqrt, exp);
+
+         if(a_sqrt > 1)
+            {
+            const Botan::BigInt a_sqrt2 = (a_sqrt*a_sqrt) % p;
+            result.test_eq("square correct", a_sqrt2, a);
+            }
+
+         return result;
+         }
+   };
+
+BOTAN_REGISTER_TEST("bn_ressol", BigInt_Ressol_Test);
 
 #endif
 
