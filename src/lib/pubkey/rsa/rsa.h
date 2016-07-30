c44fbd04739f530eaae8005bb72ea685034430fc
Jack Lloyd <lloyd@randombit.net>
2016-07-30 12:18:30 -0400

    Remove IF_Scheme_{Public,Private}Key
    With the removal of Rabin-Williams, RSA is the only remaining subclass,
    And it's very unlikely any new integer factorization based scheme would be
    added in the future.

--- src/lib/pubkey/rsa/rsa.h
+++ src/lib/pubkey/rsa/rsa.h
@@ -1,6 +1,6 @@
 /*
 * RSA
-* (C) 1999-2008 Jack Lloyd
+* (C) 1999-2008,2016 Jack Lloyd
 *
 * Botan is released under the Simplified BSD License (see license.txt)
 */
@@ -8,23 +8,19 @@
 #ifndef BOTAN_RSA_H__
 #define BOTAN_RSA_H__
 
-#include <botan/if_algo.h>
-
+#include <botan/bigint.h>
+#include <botan/x509_key.h>
 
 namespace Botan {
 
 /**
 * RSA Public Key
 */
-class BOTAN_DLL RSA_PublicKey : public virtual IF_Scheme_PublicKey
+class BOTAN_DLL RSA_PublicKey : public virtual Public_Key
    {
    public:
-      std::string algo_name() const override { return "RSA"; }
-
       RSA_PublicKey(const AlgorithmIdentifier& alg_id,
-                    const secure_vector<byte>& key_bits) :
-         IF_Scheme_PublicKey(alg_id, key_bits)
-         {}
+                    const secure_vector<byte>& key_bits);
 
       /**
       * Create a RSA_PublicKey
@@ -32,26 +28,45 @@ class BOTAN_DLL RSA_PublicKey : public virtual IF_Scheme_PublicKey
       * @arg e the exponent
       */
       RSA_PublicKey(const BigInt& n, const BigInt& e) :
-         IF_Scheme_PublicKey(n, e)
-         {}
+         m_n(n), m_e(e) {}
+
+      std::string algo_name() const override { return "RSA"; }
+
+      bool check_key(RandomNumberGenerator& rng, bool) const override;
+
+      AlgorithmIdentifier algorithm_identifier() const override;
+
+      std::vector<byte> x509_subject_public_key() const override;
+
+      /**
+      * @return public modulus
+      */
+      const BigInt& get_n() const { return m_n; }
+
+      /**
+      * @return public exponent
+      */
+      const BigInt& get_e() const { return m_e; }
+
+      size_t max_input_bits() const override { return (m_n.bits() - 1); }
+
+      size_t estimated_strength() const override;
 
    protected:
       RSA_PublicKey() {}
+
+      BigInt m_n, m_e;
    };
 
 /**
 * RSA Private Key
 */
-class BOTAN_DLL RSA_PrivateKey : public RSA_PublicKey,
-                                 public IF_Scheme_PrivateKey
+class BOTAN_DLL RSA_PrivateKey : public Private_Key, public RSA_PublicKey
    {
    public:
-      bool check_key(RandomNumberGenerator& rng, bool) const override;
-
       RSA_PrivateKey(const AlgorithmIdentifier& alg_id,
                      const secure_vector<byte>& key_bits,
-                     RandomNumberGenerator& rng) :
-         IF_Scheme_PrivateKey(rng, alg_id, key_bits) {}
+                     RandomNumberGenerator& rng);
 
       /**
       * Construct a private key from the specified parameters.
@@ -68,8 +83,7 @@ class BOTAN_DLL RSA_PrivateKey : public RSA_PublicKey,
       RSA_PrivateKey(RandomNumberGenerator& rng,
                      const BigInt& p, const BigInt& q,
                      const BigInt& e, const BigInt& d = 0,
-                     const BigInt& n = 0) :
-         IF_Scheme_PrivateKey(rng, p, q, e, d, n) {}
+                     const BigInt& n = 0);
 
       /**
       * Create a new private key with the specified bit length
@@ -79,6 +93,34 @@ class BOTAN_DLL RSA_PrivateKey : public RSA_PublicKey,
       */
       RSA_PrivateKey(RandomNumberGenerator& rng,
                      size_t bits, size_t exp = 65537);
+
+      bool check_key(RandomNumberGenerator& rng, bool) const override;
+
+      /**
+      * Get the first prime p.
+      * @return prime p
+      */
+      const BigInt& get_p() const { return m_p; }
+
+      /**
+      * Get the second prime q.
+      * @return prime q
+      */
+      const BigInt& get_q() const { return m_q; }
+
+      /**
+      * Get d with exp * d = 1 mod (p - 1, q - 1).
+      * @return d
+      */
+      const BigInt& get_d() const { return m_d; }
+
+      const BigInt& get_c() const { return m_c; }
+      const BigInt& get_d1() const { return m_d1; }
+      const BigInt& get_d2() const { return m_d2; }
+
+      secure_vector<byte> pkcs8_private_key() const override;
+   private:
+      BigInt m_d, m_p, m_q, m_d1, m_d2, m_c;
    };
 
 }
