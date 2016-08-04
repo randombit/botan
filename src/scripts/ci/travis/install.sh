5f3a96204a4888367af45828bfe05cc05dd6ef95
Daniel Neus <d.neus@sirrix.com>
2016-08-04 19:25:28 +0200

    SonarQube integration / Travis CI changes
    Travis CI changes:
    - Run SonarQube analysis for commits on master and in pull requests
    - Upgrade Travis CI build image to Ubuntu 14.04 and use standard system toolchain
    - Update lcov to 1.12 for coverage
    - Set git clone depth to 5 commits
    - Use 'builtin' ccache Travis CI addon
    - Add SonarQube badge to readme
    - No longer necessary to overwrite CXX variable in build.sh when using gcc

--- src/scripts/ci/travis/install.sh
+++ src/scripts/ci/travis/install.sh
@@ -3,14 +3,19 @@ set -ev
 which shellcheck > /dev/null && shellcheck "$0" # Run shellcheck on this if available
 
 if [ "$BUILD_MODE" = "coverage" ]; then
-    wget http://ftp.de.debian.org/debian/pool/main/l/lcov/lcov_1.11.orig.tar.gz
-    tar -xvf lcov_1.11.orig.tar.gz
+    wget http://ftp.de.debian.org/debian/pool/main/l/lcov/lcov_1.12.orig.tar.gz
+    tar -xvf lcov_1.12.orig.tar.gz
     export PREFIX="/tmp"
-    make -C lcov-1.11/ install
+    make -C lcov-1.12/ install
 
     pip install --user coverage
 
     pip install --user codecov
+fi
+
+if [ "$BUILD_MODE" = "sonarqube" ]; then
+    curl -LsS https://sonarqube.com/static/cpp/build-wrapper-linux-x86.zip > build-wrapper-linux-x86.zip
+    unzip build-wrapper-linux-x86.zip
 fi
 
 if [ "$TRAVIS_OS_NAME" = "osx" ] && [ "$TARGETOS" != "ios" ]; then
