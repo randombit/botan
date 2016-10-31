909490d3a2a8d96732c6428f0cf6e80b10afd560
Jack Lloyd <jack@randombit.net>
2016-10-31 00:43:36 -0400

    More fixes for the fuzzer setup
    [ci skip]

--- src/extra_tests/fuzzers/setup.sh
+++ src/extra_tests/fuzzers/setup.sh
@@ -6,11 +6,17 @@ mkdir corpus
 
 CFG_FLAGS="--with-debug-info --unsafe-fuzzer-mode --minimized-build --enable-modules=tls,chacha20poly1305,ocb,ccm,system_rng,auto_rng"
 
+if [ ! -d libFuzzer ]; then
+    svn co http://llvm.org/svn/llvm-project/llvm/trunk/lib/Fuzzer libFuzzer
+fi
+
+exit
+
 # Just need the static lib, not CLI or tests
 
-../../../configure.py $CFG_FLAGS --with-build-dir=afl --cc=clang --cc-bin='afl-clang-fast++'
-make -f afl/Makefile afl/libbotan-1.11.a -j2
+../../../configure.py $CFG_FLAGS --with-build-dir=afl-build --cc=clang --cc-bin='afl-clang-fast++'
+make -f afl-build/Makefile afl-build/libbotan-1.11.a -j8
 
 CLANG_COV_FLAGS="-fsanitize=address,undefined -fsanitize-coverage=edge,indirect-calls,8bit-counters -fno-sanitize-recover=undefined"
-../../../configure.py $CFG_FLAGS --with-build-dir=llvm --cc=clang "--cc-abi-flags=$CLANG_COV_FLAGS"
-make -f llvm/Makefile llvm/libbotan-1.11.a -j2
+../../../configure.py $CFG_FLAGS --with-build-dir=llvm-build --cc=clang "--cc-abi-flags=$CLANG_COV_FLAGS"
+make -f llvm-build/Makefile llvm-build/libbotan-1.11.a -j8
