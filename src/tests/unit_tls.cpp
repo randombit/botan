0b2da26e7205ee6f0a79d8df523e97a7341ea02b
Simon Cogliani <simon.cogliani@tanker.io>
2016-11-09 00:51:07 +0100

    WIP: fix unit_tls.cpp

--- src/tests/unit_tls.cpp
+++ src/tests/unit_tls.cpp
@@ -141,6 +141,7 @@ class Credentials_Manager_Test : public Botan::Credentials_Manager
       bool m_provides_client_certs;
    };
 
+#if defined(BOTAN_HAS_SECP256R1)
 Botan::Credentials_Manager*
 create_creds(Botan::RandomNumberGenerator& rng,
              bool with_client_certs = false)
@@ -191,6 +192,7 @@ create_creds(Botan::RandomNumberGenerator& rng,
    cmt->m_provides_client_certs = with_client_certs;
    return cmt;
    }
+#endif
 
 std::function<void (const byte[], size_t)> queue_inserter(std::vector<byte>& q)
    {
@@ -1021,6 +1023,8 @@ class TLS_Unit_Tests : public Test
 #if defined(BOTAN_HOUSE_ECC_CURVE_NAME)
          test_modern_versions(results, *client_ses, *server_ses, *creds, "ECDH", "AES-128/GCM", "AEAD",
                                        { { "ecc_curves", BOTAN_HOUSE_ECC_CURVE_NAME } });
+#endif
+
 #endif
          return results;
          }
