97829faaf53c51842c206963c00918423a0a0a5b
Jack Lloyd <jack@randombit.net>
2016-11-26 11:38:25 -0500

    Tweak lcov script

--- src/scripts/lcov.sh
+++ src/scripts/lcov.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-./configure.py --with-debug-info --with-coverage-info --no-optimizations --with-bzip2 --with-lzma --with-sqlite --with-zlib --with-pkcs11 --with-openssl --with-sqlite3
+./configure.py --with-debug-info --with-coverage-info --with-bzip2 --with-lzma --with-sqlite --with-zlib --with-pkcs11 --with-openssl --with-sqlite3
 
-make -l4 -j8 -k
-./botan-test --pkcs11-lib=/usr/lib/libsofthsm2.so
+make -l2 -j4 -k
+./botan-test --pkcs11-lib=/usr/lib/libsofthsm2.so --run-online-tests
 
-lcov --rc lcov_branch_coverage=1 --capture --directory . --output-file coverage.info.raw
-lcov --remove coverage.info.raw '/usr/*' --output-file coverage.info
-genhtml --rc lcov_branch_coverage=1 coverage.info --output-directory lcov-out
+#LCOV_OPTIONS="--rc lcov_branch_coverage=1"
+LCOV_OPTIONS=""
+
+lcov $LCOV_OPTIONS --capture --directory . --output-file coverage.info.raw
+lcov $LCOV_OPTIONS  --remove coverage.info.raw '/usr/*' --output-file coverage.info
+genhtml $LCOV_OPTIONS coverage.info --output-directory lcov-out
