04da65d697dd0e967153cf0c24302107591a13f5
Jack Lloyd <jack@randombit.net>
2016-12-17 12:17:27 -0500

    Add OCSP fuzzer
    Some attempts at reducing overhead in ECC math tests, unclear if
    really changed anything for my machine but probably can't hurt.
    
    Fix LLVM build flags
    
    [ci skip]

--- src/extra_tests/fuzzers/jigs/ecc_helper.h
+++ src/extra_tests/fuzzers/jigs/ecc_helper.h
@@ -33,14 +33,16 @@ inline std::ostream& operator<<(std::ostream& o, const PointGFp& point)
    return o;
    }
 
-void check_ecc_math(const EC_Group& group, const uint8_t in[], size_t len)
+void check_ecc_math(const EC_Group& group,
+                    const uint8_t in[], size_t len)
    {
+   // These depend only on the group, which is also static
+   static const Botan::PointGFp base_point = group.get_base_point();
+   static Botan::Blinded_Point_Multiply blind(base_point, group.get_order(), 4);
+
    const size_t hlen = len / 2;
    const BigInt a = BigInt::decode(in, hlen);
    const BigInt b = BigInt::decode(in + hlen, len - hlen);
-
-   const Botan::PointGFp& base_point = group.get_base_point();
-   const Botan::BigInt& group_order = group.get_order();
 
    const Botan::BigInt c = a + b;
 
@@ -51,10 +53,7 @@ void check_ecc_math(const EC_Group& group, const uint8_t in[], size_t len)
    const Botan::PointGFp A1 = P + Q;
    const Botan::PointGFp A2 = Q + P;
 
-   FUZZER_ASSERT_EQUAL(A1, R);
-   FUZZER_ASSERT_EQUAL(A2, R);
-
-   Botan::Blinded_Point_Multiply blind(base_point, group_order, 4);
+   FUZZER_ASSERT_EQUAL(A1, A2);
 
    const Botan::PointGFp P1 = blind.blinded_multiply(a, fuzzer_rng());
    const Botan::PointGFp Q1 = blind.blinded_multiply(b, fuzzer_rng());
@@ -63,8 +62,8 @@ void check_ecc_math(const EC_Group& group, const uint8_t in[], size_t len)
    const Botan::PointGFp S1 = P1 + Q1;
    const Botan::PointGFp S2 = Q1 + P1;
 
-   FUZZER_ASSERT_EQUAL(S1, R1);
-   FUZZER_ASSERT_EQUAL(S2, R1);
+   FUZZER_ASSERT_EQUAL(S1, S2);
+   FUZZER_ASSERT_EQUAL(S1, A1);
    }
 
 #endif
