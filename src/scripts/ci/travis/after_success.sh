39bcd9d80667897fe9eb71af3d65e1da8993d071
Jack Lloyd <jack@randombit.net>
2017-01-06 16:19:56 -0500

    Fix install path for lcov 1.13

--- src/scripts/ci/travis/after_success.sh
+++ src/scripts/ci/travis/after_success.sh
@@ -4,9 +4,9 @@ which shellcheck > /dev/null && shellcheck "$0" # Run shellcheck on this if avai
 
 if [ "$BUILD_MODE" = "coverage" ]; then
     GCOV="/usr/bin/gcov-4.8"
-    /tmp/usr/bin/lcov --gcov-tool "$GCOV" --directory . --capture --output-file $(pwd)/coverage.info.in
-    /tmp/usr/bin/lcov --gcov-tool "$GCOV" --remove $(pwd)/coverage.info.in 'tests/*' '/usr/*' --output-file $(pwd)/coverage.info
-    /tmp/usr/bin/lcov --gcov-tool "$GCOV" --list $(pwd)/coverage.info
+    /tmp/bin/lcov --gcov-tool "$GCOV" --directory . --capture --output-file $(pwd)/coverage.info.in
+    /tmp/bin/lcov --gcov-tool "$GCOV" --remove $(pwd)/coverage.info.in 'tests/*' '/usr/*' --output-file $(pwd)/coverage.info
+    /tmp/bin/lcov --gcov-tool "$GCOV" --list $(pwd)/coverage.info
 
     LD_LIBRARY_PATH=. coverage run --branch src/python/botan.py
 
