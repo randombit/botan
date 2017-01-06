fd230f98a287ccbcc237947d8d5e984231789629
Jack Lloyd <jack@randombit.net>
2017-01-06 11:42:03 -0500

    Update lcov version to 1.13
    It seems the lcov source tarball for 1.12 has been removed from all
    Debian mirrors. Hopefully 1.13 is a trouble free upgrade.

--- src/scripts/ci/travis/install.sh
+++ src/scripts/ci/travis/install.sh
@@ -3,10 +3,10 @@ set -ev
 which shellcheck > /dev/null && shellcheck "$0" # Run shellcheck on this if available
 
 if [ "$BUILD_MODE" = "coverage" ]; then
-    wget http://ftp.de.debian.org/debian/pool/main/l/lcov/lcov_1.12.orig.tar.gz
-    tar -xvf lcov_1.12.orig.tar.gz
+    wget http://ftp.de.debian.org/debian/pool/main/l/lcov/lcov_1.13.orig.tar.gz
+    tar -xvf lcov_1.13.orig.tar.gz
     export PREFIX="/tmp"
-    make -C lcov-1.12/ install
+    make -C lcov-1.13/ install
 
     pip install --user coverage
 
