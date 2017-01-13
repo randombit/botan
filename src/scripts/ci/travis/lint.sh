d0da451d27dc053d12a64e174feea2b70f74dedd
Jack Lloyd <jack@randombit.net>
2017-01-12 22:45:59 -0500

    Run pylint on botan.py in Travis

--- src/scripts/ci/travis/lint.sh
+++ src/scripts/ci/travis/lint.sh
@@ -2,7 +2,12 @@
 set -ev
 which shellcheck > /dev/null && shellcheck "$0" # Run shellcheck on this if available
 
-echo "travis_fold:start:pylint"
+echo "travis_fold:start:pylint_configure"
 python3 -m pylint configure.py
 python2 -m pylint configure.py
-echo "travis_fold:end:pylint"
+echo "travis_fold:end:pylint_configure"
+
+echo "travis_fold:start:pylint_botanpy"
+python3 -m pylint src/python/botan.py
+python2 -m pylint src/python/botan.py
+echo "travis_fold:end:pylint_botanpy"
