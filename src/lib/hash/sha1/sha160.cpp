455a39f70e6de8376f78f318f04d07af7d245be3
Jack Lloyd <jack@randombit.net>
2017-01-17 16:28:20 -0500

    Add ARMv8 SHA-1 support
    Based on patch from Jeffrey Walton in GH #840
    
    Only tested in qemu so far.

--- src/lib/hash/sha1/sha160.cpp
+++ src/lib/hash/sha1/sha160.cpp
@@ -68,6 +68,13 @@ void SHA_160::compress_n(const uint8_t input[], size_t blocks)
       }
 #endif
 
+#if defined(BOTAN_HAS_SHA1_ARMV8)
+   if(CPUID::has_arm_sha1())
+      {
+      return sha1_armv8_compress_n(m_digest, input, blocks);
+      }
+#endif
+
 #if defined(BOTAN_HAS_SHA1_SSE2)
    if(CPUID::has_sse2())
       {
