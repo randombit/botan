9dcf7c78c04c954cd1b49e80feb7478486c13531
Jack Lloyd <jack@randombit.net>
2017-01-17 13:25:01 -0500

    Fixes for 32-bit ARM

--- src/lib/utils/cpuid.cpp
+++ src/lib/utils/cpuid.cpp
@@ -164,21 +164,52 @@ uint64_t arm_detect_cpu_features(size_t* cache_line_size)
 #if defined(BOTAN_TARGET_OS_HAS_GETAUXVAL)
    errno = 0;
 
-#if defined(BOTAN_TARGET_ARCH_IS_ARM32)
-   const unsigned long hwcap = ::getauxval(AT_HWCAP2);
-#elif defined(BOTAN_TARGET_ARCH_IS_ARM64)
-   const unsigned long hwcap = ::getauxval(AT_HWCAP);
-#endif
+   /*
+   * On systems with getauxval these bits should normally be defined
+   * in bits/auxv.h but some buggy? glibc installs seem to miss them.
+   * These following values are all fixed, for the Linux ELF format,
+   * so we just hardcode them in ARM_hwcap_bit enum.
+   */
 
-   if(hwcap & HWCAP_ASIMD)
+   enum ARM_hwcap_bit {
+#if defined(BOTAN_TARGET_ARCH_IS_ARM32)
+      NEON_bit  = (1 << 12),
+      AES_bit   = (1 << 0),
+      PMULL_bit = (1 << 1),
+      SHA1_bit  = (1 << 2),
+      SHA2_bit  = (1 << 3),
+
+      ARCH_hwcap_neon   = 16, // AT_HWCAP
+      ARCH_hwcap_crypto = 26, // AT_HWCAP2
+#elif defined(BOTAN_TARGET_ARCH_IS_ARM64)
+      NEON_bit  = (1 << 1),
+      AES_bit   = (1 << 3),
+      PMULL_bit = (1 << 4),
+      SHA1_bit  = (1 << 5),
+      SHA2_bit  = (1 << 6),
+
+      ARCH_hwcap_neon   = 16, // AT_HWCAP
+      ARCH_hwcap_crypto = 16, // AT_HWCAP
+#endif
+   };
+
+   const unsigned long hwcap_neon = ::getauxval(ARM_hwcap_bit::ARCH_hwcap_neon);
+   if(hwcap_neon & ARM_hwcap_bit::NEON_bit)
       detected_features |= CPUID::CPUID_ARM_NEON_BIT;
-   if(hwcap & HWCAP_AES)
+
+   /*
+   On aarch64 this ends up calling getauxval twice with AT_HWCAP
+   This doesn't seem worth optimizing this out, since getauxval is
+   just reading a field in the ELF header.
+   */
+   const unsigned long hwcap_crypto = ::getauxval(ARM_hwcap_bit::ARCH_hwcap_crypto);
+   if(hwcap_crypto & ARM_hwcap_bit::AES_bit)
       detected_features |= CPUID::CPUID_ARM_AES_BIT;
-   if(hwcap & HWCAP_PMULL)
+   if(hwcap_crypto & ARM_hwcap_bit::PMULL_bit)
       detected_features |= CPUID::CPUID_ARM_PMULL_BIT;
-   if(hwcap & HWCAP_SHA1)
+   if(hwcap_crypto & ARM_hwcap_bit::SHA1_bit)
       detected_features |= CPUID::CPUID_ARM_SHA1_BIT;
-   if(hwcap & HWCAP_SHA2)
+   if(hwcap_crypto & ARM_hwcap_bit::SHA2_bit)
       detected_features |= CPUID::CPUID_ARM_SHA2_BIT;
 
    const unsigned long dcache_line = ::getauxval(AT_DCACHEBSIZE);
