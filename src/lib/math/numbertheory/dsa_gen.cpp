78d8fb3d77cc0054229f951324f77610b91e0e9a
Never <neverhub@users.noreply.github.com>
2017-02-13 14:00:39 +0100

    increase miller-rabin iterations for dsa primes (FIPS-186-4)

--- src/lib/math/numbertheory/dsa_gen.cpp
+++ src/lib/math/numbertheory/dsa_gen.cpp
@@ -81,7 +81,7 @@ bool generate_dsa_primes(RandomNumberGenerator& rng,
    q.set_bit(qbits-1);
    q.set_bit(0);
 
-   if(!is_prime(q, rng))
+   if(!is_prime(q, rng, 126))
       return false;
 
    const size_t n = (pbits-1) / (HASH_SIZE * 8),
@@ -107,7 +107,7 @@ bool generate_dsa_primes(RandomNumberGenerator& rng,
 
          p = X - (X % (2*q) - 1);
 
-         if(p.bits() == pbits && is_prime(p, rng))
+         if(p.bits() == pbits && is_prime(p, rng, 126))
             return true;
          }
       }
