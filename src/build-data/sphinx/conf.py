213a0fbb267c5f8e3a5a04f039abe57120795c9a
René Korthaus <r.korthaus@sirrix.com>
2017-03-15 20:33:57 +0100

    Set latex papersize to a4

--- src/build-data/sphinx/conf.py
+++ src/build-data/sphinx/conf.py
@@ -176,11 +176,10 @@ htmlhelp_basename = 'botandoc'
 
 # -- Options for LaTeX output --------------------------------------------------
 
-# The paper size ('letter' or 'a4').
-#latex_paper_size = 'letter'
-
-# The font size ('10pt', '11pt' or '12pt').
-#latex_font_size = '10pt'
+# Various latex options, like papersize and pointsize
+latex_elements = {
+  'papersize': 'a4paper',
+}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
