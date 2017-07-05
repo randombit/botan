c0a46d16717aa2c6e760517a220fdbdbdbf6af8e
Zoltan Gyarmati <mr.zoltan.gyarmati@gmail.com>
2017-07-06 01:41:49 +0200

    add unit test for the destdir prepending logic

--- src/scripts/python_uniitests.py
+++ src/scripts/python_uniitests.py
@@ -10,12 +10,15 @@ Requires Python 3.
 Botan is released under the Simplified BSD License (see license.txt)
 """
 
+import os
 import sys
 import unittest
 
+import install
+
 sys.path.append("../..") # Botan repo root
 from configure import ModulesChooser # pylint: disable=wrong-import-position
-
+from install import prepend_destdir # pylint: disable=wrong-import-position
 
 class ModulesChooserResolveDependencies(unittest.TestCase):
     def test_base(self):
@@ -173,6 +176,34 @@ class ModulesChooserResolveDependencies(unittest.TestCase):
         ok, modules = ModulesChooser.resolve_dependencies(available_modules, table, "G")
         self.assertTrue(ok)
         self.assertEqual(modules, set(["G", "A", "C", "E"]))
+
+
+class PrependDestdir(unittest.TestCase):
+    def test_base(self):
+        os.environ["DESTDIR"] = "/"
+        self.assertEqual(prepend_destdir("/home/me"), "/home/me")
+        self.assertEqual(prepend_destdir("relative_path"), "/relative_path")
+        self.assertEqual(prepend_destdir("./relative_path"), "/relative_path")
+        self.assertEqual(prepend_destdir("relative/sub"), "/relative/sub")
+
+        self.assertEqual(prepend_destdir("/home/me/"), "/home/me")
+        self.assertEqual(prepend_destdir("relative_path/"), "/relative_path")
+
+        self.assertEqual(prepend_destdir("/home/me/../me2"), "/home/me2")
+        self.assertEqual(prepend_destdir("relative/sub/../sub2"), "/relative/sub2")
+
+        os.environ["DESTDIR"] = "/opt"
+        self.assertEqual(prepend_destdir("/home/me"), "/opt/home/me")
+        self.assertEqual(prepend_destdir("relative_path"), "/opt/relative_path")
+        self.assertEqual(prepend_destdir("./relative_path"), "/opt/relative_path")
+        self.assertEqual(prepend_destdir("relative/sub"), "/opt/relative/sub")
+
+        self.assertEqual(prepend_destdir("/home/me/"), "/opt/home/me")
+        self.assertEqual(prepend_destdir("relative_path/"), "/opt/relative_path")
+
+        self.assertEqual(prepend_destdir("/home/me/../me2"), "/opt/home/me2")
+        self.assertEqual(prepend_destdir("relative/sub/../sub2"), "/opt/relative/sub2")
+
 
 
 if __name__ == '__main__':
