2a3c0f42f1ce0854194ec347761f102e26eb9575
Simon Warta <simon@kullo.net>
2017-07-30 17:14:55 +0200

    Deduplicate pylint arguments in lint.sh

--- src/scripts/ci/travis/lint.sh
+++ src/scripts/ci/travis/lint.sh
@@ -2,25 +2,29 @@
 set -ev
 which shellcheck > /dev/null && shellcheck "$0" # Run shellcheck on this if available
 
+shopt -s expand_aliases
+
 # Disabled rules in Python 2 only
 # superfluous-parens: needed for print statements
 # too-many-locals: variable counting differs from pylint3
+alias python2_pylint='python2 -m pylint --disable=superfluous-parens,too-many-locals'
+alias python3_pylint='python3 -m pylint'
 
 echo "travis_fold:start:pylint_configure"
-python3 -m pylint configure.py
-python2 -m pylint --disable=superfluous-parens,too-many-locals configure.py
+python2_pylint configure.py
+python3_pylint configure.py
 echo "travis_fold:end:pylint_configure"
 
 echo "travis_fold:start:pylint_install"
-python3 -m pylint src/scripts/install.py
-python2 -m pylint --disable=superfluous-parens,too-many-locals src/scripts/install.py
+python2_pylint src/scripts/install.py
+python3_pylint src/scripts/install.py
 echo "travis_fold:end:pylint_configure"
 
 echo "travis_fold:start:pylint_python_unittests"
-python3 -m pylint src/scripts/python_uniitests.py
+python3_pylint src/scripts/python_uniitests.py
 echo "travis_fold:end:pylint_python_unittests"
 
 echo "travis_fold:start:pylint_botanpy"
-python3 -m pylint src/python/botan2.py
-python2 -m pylint --disable=superfluous-parens,too-many-locals src/python/botan2.py
+python2_pylint src/python/botan2.py
+python3_pylint src/python/botan2.py
 echo "travis_fold:end:pylint_botanpy"
