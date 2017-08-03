147eb1c26c236a362c4fb42a3d119ef10175092d
Jack Lloyd <jack@randombit.net>
2017-08-03 13:54:48 -0400

    Add a test of PGP S2K iteration encoding

--- src/tests/test_pbkdf.cpp
+++ src/tests/test_pbkdf.cpp
@@ -10,6 +10,10 @@
    #include <botan/pbkdf.h>
 #endif
 
+#if defined(BOTAN_HAS_PGP_S2K)
+   #include <botan/pgp_s2k.h>
+#endif
+
 namespace Botan_Tests {
 
 namespace {
@@ -50,6 +54,55 @@ class PBKDF_KAT_Tests : public Text_Based_Test
    };
 
 BOTAN_REGISTER_TEST("pbkdf", PBKDF_KAT_Tests);
+
+#endif
+
+#if defined(BOTAN_HAS_PGP_S2K)
+
+class PGP_S2K_Iter_Test : public Test
+   {
+   public:
+      std::vector<Test::Result> run() override
+         {
+         Test::Result result("PGP_S2K iteration encoding");
+
+         // The maximum representable iteration count
+         const size_t max_iter = 65011712;
+
+         result.test_eq("Encoding of large value accepted",
+                        Botan::OpenPGP_S2K::encode_count(max_iter * 2), size_t(255));
+         result.test_eq("Encoding of small value accepted",
+                        Botan::OpenPGP_S2K::encode_count(0), size_t(0));
+
+         for(size_t c = 0; c != 256; ++c)
+            {
+            const size_t dec = Botan::OpenPGP_S2K::decode_count(c);
+            const size_t comp_dec = (16 + (c & 0x0F)) << ((c >> 4) + 6);
+            result.test_eq("Decoded value matches PGP formula", dec, comp_dec);
+            }
+
+         uint8_t last_enc = 0;
+
+         for(size_t i = 0; i <= max_iter; i += 64)
+            {
+            const uint8_t enc = Botan::OpenPGP_S2K::encode_count(i);
+            result.test_lte("Encoded value non-decreasing", last_enc, enc);
+
+            /*
+            The iteration count as encoded may not be exactly the
+            value requested, but should never be less
+            */
+            const size_t dec = Botan::OpenPGP_S2K::decode_count(enc);
+            result.test_gte("Decoded value is >= requested", dec, i);
+
+            last_enc = enc;
+            }
+
+         return std::vector<Test::Result>{result};
+         }
+   };
+
+BOTAN_REGISTER_TEST("pgp_s2k_iter", PGP_S2K_Iter_Test);
 
 #endif
 
