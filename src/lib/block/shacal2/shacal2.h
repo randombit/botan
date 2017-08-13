61a7a5757bfe19ff1a686055ab75cae1fc9f485e
Jack Lloyd <jack@randombit.net>
2017-08-13 06:00:54 -0400

    Add SHACAL2
    256 bit ARX block cipher with hardware support, what's not to love.

---
+++ src/lib/block/shacal2/shacal2.h
@@ -0,0 +1,36 @@
+/*
+* SHACAL-2
+* (C) 2017 Jack Lloyd
+*
+* Botan is released under the Simplified BSD License (see license.txt)
+*/
+
+#ifndef BOTAN_SHACAL2_H__
+#define BOTAN_SHACAL2_H__
+
+#include <botan/block_cipher.h>
+
+namespace Botan {
+
+/**
+* SHACAL2
+*/
+class BOTAN_DLL SHACAL2 final : public Block_Cipher_Fixed_Params<32, 16, 64, 4>
+   {
+   public:
+      void encrypt_n(const uint8_t in[], uint8_t out[], size_t blocks) const override;
+      void decrypt_n(const uint8_t in[], uint8_t out[], size_t blocks) const override;
+
+      std::string provider() const override;
+      void clear() override;
+      std::string name() const override { return "SHACAL2"; }
+      BlockCipher* clone() const override { return new SHACAL2; }
+   private:
+      void key_schedule(const uint8_t[], size_t) override;
+
+      secure_vector<uint32_t> m_RK;
+   };
+
+}
+
+#endif
