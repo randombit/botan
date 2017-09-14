206091f9bea256e5259bc7dd84323bb0377985d1
Atanas Filyanov <a.filyanov@sirrix.com>
2017-09-14 10:48:31 +0200

    Allow GCM tag size in the range 96..128 bits

--- src/lib/modes/aead/gcm/gcm.cpp
+++ src/lib/modes/aead/gcm/gcm.cpp
@@ -182,7 +182,8 @@ GCM_Mode::GCM_Mode(BlockCipher* cipher, size_t tag_size) :
 
    m_ctr.reset(new CTR_BE(cipher, 4)); // CTR_BE takes ownership of cipher
 
-   if(m_tag_size != 8 && m_tag_size != 12 && m_tag_size != GCM_BS)
+   /* We allow any of the values 128, 120, 112, 104, or 96 bits as a tag size */
+   if(m_tag_size < 12 || m_tag_size > 16)
       throw Invalid_Argument(name() + ": Bad tag size " + std::to_string(m_tag_size));
    }
 
