c90e0e21910385ed930865b7dc3d34c6e11685a6
Jack Lloyd <jack@randombit.net>
2017-09-27 10:14:34 -0400

    Add HKDF-Expand-Label
    Step 1/n of TLS 1.3

--- src/tests/test_kdf.cpp
+++ src/tests/test_kdf.cpp
@@ -10,6 +10,10 @@
    #include <botan/kdf.h>
 #endif
 
+#if defined(BOTAN_HAS_HKDF)
+   #include <botan/hkdf.h>
+#endif
+
 namespace Botan_Tests {
 
 namespace {
@@ -52,6 +56,40 @@ class KDF_KAT_Tests final : public Text_Based_Test
    };
 
 BOTAN_REGISTER_TEST("kdf", KDF_KAT_Tests);
+
+#endif
+
+#if defined(BOTAN_HAS_HKDF)
+class HKDF_Expand_Label_Tests final : public Text_Based_Test
+   {
+   public:
+      HKDF_Expand_Label_Tests() :
+         Text_Based_Test("hkdf_label.vec", "Secret,Label,HashValue,Output") {}
+
+      Test::Result run_one_test(const std::string& hash_name, const VarMap& vars) override
+         {
+         Test::Result result("HKDF-Expand-Label(" + hash_name + ")");
+
+         const std::vector<uint8_t> secret = get_req_bin(vars, "Secret");
+         const std::vector<uint8_t> hashval = get_req_bin(vars, "HashValue");
+         const std::string label = get_req_str(vars, "Label");
+         const std::vector<uint8_t> expected = get_req_bin(vars, "Output");
+
+         Botan::secure_vector<uint8_t> output =
+            Botan::hkdf_expand_label(hash_name,
+                                     secret.data(), secret.size(),
+                                     label,
+                                     hashval.data(), hashval.size(),
+                                     expected.size());
+
+         result.test_eq("Output matches", output, expected);
+
+         return result;
+         }
+
+   };
+
+BOTAN_REGISTER_TEST("hkdf_expand_label", HKDF_Expand_Label_Tests);
 
 #endif
 
