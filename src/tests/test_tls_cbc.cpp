fd95d06f4e050a2efc3cb79c69f1ea3d04bacb27
Jack Lloyd <jack@randombit.net>
2017-09-29 12:26:41 -0400

    Unused variable
    [ci skip]

--- src/tests/test_tls_cbc.cpp
+++ src/tests/test_tls_cbc.cpp
@@ -19,7 +19,7 @@ class TLS_CBC_Padding_Tests : public Text_Based_Test
    public:
       TLS_CBC_Padding_Tests() : Text_Based_Test("tls_cbc.vec", "Record,Output") {}
 
-      Test::Result run_one_test(const std::string& algo, const VarMap& vars) override
+      Test::Result run_one_test(const std::string&, const VarMap& vars) override
          {
          const std::vector<uint8_t> record    = get_req_bin(vars, "Record");
          const size_t output = get_req_sz(vars, "Output");
