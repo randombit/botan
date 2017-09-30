86ec48552bdb35b063343dd37bfd8963e65f898c
Jack Lloyd <jack@randombit.net>
2017-09-30 12:50:09 -0400

    Another FFI struct vs class fix

--- src/lib/ffi/ffi_util.h
+++ src/lib/ffi/ffi_util.h
@@ -46,7 +46,7 @@ struct botan_struct
    };
 
 #define BOTAN_FFI_DECLARE_STRUCT(NAME, TYPE, MAGIC) \
-   class NAME final : public botan_struct<TYPE, MAGIC> { public: explicit NAME(TYPE* x) : botan_struct(x) {} }
+   struct NAME final : public botan_struct<TYPE, MAGIC> { explicit NAME(TYPE* x) : botan_struct(x) {} }
 
 // Declared in ffi.cpp
 int ffi_error_exception_thrown(const char* func_name, const char* exn);
