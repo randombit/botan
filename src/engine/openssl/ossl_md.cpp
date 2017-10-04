e61a5f56fb404db3655909be4ce23c3a816cbb60
Alon Bar-Lev <alon.barlev@gmail.com>
2017-10-04 22:16:31 +0300

    openssl: support libressl
    Signed-off-by: Alon Bar-Lev <alon.barlev@gmail.com>

--- src/engine/openssl/ossl_md.cpp
+++ src/engine/openssl/ossl_md.cpp
@@ -8,7 +8,7 @@
 #include <botan/internal/openssl_engine.h>
 #include <openssl/evp.h>
 
-#if OPENSSL_VERSION_NUMBER >= 0x10100000
+#if OPENSSL_VERSION_NUMBER >= 0x10100000 && !defined(LIBRESSL_VERSION_NUMBER)
   #error "OpenSSL 1.1 API not supported in Botan 1.10, upgrade to 2.x"
 #endif
 
