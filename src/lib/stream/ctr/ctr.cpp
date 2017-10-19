9be977646f9373ff596e8fdd5ebd73f3281cde49
Jack Lloyd <jack@randombit.net>
2017-10-19 12:43:15 -0400

    Appease Sonar

--- src/lib/stream/ctr/ctr.cpp
+++ src/lib/stream/ctr/ctr.cpp
@@ -143,7 +143,7 @@ void CTR_BE::add_counter(const uint64_t counter)
          uint64_t b0 = load_be<uint64_t>(&m_counter[off], 0);
          uint64_t b1 = load_be<uint64_t>(&m_counter[off], 1);
          b1 += counter;
-         b1 += (b1 < counter); // carry
+         b1 += (b1 < counter) ? 1 : 0; // carry
          store_be(b0, &m_counter[off]);
          store_be(b1, &m_counter[off+8]);
          off += BS;
