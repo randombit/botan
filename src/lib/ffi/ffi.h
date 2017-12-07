b483c7cfd8d3da28ba47d32f77dfda930766175a
Jack Lloyd <jack@randombit.net>
2017-12-07 13:53:19 -0500

    Some further sketch work on an C interface for TLS [ci skip]

--- src/lib/ffi/ffi.h
+++ src/lib/ffi/ffi.h
@@ -1334,22 +1334,56 @@ int botan_fpe_decrypt(botan_fpe_t fpe, botan_mp_t x, const uint8_t tweak[], size
 /*
 * TLS (WIP)
 */
-#if defined(BOTAN_HAS_TLS) && 0
 
+/**
+* TLS Policy objects
+*/
+typedef struct botan_tls_policy_struct* botan_tls_policy_t;
+
+int botan_tls_policy_default_init(botan_tls_policy_t* policy);
+int botan_tls_policy_nsa_suiteb_init(botan_tls_policy_t* policy);
+int botan_tls_policy_bsi_tr_02102_2_init(botan_tls_policy_t* policy);
+int botan_tls_policy_text_init(botan_tls_policy_t* policy, const char* policy_text);
+int botan_tls_policy_destroy(botan_tls_policy_t policy);
+
+/*
+* TLS Session Managers
+*/
+typedef struct botan_tls_session_manager_struct* botan_tls_session_manager_t;
+
+int botan_tls_session_manager_memory_init(botan_tls_session_manager_t* mgr,
+                                          size_t max_sessions);
+
+int botan_tls_session_manager_sql_init(botan_tls_session_manager_t* mgr,
+                                       const char* sql_db_filename,
+                                       const char* db_passphrase,
+                                       size_t max_sessions);
+
+// TODO botan_tls_session_manager_custom_init taking function callbacks
+
+int botan_tls_session_manager_destroy(botan_tls_session_manager_t mgr);
+
+/**
+* TLS Session objects
+*/
 typedef struct botan_tls_session_struct* botan_tls_session_t;
 
+BOTAN_TEST_API int botan_tls_session_encrypt(botan_tls_session_t session,
+                                             botan_rng_t rng,
+                                             uint8_t key[], size_t* key_len);
+
 BOTAN_TEST_API int botan_tls_session_decrypt(botan_tls_session_t* session,
-                                        const uint8_t key[], size_t key_len,
-                                        const uint8_t blob[], size_t blob_len);
+                                             const uint8_t key[], size_t key_len,
+                                             const uint8_t blob[], size_t blob_len);
 
 BOTAN_TEST_API int botan_tls_session_get_version(botan_tls_session_t session, uint16_t* tls_version);
 BOTAN_TEST_API int botan_tls_session_get_ciphersuite(botan_tls_session_t session, uint16_t* ciphersuite);
-BOTAN_TEST_API int botan_tls_session_encrypt(botan_tls_session_t session, botan_rng_t rng, uint8_t key[], size_t* key_len);
 
 BOTAN_TEST_API int botan_tls_session_get_peer_certs(botan_tls_session_t session, botan_x509_cert_t certs[], size_t* cert_len);
 
-// TODO: peer certs, validation, ...
-
+/**
+* TLS Channel Callbacks
+*/
 typedef struct botan_tls_channel_struct* botan_tls_channel_t;
 
 typedef void (*botan_tls_channel_output_fn)(void* application_data, const uint8_t* data, size_t data_len);
@@ -1363,20 +1397,22 @@ typedef void (*botan_tls_channel_session_established)(void* application_data,
                                                       botan_tls_session_t session);
 
 BOTAN_TEST_API int botan_tls_channel_init_client(botan_tls_channel_t* channel,
-                                            botan_tls_channel_output_fn output_fn,
-                                            botan_tls_channel_data_cb data_cb,
-                                            botan_tls_channel_alert_cb alert_cb,
-                                            botan_tls_channel_session_established session_cb,
-                                            const char* server_name);
+                                                 botan_tls_channel_output_fn output_fn,
+                                                 botan_tls_channel_data_cb data_cb,
+                                                 botan_tls_channel_alert_cb alert_cb,
+                                                 botan_tls_channel_session_established session_cb,
+                                                 botan_tls_session_manager_t session_manager,
+                                                 const char* server_name);
 
 BOTAN_TEST_API int botan_tls_channel_init_server(botan_tls_channel_t* channel,
-                                            botan_tls_channel_output_fn output_fn,
-                                            botan_tls_channel_data_cb data_cb,
-                                            botan_tls_channel_alert_cb alert_cb,
-                                            botan_tls_channel_session_established session_cb);
+                                                 botan_tls_channel_output_fn output_fn,
+                                                 botan_tls_channel_data_cb data_cb,
+                                                 botan_tls_channel_alert_cb alert_cb,
+                                                 botan_tls_channel_session_established session_cb,
+                                                 botan_tls_session_manager_t session_manager);
 
 BOTAN_TEST_API int botan_tls_channel_received_data(botan_tls_channel_t chan,
-                                              const uint8_t input[], size_t len);
+                                                   const uint8_t input[], size_t len);
 
 /**
 * Returns 0 for client, 1 for server, negative for error
@@ -1384,13 +1420,12 @@ BOTAN_TEST_API int botan_tls_channel_received_data(botan_tls_channel_t chan,
 BOTAN_TEST_API int botan_tls_channel_type(botan_tls_channel_t chan);
 
 BOTAN_TEST_API int botan_tls_channel_send(botan_tls_channel_t chan,
-                                     const uint8_t input[], size_t len);
+                                          const uint8_t input[], size_t len);
 
 BOTAN_TEST_API int botan_tls_channel_close(botan_tls_channel_t chan);
 
 BOTAN_TEST_API int botan_tls_channel_destroy(botan_tls_channel_t chan);
 
-#endif
 #ifdef __cplusplus
 }
 #endif
---
+++ src/lib/ffi/ffi_tls.cpp
@@ -0,0 +1,260 @@
+/*
+* (C) 2017 Jack Lloyd
+*
+* Botan is released under the Simplified BSD License (see license.txt)
+*/
+
+#include <botan/ffi.h>
+#include <botan/internal/ffi_util.h>
+#include <botan/internal/ffi_rng.h>
+
+#if defined(BOTAN_HAS_TLS)
+  #include <botan/tls_channel.h>
+  #include <botan/tls_session.h>
+  #include <botan/tls_session_manager.h>
+  #include <botan/tls_policy.h>
+#endif
+
+extern "C" {
+
+using namespace Botan_FFI;
+
+#if defined(BOTAN_HAS_TLS)
+
+struct FFI_TLS_Channel_Struct
+   {
+   std::unique_ptr<Botan::TLS::Channel> channel;
+   std::unique_ptr<Botan::TLS::Callbacks> callbacks;
+   };
+
+BOTAN_FFI_DECLARE_STRUCT(botan_tls_session_manager_struct, Botan::TLS::Session_Manager, 0x634C6D67);
+BOTAN_FFI_DECLARE_STRUCT(botan_tls_session_struct, Botan::TLS::Session, 0x93091969);
+BOTAN_FFI_DECLARE_STRUCT(botan_tls_policy_struct, Botan::TLS::Policy, 0x6E590C76);
+BOTAN_FFI_DECLARE_STRUCT(botan_tls_channel_struct, FFI_TLS_Channel_Struct, 0xE818A572);
+
+#else
+
+struct FFI_TLS_Dummy_Struct {};
+
+BOTAN_FFI_DECLARE_STRUCT(botan_tls_session_manager_struct, FFI_TLS_Dummy_Struct, 0x634C6D67);
+BOTAN_FFI_DECLARE_STRUCT(botan_tls_session_struct, FFI_TLS_Dummy_Struct, 0x93091969);
+BOTAN_FFI_DECLARE_STRUCT(botan_tls_policy_struct, FFI_TLS_Dummy_Struct, 0x6E590C76);
+BOTAN_FFI_DECLARE_STRUCT(botan_tls_channel_struct, FFI_TLS_Dummy_Struct, 0xE818A572);
+
+
+#endif
+
+int botan_tls_policy_default_init(botan_tls_policy_t* policy)
+   {
+#if defined(BOTAN_HAS_TLS)
+   return -1;
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_policy_nsa_suiteb_init(botan_tls_policy_t* policy)
+   {
+
+   }
+
+int botan_tls_policy_bsi_tr_02102_2_init(botan_tls_policy_t* policy)
+   {
+#if defined(BOTAN_HAS_TLS)
+   return -1;
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_policy_text_init(botan_tls_policy_t* policy, const char* policy_text)
+   {
+#if defined(BOTAN_HAS_TLS)
+   return -1;
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_policy_destroy(botan_tls_policy_t policy)
+   {
+   return BOTAN_FFI_CHECKED_DELETE(policy);
+   }
+
+int botan_tls_session_manager_memory_init(botan_tls_session_manager_t* mgr,
+                                          size_t max_sessions)
+   {
+#if defined(BOTAN_HAS_TLS)
+   return -1;
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_session_manager_sql_init(botan_tls_session_manager_t* mgr,
+                                       const char* sql_db_filename,
+                                       const char* db_passphrase,
+                                       size_t max_sessions)
+   {
+#if defined(BOTAN_HAS_TLS) && defined(BOTAN_HAS_TLS_SESSION_MANAGER_SQL_DB)
+   return -1;
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_session_manager_destroy(botan_tls_session_manager_t mgr)
+   {
+   return BOTAN_FFI_CHECKED_DELETE(mgr);
+   }
+
+int botan_tls_session_encrypt(botan_tls_session_t session, botan_rng_t rng, uint8_t key[], size_t* key_len)
+   {
+#if defined(BOTAN_HAS_TLS)
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED; // todo
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_session_decrypt(botan_tls_session_t* session,
+                              const uint8_t key[], size_t key_len,
+                              const uint8_t blob[], size_t blob_len)
+   {
+#if defined(BOTAN_HAS_TLS)
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED; // todo
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_session_get_version(botan_tls_session_t session, uint16_t* tls_version)
+   {
+#if defined(BOTAN_HAS_TLS)
+   return BOTAN_FFI_DO(Botan::TLS::Session, session, s, { *tls_version = s.version().raw_version(); });
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_session_get_ciphersuite(botan_tls_session_t session, uint16_t* ciphersuite)
+   {
+#if defined(BOTAN_HAS_TLS)
+   return BOTAN_FFI_DO(Botan::TLS::Session, session, s, { *ciphersuite = s.ciphersuite_code(); });
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_session_get_peer_certs(botan_tls_session_t session, botan_x509_cert_t certs[], size_t* cert_len)
+   {
+#if defined(BOTAN_HAS_TLS)
+
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_channel_init_client(botan_tls_channel_t* channel,
+                                  botan_tls_channel_output_fn output_fn,
+                                  botan_tls_channel_data_cb data_cb,
+                                  botan_tls_channel_alert_cb alert_cb,
+                                  botan_tls_channel_session_established session_cb,
+                                  botan_tls_session_manager_t session_manager,
+                                  const char* server_name)
+   {
+#if defined(BOTAN_HAS_TLS)
+
+   if(channel == nullptr ||
+      output_fn == nullptr ||
+      data_cb == nullptr ||
+      alert_cb == nullptr ||
+      session_cb == nullptr)
+      {
+      return BOTAN_FFI_ERROR_NULL_POINTER;
+      }
+
+   return ffi_guard_thunk(BOTAN_CURRENT_FUNCTION, [=]() {
+
+      *channel = nullptr;
+
+      std::unique_ptr<Botan::TLS::Callbacks> ffi_cb(
+         new FFI_TLS_Callbacks(output_fn, data_cb, alert_cb, session_cb));
+
+      std::unique_ptr<Botan::TLS::Channel> c(new Botan::TLS::Client(*ffi_cb, 
+            Channel(Callbacks& callbacks,
+              Session_Manager& session_manager,
+              RandomNumberGenerator& rng,
+              const Policy& policy,
+              bool is_datagram,
+              size_t io_buf_sz = IO_BUF_DEFAULT_SIZE);
+
+
+      *bc = new botan_block_cipher_struct(cipher.release());
+      return BOTAN_FFI_SUCCESS;
+      });
+
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_channel_init_server(botan_tls_channel_t* channel,
+                                  botan_tls_channel_output_fn output_fn,
+                                  botan_tls_channel_data_cb data_cb,
+                                  botan_tls_channel_alert_cb alert_cb,
+                                  botan_tls_channel_session_established session_cb)
+   {
+#if defined(BOTAN_HAS_TLS)
+
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_channel_received_data(botan_tls_channel_t chan,
+                                    const uint8_t input[], size_t len)
+   {
+#if defined(BOTAN_HAS_TLS)
+   BOTAN_FFI_DO(Botan::TLS::Channel, chan, c, { c.received_data(input, len); });
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_channel_type(botan_tls_channel_t chan)
+   {
+#if defined(BOTAN_HAS_TLS)
+   BOTAN_FFI_DO(Botan::TLS::Channel, chan, c, {
+      return (dynamic_cast<Botan::TLS::Client*>(&c) != nullptr) ? 0 : 1;
+      });
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_channel_send(botan_tls_channel_t chan,
+                           const uint8_t input[], size_t len)
+   {
+#if defined(BOTAN_HAS_TLS)
+   BOTAN_FFI_DO(Botan::TLS::Channel, chan, c, { c.send(input, len); });
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_channel_close(botan_tls_channel_t chan)
+   {
+#if defined(BOTAN_HAS_TLS)
+   BOTAN_FFI_DO(Botan::TLS::Channel, chan, c, { c.close(); });
+#else
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
+   }
+
+int botan_tls_channel_destroy(botan_tls_channel_t chan)
+   {
+   return BOTAN_FFI_CHECKED_DELETE(chan);
+   }
+
+}
