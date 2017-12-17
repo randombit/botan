eb76255d74f8281f0edd6acc0df6086a29c6d1d4
Jack Lloyd <jack@randombit.net>
2017-12-17 06:46:22 -0500

    Try to debug Boost build failure - amalgamation problem?

--- src/scripts/ci_build.py
+++ src/scripts/ci_build.py
@@ -71,7 +71,7 @@ def determine_flags(target, target_os, target_cpu, target_cc, cc_bin, ccache, ro
     if target in ['mini-static', 'mini-shared']:
         flags += ['--minimized-build', '--enable-modules=system_rng,sha2_32,sha2_64,aes']
 
-    if target == 'shared':
+    if target == 'shared' and target_os != 'osx':
         # Enabling amalgamation build for shared is somewhat arbitrary, but we want to test it
         # somewhere. In addition the majority of the Windows builds are shared, and MSVC is
         # much faster compiling via the amalgamation than individual files.
