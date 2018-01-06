ec1cfdd6b83782ac31bc66023145e90192442911
Jack Lloyd <jack@randombit.net>
2018-01-06 18:00:20 -0500

    Cleanups

--- src/lib/pubkey/curve25519/donna.cpp
+++ src/lib/pubkey/curve25519/donna.cpp
@@ -75,6 +75,16 @@ inline void fdifference_backwards(uint64_t out[5], const uint64_t in[5])
    out[4] = in[4] + two54m8 - out[4];
    }
 
+inline void fadd_sub(uint64_t x[5],
+                     uint64_t y[5])
+   {
+   // TODO merge these and avoid the tmp array
+   uint64_t tmp[5];
+   copy_mem(tmp, y, 5);
+   fsum(y, x);
+   fdifference_backwards(x, tmp);  // does x - z
+   }
+
 /* Multiply a number by a scalar: out = in * scalar */
 inline void fscalar_product(uint64_t out[5], const uint64_t in[5], const uint64_t scalar)
    {
@@ -99,52 +109,50 @@ inline void fscalar_product(uint64_t out[5], const uint64_t in[5], const uint64_
 /* Multiply two numbers: out = in2 * in
 *
 * out must be distinct to both inputs. The inputs are reduced coefficient
-* form, the out is not.
+* form, the output is not.
 *
 * Assumes that in[i] < 2**55 and likewise for in2.
 * On return, out[i] < 2**52
 */
-inline void fmul(uint64_t out[5], const uint64_t in2[5], const uint64_t in[5])
+inline void fmul(uint64_t out[5], const uint64_t in[5], const uint64_t in2[5])
    {
-   uint128_t t[5];
-   uint64_t r0,r1,r2,r3,r4,s0,s1,s2,s3,s4,c;
+   const uint128_t s0 = in2[0];
+   const uint128_t s1 = in2[1];
+   const uint128_t s2 = in2[2];
+   const uint128_t s3 = in2[3];
+   const uint128_t s4 = in2[4];
 
-   r0 = in[0];
-   r1 = in[1];
-   r2 = in[2];
-   r3 = in[3];
-   r4 = in[4];
+   uint64_t r0 = in[0];
+   uint64_t r1 = in[1];
+   uint64_t r2 = in[2];
+   uint64_t r3 = in[3];
+   uint64_t r4 = in[4];
 
-   s0 = in2[0];
-   s1 = in2[1];
-   s2 = in2[2];
-   s3 = in2[3];
-   s4 = in2[4];
-
-   t[0]  =  uint128_t(r0) * s0;
-   t[1]  =  uint128_t(r0) * s1 + uint128_t(r1) * s0;
-   t[2]  =  uint128_t(r0) * s2 + uint128_t(r2) * s0 + uint128_t(r1) * s1;
-   t[3]  =  uint128_t(r0) * s3 + uint128_t(r3) * s0 + uint128_t(r1) * s2 + uint128_t(r2) * s1;
-   t[4]  =  uint128_t(r0) * s4 + uint128_t(r4) * s0 + uint128_t(r3) * s1 + uint128_t(r1) * s3 + uint128_t(r2) * s2;
+   uint128_t t0 = r0 * s0;
+   uint128_t t1 = r0 * s1 + r1 * s0;
+   uint128_t t2 = r0 * s2 + r2 * s0 + r1 * s1;
+   uint128_t t3 = r0 * s3 + r3 * s0 + r1 * s2 + r2 * s1;
+   uint128_t t4 = r0 * s4 + r4 * s0 + r3 * s1 + r1 * s3 + r2 * s2;
 
    r4 *= 19;
    r1 *= 19;
    r2 *= 19;
    r3 *= 19;
 
-   t[0] += uint128_t(r4) * s1 + uint128_t(r1) * s4 + uint128_t(r2) * s3 + uint128_t(r3) * s2;
-   t[1] += uint128_t(r4) * s2 + uint128_t(r2) * s4 + uint128_t(r3) * s3;
-   t[2] += uint128_t(r4) * s3 + uint128_t(r3) * s4;
-   t[3] += uint128_t(r4) * s4;
+   t0 += r4 * s1 + r1 * s4 + r2 * s3 + r3 * s2;
+   t1 += r4 * s2 + r2 * s4 + r3 * s3;
+   t2 += r4 * s3 + r3 * s4;
+   t3 += r4 * s4;
 
-   r0 = t[0] & 0x7ffffffffffff; c = carry_shift(t[0], 51);
-   t[1] += c;      r1 = t[1] & 0x7ffffffffffff; c = carry_shift(t[1], 51);
-   t[2] += c;      r2 = t[2] & 0x7ffffffffffff; c = carry_shift(t[2], 51);
-   t[3] += c;      r3 = t[3] & 0x7ffffffffffff; c = carry_shift(t[3], 51);
-   t[4] += c;      r4 = t[4] & 0x7ffffffffffff; c = carry_shift(t[4], 51);
-   r0 +=   c * 19; c = carry_shift(r0, 51); r0 = r0 & 0x7ffffffffffff;
-   r1 +=   c;      c = carry_shift(r1, 51); r1 = r1 & 0x7ffffffffffff;
-   r2 +=   c;
+   r0 = t0 & 0x7ffffffffffff; t1 += carry_shift(t0, 51);
+   r1 = t1 & 0x7ffffffffffff; t2 += carry_shift(t1, 51);
+   r2 = t2 & 0x7ffffffffffff; t3 += carry_shift(t2, 51);
+   r3 = t3 & 0x7ffffffffffff; t4 += carry_shift(t3, 51);
+   r4 = t4 & 0x7ffffffffffff; uint64_t c = carry_shift(t4, 51);
+
+   r0 += c * 19; c = r0 >> 51; r0 = r0 & 0x7ffffffffffff;
+   r1 += c;      c = r1 >> 51; r1 = r1 & 0x7ffffffffffff;
+   r2 += c;
 
    out[0] = r0;
    out[1] = r1;
@@ -179,13 +187,11 @@ inline void fsquare_times(uint64_t out[5], const uint64_t in[5], size_t count)
       r1 = t1 & 0x7ffffffffffff; t2 += carry_shift(t1, 51);
       r2 = t2 & 0x7ffffffffffff; t3 += carry_shift(t2, 51);
       r3 = t3 & 0x7ffffffffffff; t4 += carry_shift(t3, 51);
-      r4 = t4 & 0x7ffffffffffff;
+      r4 = t4 & 0x7ffffffffffff; uint64_t c = carry_shift(t4, 51);
 
-      uint64_t c = carry_shift(t4, 51);
-
-      r0 +=   c * 19; c = r0 >> 51; r0 = r0 & 0x7ffffffffffff;
-      r1 +=   c;      c = r1 >> 51; r1 = r1 & 0x7ffffffffffff;
-      r2 +=   c;
+      r0 += c * 19; c = r0 >> 51; r0 = r0 & 0x7ffffffffffff;
+      r1 += c;      c = r1 >> 51; r1 = r1 & 0x7ffffffffffff;
+      r2 += c;
       }
 
    out[0] = r0;
@@ -193,6 +199,11 @@ inline void fsquare_times(uint64_t out[5], const uint64_t in[5], size_t count)
    out[2] = r2;
    out[3] = r3;
    out[4] = r4;
+   }
+
+inline void fsquare(uint64_t out[5], const uint64_t in[5])
+   {
+   return fsquare_times(out, in, 1);
    }
 
 /* Take a little-endian, 32-byte number and expand it into polynomial form */
@@ -216,17 +227,14 @@ inline void fcontract(uint8_t *out, const uint64_t input[5])
    uint128_t t3 = input[3];
    uint128_t t4 = input[4];
 
-   t1 += t0 >> 51;        t0 &= 0x7ffffffffffff;
-   t2 += t1 >> 51;        t1 &= 0x7ffffffffffff;
-   t3 += t2 >> 51;        t2 &= 0x7ffffffffffff;
-   t4 += t3 >> 51;        t3 &= 0x7ffffffffffff;
-   t0 += (t4 >> 51) * 19; t4 &= 0x7ffffffffffff;
-
-   t1 += t0 >> 51;        t0 &= 0x7ffffffffffff;
-   t2 += t1 >> 51;        t1 &= 0x7ffffffffffff;
-   t3 += t2 >> 51;        t2 &= 0x7ffffffffffff;
-   t4 += t3 >> 51;        t3 &= 0x7ffffffffffff;
-   t0 += (t4 >> 51) * 19; t4 &= 0x7ffffffffffff;
+   for(size_t i = 0; i != 2; ++i)
+      {
+      t1 += t0 >> 51;        t0 &= 0x7ffffffffffff;
+      t2 += t1 >> 51;        t1 &= 0x7ffffffffffff;
+      t3 += t2 >> 51;        t2 &= 0x7ffffffffffff;
+      t4 += t3 >> 51;        t3 &= 0x7ffffffffffff;
+      t0 += (t4 >> 51) * 19; t4 &= 0x7ffffffffffff;
+      }
 
    /* now t is between 0 and 2^255-1, properly carried. */
    /* case 1: between 0 and 2^255-20. case 2: between 2^255-19 and 2^255-1. */
@@ -282,8 +290,6 @@ void fmonty(uint64_t result_two_q_x[5],
             uint64_t in_q_dash_z[5],
             const uint64_t q_minus_q_dash[5])
    {
-   uint64_t origx[5];
-   uint64_t origxprime[5];
    uint64_t zzz[5];
    uint64_t xx[5];
    uint64_t zz[5];
@@ -291,38 +297,36 @@ void fmonty(uint64_t result_two_q_x[5],
    uint64_t zzprime[5];
    uint64_t zzzprime[5];
 
-   copy_mem(origx, in_q_x, 5);
-   fsum(in_q_x, in_q_z);
-   fdifference_backwards(in_q_z, origx);  // does x - z
+   fadd_sub(in_q_z, in_q_x);
+   fadd_sub(in_q_dash_z, in_q_dash_x);
 
-   copy_mem(origxprime, in_q_dash_x, 5);
-   fsum(in_q_dash_x, in_q_dash_z);
-   fdifference_backwards(in_q_dash_z, origxprime);
    fmul(xxprime, in_q_dash_x, in_q_z);
-   fmul(zzprime, in_q_x, in_q_dash_z);
-   copy_mem(origxprime, xxprime, 5);
-   fsum(xxprime, zzprime);
-   fdifference_backwards(zzprime, origxprime);
-   fsquare_times(result_q_plus_q_dash_x, xxprime, 1);
-   fsquare_times(zzzprime, zzprime, 1);
+   fmul(zzprime, in_q_dash_z, in_q_x);
+
+   fadd_sub(zzprime, xxprime);
+
+   fsquare(result_q_plus_q_dash_x, xxprime);
+   fsquare(zzzprime, zzprime);
    fmul(result_q_plus_q_dash_z, zzzprime, q_minus_q_dash);
 
-   fsquare_times(xx, in_q_x, 1);
-   fsquare_times(zz, in_q_z, 1);
+   fsquare(xx, in_q_x);
+   fsquare(zz, in_q_z);
    fmul(result_two_q_x, xx, zz);
+
    fdifference_backwards(zz, xx);  // does zz = xx - zz
    fscalar_product(zzz, zz, 121665);
    fsum(zzz, xx);
+
    fmul(result_two_q_z, zz, zzz);
    }
 
-// -----------------------------------------------------------------------------
-// Maybe swap the contents of two uint64_t arrays (@a and @b), each @len elements
-// long. Perform the swap iff @swap is non-zero.
-//
-// This function performs the swap without leaking any side-channel
-// information.
-// -----------------------------------------------------------------------------
+/*
+* Maybe swap the contents of two uint64_t arrays (@a and @b),
+* Param @iswap is assumed to be either 0 or 1
+*
+* This function performs the swap without leaking any side-channel
+* information.
+*/
 void swap_conditional(uint64_t a[5], uint64_t b[5], uint64_t iswap)
    {
    const uint64_t swap = static_cast<uint64_t>(-iswap);
@@ -337,7 +341,7 @@ void swap_conditional(uint64_t a[5], uint64_t b[5], uint64_t iswap)
 
 /* Calculates nQ where Q is the x-coordinate of a point on the curve
 *
-*   resultx/resultz: the x coordinate of the resulting curve point (short form)
+*   resultx/resultz: the x/z coordinate of the resulting curve point (short form)
 *   n: a little endian, 32-byte number
 *   q: a point of the curve (short form)
 */
@@ -356,14 +360,14 @@ void cmult(uint64_t resultx[5], uint64_t resultz[5], const uint8_t n[32], const 
 
    for(size_t i = 0; i < 32; ++i)
       {
-      const uint64_t bit0 = (n[31 - i] >> (7)) & 1;
-      const uint64_t bit1 = (n[31 - i] >> (6)) & 1;
-      const uint64_t bit2 = (n[31 - i] >> (5)) & 1;
-      const uint64_t bit3 = (n[31 - i] >> (4)) & 1;
-      const uint64_t bit4 = (n[31 - i] >> (3)) & 1;
-      const uint64_t bit5 = (n[31 - i] >> (2)) & 1;
-      const uint64_t bit6 = (n[31 - i] >> (1)) & 1;
-      const uint64_t bit7 = (n[31 - i] >> (0)) & 1;
+      const uint64_t bit0 = (n[31 - i] >> 7) & 1;
+      const uint64_t bit1 = (n[31 - i] >> 6) & 1;
+      const uint64_t bit2 = (n[31 - i] >> 5) & 1;
+      const uint64_t bit3 = (n[31 - i] >> 4) & 1;
+      const uint64_t bit4 = (n[31 - i] >> 3) & 1;
+      const uint64_t bit5 = (n[31 - i] >> 2) & 1;
+      const uint64_t bit6 = (n[31 - i] >> 1) & 1;
+      const uint64_t bit7 = (n[31 - i] >> 0) & 1;
 
       swap_conditional(c, a, bit0);
       swap_conditional(d, b, bit0);
@@ -383,7 +387,6 @@ void cmult(uint64_t resultx[5], uint64_t resultz[5], const uint8_t n[32], const 
 
       swap_conditional(c, a, bit3 ^ bit4);
       swap_conditional(d, b, bit3 ^ bit4);
-
       fmonty(g, h, e, f, c, d, a, b, q);
 
       swap_conditional(g, e, bit4 ^ bit5);
@@ -417,11 +420,11 @@ void crecip(uint64_t out[5], const uint64_t z[5])
    uint64_t c[5];
    uint64_t t0[5];
 
-   /* 2 */ fsquare_times(a, z, 1); // a = 2
+   /* 2 */ fsquare(a, z); // a = 2
    /* 8 */ fsquare_times(t0, a, 2);
    /* 9 */ fmul(b, t0, z); // b = 9
    /* 11 */ fmul(a, b, a); // a = 11
-   /* 22 */ fsquare_times(t0, a, 1);
+   /* 22 */ fsquare(t0, a);
    /* 2^5 - 2^0 = 31 */ fmul(b, t0, b);
    /* 2^10 - 2^5 */ fsquare_times(t0, b, 5);
    /* 2^10 - 2^0 */ fmul(b, t0, b);
