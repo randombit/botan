a3d712149cfe7fc8a2ce8885bd29264cff496639
Jack Lloyd <jack@randombit.net>
2018-02-25 18:46:18 -0500

    Add comment explaining why we are using these useless lambdas

--- src/tests/unit_ecc.cpp
+++ src/tests/unit_ecc.cpp
@@ -188,6 +188,8 @@ class NIST_Curve_Reduction_Tests final : public Test
          {
          std::vector<Test::Result> results;
 
+         // Using lambdas here to avoid strange UbSan warning (#1370)
+
 #if defined(BOTAN_HAS_NIST_PRIME_REDUCERS_W32)
          results.push_back(random_redc_test(
 	    "P-384",
