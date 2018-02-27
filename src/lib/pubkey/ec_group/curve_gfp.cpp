randombit/botan

commit 5fcc1c70d7ae2b2bac8598629e576a7a484b770a

Avoid unnecessary calls to BigInt::grow_to

src/lib/pubkey/ec_group/curve_gfp.cpp
@@ -85,7 +85,8 @@ void CurveGFp_Montgomery::curve_mul(BigInt& z, const BigInt& x, const BigInt& y,
    const size_t output_size = 2*m_p_words + 1;
    ws.resize(2*(m_p_words+2));
 
-   z.grow_to(output_size);
+   if(z.size() < output_size)
+      z.grow_to(output_size);
    z.clear();
 
    bigint_monty_mul(z, x, y,
@@ -109,7 +110,8 @@ void CurveGFp_Montgomery::curve_sqr(BigInt& z, const BigInt& x,
 
    ws.resize(2*(m_p_words+2));
 
-   z.grow_to(output_size);
+   if(z.size() < output_size)
+      z.grow_to(output_size);
    z.clear();
 
    bigint_monty_sqr(z, x, m_p.data(), m_p_words, m_p_dash,
@@ -164,9 +166,11 @@ void CurveGFp_NIST::curve_mul(BigInt& z, const BigInt& x, const BigInt& y,
 
    const size_t p_words = get_p_words();
    const size_t output_size = 2*p_words + 1;
+
    ws.resize(2*(p_words+2));
 
-   z.grow_to(output_size);
+   if(z.size() < output_size)
+      z.grow_to(output_size);
    z.clear();
 
    bigint_mul(z, x, y, ws.data(), ws.size());
@@ -188,7 +192,8 @@ void CurveGFp_NIST::curve_sqr(BigInt& z, const BigInt& x,
 
    ws.resize(2*(p_words+2));
 
-   z.grow_to(output_size);
+   if(z.size() < output_size)
+      z.grow_to(output_size);
    z.clear();
 
    bigint_sqr(z.mutable_data(), output_size,

commit 74ca458b14d265b68ad25cf918aa216b3fa51103

Fix overflow in monty_redc
OSS-Fuzz caught a bug introduced in 5fcc1c70d7a. bigint_monty_redc
assumes z is 2*p_words+2 words long. Previously the implicit rounding
up in grow_to ensured a resize would result in a sufficiently large
value.

OSS-Fuzz 6581 6588 6593

src/lib/pubkey/ec_group/curve_gfp.cpp
@@ -82,7 +82,7 @@ void CurveGFp_Montgomery::curve_mul(BigInt& z, const BigInt& x, const BigInt& y,
       return;
       }
 
-   const size_t output_size = 2*m_p_words + 1;
+   const size_t output_size = 2*m_p_words + 2;
    ws.resize(2*(m_p_words+2));
 
    if(z.size() < output_size)
@@ -106,7 +106,7 @@ void CurveGFp_Montgomery::curve_sqr(BigInt& z, const BigInt& x,
    const size_t x_sw = x.sig_words();
    BOTAN_ASSERT(x_sw <= m_p_words, "Input in range");
 
-   const size_t output_size = 2*m_p_words + 1;
+   const size_t output_size = 2*m_p_words + 2;
 
    ws.resize(2*(m_p_words+2));
 
@@ -165,7 +165,7 @@ void CurveGFp_NIST::curve_mul(BigInt& z, const BigInt& x, const BigInt& y,
       }
 
    const size_t p_words = get_p_words();
-   const size_t output_size = 2*p_words + 1;
+   const size_t output_size = 2*p_words + 2;
 
    ws.resize(2*(p_words+2));
 
@@ -188,7 +188,7 @@ void CurveGFp_NIST::curve_sqr(BigInt& z, const BigInt& x,
       }
 
    const size_t p_words = get_p_words();
-   const size_t output_size = 2*p_words + 1;
+   const size_t output_size = 2*p_words + 2;
 
    ws.resize(2*(p_words+2));
 