6ae002ebd3a5f648860e932604b6e63ca7201011
Jack Lloyd <jack@randombit.net>
2018-03-10 11:32:42 -0500

    Avoid warning in threefish.h
    Causes a warning in amalgamation which is bad news

--- src/lib/block/threefish_512/threefish.h
+++ src/lib/block/threefish_512/threefish.h
@@ -8,12 +8,6 @@
 #ifndef BOTAN_THREEFISH_H_
 #define BOTAN_THREEFISH_H_
 
-#if defined(__GNUC__)
-  #warning "botan/threefish.h is deprecated"
-#elif defined(_MSC_VER)
-  #pragma message ("botan/threefish.h is deprecated")
-#endif
-
 #include <botan/threefish_512.h>
 
 #endif
