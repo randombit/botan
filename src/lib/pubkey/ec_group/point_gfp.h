b08f7beb877569fd94736c5a67b9e28fcdd968b6
Jack Lloyd <jack@randombit.net>
2018-03-20 09:23:33 -0400

    Nudge users to EC_Group::OS2ECP

--- src/lib/pubkey/ec_group/point_gfp.h
+++ src/lib/pubkey/ec_group/point_gfp.h
@@ -303,21 +303,27 @@ inline secure_vector<uint8_t> BOTAN_DEPRECATED("Use PointGFp::encode")
    return secure_vector<uint8_t>(enc.begin(), enc.end());
    }
 
+/**
+* Perform point decoding
+* Use EC_Group::OS2ECP instead
+*/
 PointGFp BOTAN_PUBLIC_API(2,0) OS2ECP(const uint8_t data[], size_t data_len,
                                       const CurveGFp& curve);
 
 /**
 * Perform point decoding
+* Use EC_Group::OS2ECP instead
+*
 * @param data the encoded point
 * @param data_len length of data in bytes
 * @param curve_p the curve equation prime
 * @param curve_a the curve equation a parameter
 * @param curve_b the curve equation b parameter
 */
-std::pair<BigInt, BigInt> BOTAN_PUBLIC_API(2,5) OS2ECP(const uint8_t data[], size_t data_len,
-                                                       const BigInt& curve_p,
-                                                       const BigInt& curve_a,
-                                                       const BigInt& curve_b);
+std::pair<BigInt, BigInt> BOTAN_UNSTABLE_API OS2ECP(const uint8_t data[], size_t data_len,
+                                                    const BigInt& curve_p,
+                                                    const BigInt& curve_a,
+                                                    const BigInt& curve_b);
 
 template<typename Alloc>
 PointGFp OS2ECP(const std::vector<uint8_t, Alloc>& data, const CurveGFp& curve)
