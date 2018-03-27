randombit/botan

commit 8a7559e4f8ad179802ef482131faa5dfc0a074ad

Add some helpers for handling carries in Ed25519

src/lib/pubkey/ed25519/sc_muladd.cpp
@@ -28,41 +28,43 @@ void sc_muladd(uint8_t* s, const uint8_t* a, const uint8_t* b, const uint8_t* c)
    const int64_t X20 = (1 << 20);
    const int64_t X21 = (1 << 21);
 
-   int64_t a0 = 2097151 & load_3(a);
-   int64_t a1 = 2097151 & (load_4(a + 2) >> 5);
-   int64_t a2 = 2097151 & (load_3(a + 5) >> 2);
-   int64_t a3 = 2097151 & (load_4(a + 7) >> 7);
-   int64_t a4 = 2097151 & (load_4(a + 10) >> 4);
-   int64_t a5 = 2097151 & (load_3(a + 13) >> 1);
-   int64_t a6 = 2097151 & (load_4(a + 15) >> 6);
-   int64_t a7 = 2097151 & (load_3(a + 18) >> 3);
-   int64_t a8 = 2097151 & load_3(a + 21);
-   int64_t a9 = 2097151 & (load_4(a + 23) >> 5);
-   int64_t a10 = 2097151 & (load_3(a + 26) >> 2);
+   const int32_t MASK = 0x1fffff;
+
+   int64_t a0 = MASK & load_3(a);
+   int64_t a1 = MASK & (load_4(a + 2) >> 5);
+   int64_t a2 = MASK & (load_3(a + 5) >> 2);
+   int64_t a3 = MASK & (load_4(a + 7) >> 7);
+   int64_t a4 = MASK & (load_4(a + 10) >> 4);
+   int64_t a5 = MASK & (load_3(a + 13) >> 1);
+   int64_t a6 = MASK & (load_4(a + 15) >> 6);
+   int64_t a7 = MASK & (load_3(a + 18) >> 3);
+   int64_t a8 = MASK & load_3(a + 21);
+   int64_t a9 = MASK & (load_4(a + 23) >> 5);
+   int64_t a10 = MASK & (load_3(a + 26) >> 2);
    int64_t a11 = (load_4(a + 28) >> 7);
-   int64_t b0 = 2097151 & load_3(b);
-   int64_t b1 = 2097151 & (load_4(b + 2) >> 5);
-   int64_t b2 = 2097151 & (load_3(b + 5) >> 2);
-   int64_t b3 = 2097151 & (load_4(b + 7) >> 7);
-   int64_t b4 = 2097151 & (load_4(b + 10) >> 4);
-   int64_t b5 = 2097151 & (load_3(b + 13) >> 1);
-   int64_t b6 = 2097151 & (load_4(b + 15) >> 6);
-   int64_t b7 = 2097151 & (load_3(b + 18) >> 3);
-   int64_t b8 = 2097151 & load_3(b + 21);
-   int64_t b9 = 2097151 & (load_4(b + 23) >> 5);
-   int64_t b10 = 2097151 & (load_3(b + 26) >> 2);
+   int64_t b0 = MASK & load_3(b);
+   int64_t b1 = MASK & (load_4(b + 2) >> 5);
+   int64_t b2 = MASK & (load_3(b + 5) >> 2);
+   int64_t b3 = MASK & (load_4(b + 7) >> 7);
+   int64_t b4 = MASK & (load_4(b + 10) >> 4);
+   int64_t b5 = MASK & (load_3(b + 13) >> 1);
+   int64_t b6 = MASK & (load_4(b + 15) >> 6);
+   int64_t b7 = MASK & (load_3(b + 18) >> 3);
+   int64_t b8 = MASK & load_3(b + 21);
+   int64_t b9 = MASK & (load_4(b + 23) >> 5);
+   int64_t b10 = MASK & (load_3(b + 26) >> 2);
    int64_t b11 = (load_4(b + 28) >> 7);
-   int64_t c0 = 2097151 & load_3(c);
-   int64_t c1 = 2097151 & (load_4(c + 2) >> 5);
-   int64_t c2 = 2097151 & (load_3(c + 5) >> 2);
-   int64_t c3 = 2097151 & (load_4(c + 7) >> 7);
-   int64_t c4 = 2097151 & (load_4(c + 10) >> 4);
-   int64_t c5 = 2097151 & (load_3(c + 13) >> 1);
-   int64_t c6 = 2097151 & (load_4(c + 15) >> 6);
-   int64_t c7 = 2097151 & (load_3(c + 18) >> 3);
-   int64_t c8 = 2097151 & load_3(c + 21);
-   int64_t c9 = 2097151 & (load_4(c + 23) >> 5);
-   int64_t c10 = 2097151 & (load_3(c + 26) >> 2);
+   int64_t c0 = MASK & load_3(c);
+   int64_t c1 = MASK & (load_4(c + 2) >> 5);
+   int64_t c2 = MASK & (load_3(c + 5) >> 2);
+   int64_t c3 = MASK & (load_4(c + 7) >> 7);
+   int64_t c4 = MASK & (load_4(c + 10) >> 4);
+   int64_t c5 = MASK & (load_3(c + 13) >> 1);
+   int64_t c6 = MASK & (load_4(c + 15) >> 6);
+   int64_t c7 = MASK & (load_3(c + 18) >> 3);
+   int64_t c8 = MASK & load_3(c + 21);
+   int64_t c9 = MASK & (load_4(c + 23) >> 5);
+   int64_t c10 = MASK & (load_3(c + 26) >> 2);
    int64_t c11 = (load_4(c + 28) >> 7);
    int64_t s0;
    int64_t s1;
@@ -88,29 +90,6 @@ void sc_muladd(uint8_t* s, const uint8_t* a, const uint8_t* b, const uint8_t* c)
    int64_t s21;
    int64_t s22;
    int64_t s23;
-   int64_t carry0;
-   int64_t carry1;
-   int64_t carry2;
-   int64_t carry3;
-   int64_t carry4;
-   int64_t carry5;
-   int64_t carry6;
-   int64_t carry7;
-   int64_t carry8;
-   int64_t carry9;
-   int64_t carry10;
-   int64_t carry11;
-   int64_t carry12;
-   int64_t carry13;
-   int64_t carry14;
-   int64_t carry15;
-   int64_t carry16;
-   int64_t carry17;
-   int64_t carry18;
-   int64_t carry19;
-   int64_t carry20;
-   int64_t carry21;
-   int64_t carry22;
 
    s0 = c0 + a0*b0;
    s1 = c1 + a0*b1 + a1*b0;
@@ -137,76 +116,30 @@ void sc_muladd(uint8_t* s, const uint8_t* a, const uint8_t* b, const uint8_t* c)
    s22 = a11*b11;
    s23 = 0;
 
-   carry0 = (s0 + X20) >> 21;
-   s1 += carry0;
-   s0 -= carry0 * X21;
-   carry2 = (s2 + X20) >> 21;
-   s3 += carry2;
-   s2 -= carry2 * X21;
-   carry4 = (s4 + X20) >> 21;
-   s5 += carry4;
-   s4 -= carry4 * X21;
-   carry6 = (s6 + X20) >> 21;
-   s7 += carry6;
-   s6 -= carry6 * X21;
-   carry8 = (s8 + X20) >> 21;
-   s9 += carry8;
-   s8 -= carry8 * X21;
-   carry10 = (s10 + X20) >> 21;
-   s11 += carry10;
-   s10 -= carry10 * X21;
-   carry12 = (s12 + X20) >> 21;
-   s13 += carry12;
-   s12 -= carry12 * X21;
-   carry14 = (s14 + X20) >> 21;
-   s15 += carry14;
-   s14 -= carry14 * X21;
-   carry16 = (s16 + X20) >> 21;
-   s17 += carry16;
-   s16 -= carry16 * X21;
-   carry18 = (s18 + X20) >> 21;
-   s19 += carry18;
-   s18 -= carry18 * X21;
-   carry20 = (s20 + X20) >> 21;
-   s21 += carry20;
-   s20 -= carry20 * X21;
-   carry22 = (s22 + X20) >> 21;
-   s23 += carry22;
-   s22 -= carry22 * X21;
+   carry<21>(s0, s1);
+   carry<21>(s2, s3);
+   carry<21>(s4, s5);
+   carry<21>(s6, s7);
+   carry<21>(s8, s9);
+   carry<21>(s10, s11);
+   carry<21>(s12, s13);
+   carry<21>(s14, s15);
+   carry<21>(s16, s17);
+   carry<21>(s18, s19);
+   carry<21>(s20, s21);
+   carry<21>(s22, s23);
 
-   carry1 = (s1 + X20) >> 21;
-   s2 += carry1;
-   s1 -= carry1 * X21;
-   carry3 = (s3 + X20) >> 21;
-   s4 += carry3;
-   s3 -= carry3 * X21;
-   carry5 = (s5 + X20) >> 21;
-   s6 += carry5;
-   s5 -= carry5 * X21;
-   carry7 = (s7 + X20) >> 21;
-   s8 += carry7;
-   s7 -= carry7 * X21;
-   carry9 = (s9 + X20) >> 21;
-   s10 += carry9;
-   s9 -= carry9 * X21;
-   carry11 = (s11 + X20) >> 21;
-   s12 += carry11;
-   s11 -= carry11 * X21;
-   carry13 = (s13 + X20) >> 21;
-   s14 += carry13;
-   s13 -= carry13 * X21;
-   carry15 = (s15 + X20) >> 21;
-   s16 += carry15;
-   s15 -= carry15 * X21;
-   carry17 = (s17 + X20) >> 21;
-   s18 += carry17;
-   s17 -= carry17 * X21;
-   carry19 = (s19 + X20) >> 21;
-   s20 += carry19;
-   s19 -= carry19 * X21;
-   carry21 = (s21 + X20) >> 21;
-   s22 += carry21;
-   s21 -= carry21 * X21;
+   carry<21>(s1, s2);
+   carry<21>(s3, s4);
+   carry<21>(s5, s6);
+   carry<21>(s7, s8);
+   carry<21>(s9, s10);
+   carry<21>(s11, s12);
+   carry<21>(s13, s14);
+   carry<21>(s15, s16);
+   carry<21>(s17, s18);
+   carry<21>(s19, s20);
+   carry<21>(s21, s22);
 
    s11 += s23 * 666643;
    s12 += s23 * 470296;
@@ -256,40 +189,18 @@ void sc_muladd(uint8_t* s, const uint8_t* a, const uint8_t* b, const uint8_t* c)
    s11 -= s18 * 683901;
    s18 = 0;
 
-   carry6 = (s6 + X20) >> 21;
-   s7 += carry6;
-   s6 -= carry6 * X21;
-   carry8 = (s8 + X20) >> 21;
-   s9 += carry8;
-   s8 -= carry8 * X21;
-   carry10 = (s10 + X20) >> 21;
-   s11 += carry10;
-   s10 -= carry10 * X21;
-   carry12 = (s12 + X20) >> 21;
-   s13 += carry12;
-   s12 -= carry12 * X21;
-   carry14 = (s14 + X20) >> 21;
-   s15 += carry14;
-   s14 -= carry14 * X21;
-   carry16 = (s16 + X20) >> 21;
-   s17 += carry16;
-   s16 -= carry16 * X21;
+   carry<21>(s6, s7);
+   carry<21>(s8, s9);
+   carry<21>(s10, s11);
+   carry<21>(s12, s13);
+   carry<21>(s14, s15);
+   carry<21>(s16, s17);
 
-   carry7 = (s7 + X20) >> 21;
-   s8 += carry7;
-   s7 -= carry7 * X21;
-   carry9 = (s9 + X20) >> 21;
-   s10 += carry9;
-   s9 -= carry9 * X21;
-   carry11 = (s11 + X20) >> 21;
-   s12 += carry11;
-   s11 -= carry11 * X21;
-   carry13 = (s13 + X20) >> 21;
-   s14 += carry13;
-   s13 -= carry13 * X21;
-   carry15 = (s15 + X20) >> 21;
-   s16 += carry15;
-   s15 -= carry15 * X21;
+   carry<21>(s7, s8);
+   carry<21>(s9, s10);
+   carry<21>(s11, s12);
+   carry<21>(s13, s14);
+   carry<21>(s15, s16);
 
    s5 += s17 * 666643;
    s6 += s17 * 470296;
@@ -339,43 +250,19 @@ void sc_muladd(uint8_t* s, const uint8_t* a, const uint8_t* b, const uint8_t* c)
    s5 -= s12 * 683901;
    s12 = 0;
 
-   carry0 = (s0 + X20) >> 21;
-   s1 += carry0;
-   s0 -= carry0 * X21;
-   carry2 = (s2 + X20) >> 21;
-   s3 += carry2;
-   s2 -= carry2 * X21;
-   carry4 = (s4 + X20) >> 21;
-   s5 += carry4;
-   s4 -= carry4 * X21;
-   carry6 = (s6 + X20) >> 21;
-   s7 += carry6;
-   s6 -= carry6 * X21;
-   carry8 = (s8 + X20) >> 21;
-   s9 += carry8;
-   s8 -= carry8 * X21;
-   carry10 = (s10 + X20) >> 21;
-   s11 += carry10;
-   s10 -= carry10 * X21;
+   carry<21>(s0, s1);
+   carry<21>(s2, s3);
+   carry<21>(s4, s5);
+   carry<21>(s6, s7);
+   carry<21>(s8, s9);
+   carry<21>(s10, s11);
 
-   carry1 = (s1 + X20) >> 21;
-   s2 += carry1;
-   s1 -= carry1 * X21;
-   carry3 = (s3 + X20) >> 21;
-   s4 += carry3;
-   s3 -= carry3 * X21;
-   carry5 = (s5 + X20) >> 21;
-   s6 += carry5;
-   s5 -= carry5 * X21;
-   carry7 = (s7 + X20) >> 21;
-   s8 += carry7;
-   s7 -= carry7 * X21;
-   carry9 = (s9 + X20) >> 21;
-   s10 += carry9;
-   s9 -= carry9 * X21;
-   carry11 = (s11 + X20) >> 21;
-   s12 += carry11;
-   s11 -= carry11 * X21;
+   carry<21>(s1, s2);
+   carry<21>(s3, s4);
+   carry<21>(s5, s6);
+   carry<21>(s7, s8);
+   carry<21>(s9, s10);
+   carry<21>(s11, s12);
 
    s0 += s12 * 666643;
    s1 += s12 * 470296;
@@ -385,42 +272,18 @@ void sc_muladd(uint8_t* s, const uint8_t* a, const uint8_t* b, const uint8_t* c)
    s5 -= s12 * 683901;
    s12 = 0;
 
-   carry0 = s0 >> 21;
-   s1 += carry0;
-   s0 -= carry0 * X21;
-   carry1 = s1 >> 21;
-   s2 += carry1;
-   s1 -= carry1 * X21;
-   carry2 = s2 >> 21;
-   s3 += carry2;
-   s2 -= carry2 * X21;
-   carry3 = s3 >> 21;
-   s4 += carry3;
-   s3 -= carry3 * X21;
-   carry4 = s4 >> 21;
-   s5 += carry4;
-   s4 -= carry4 * X21;
-   carry5 = s5 >> 21;
-   s6 += carry5;
-   s5 -= carry5 * X21;
-   carry6 = s6 >> 21;
-   s7 += carry6;
-   s6 -= carry6 * X21;
-   carry7 = s7 >> 21;
-   s8 += carry7;
-   s7 -= carry7 * X21;
-   carry8 = s8 >> 21;
-   s9 += carry8;
-   s8 -= carry8 * X21;
-   carry9 = s9 >> 21;
-   s10 += carry9;
-   s9 -= carry9 * X21;
-   carry10 = s10 >> 21;
-   s11 += carry10;
-   s10 -= carry10 * X21;
-   carry11 = s11 >> 21;
-   s12 += carry11;
-   s11 -= carry11 * X21;
+   carry<21>(s0, s1);
+   carry<21>(s1, s2);
+   carry<21>(s2, s3);
+   carry<21>(s3, s4);
+   carry<21>(s4, s5);
+   carry<21>(s5, s6);
+   carry<21>(s6, s7);
+   carry<21>(s7, s8);
+   carry<21>(s8, s9);
+   carry<21>(s9, s10);
+   carry<21>(s10, s11);
+   carry0<21>(s11, s12);
 
    s0 += s12 * 666643;
    s1 += s12 * 470296;
@@ -430,39 +293,17 @@ void sc_muladd(uint8_t* s, const uint8_t* a, const uint8_t* b, const uint8_t* c)
    s5 -= s12 * 683901;
    s12 = 0;
 
-   carry0 = s0 >> 21;
-   s1 += carry0;
-   s0 -= carry0 * X21;
-   carry1 = s1 >> 21;
-   s2 += carry1;
-   s1 -= carry1 * X21;
-   carry2 = s2 >> 21;
-   s3 += carry2;
-   s2 -= carry2 * X21;
-   carry3 = s3 >> 21;
-   s4 += carry3;
-   s3 -= carry3 * X21;
-   carry4 = s4 >> 21;
-   s5 += carry4;
-   s4 -= carry4 * X21;
-   carry5 = s5 >> 21;
-   s6 += carry5;
-   s5 -= carry5 * X21;
-   carry6 = s6 >> 21;
-   s7 += carry6;
-   s6 -= carry6 * X21;
-   carry7 = s7 >> 21;
-   s8 += carry7;
-   s7 -= carry7 * X21;
-   carry8 = s8 >> 21;
-   s9 += carry8;
-   s8 -= carry8 * X21;
-   carry9 = s9 >> 21;
-   s10 += carry9;
-   s9 -= carry9 * X21;
-   carry10 = s10 >> 21;
-   s11 += carry10;
-   s10 -= carry10 * X21;
+   carry0<21>(s0, s1);
+   carry0<21>(s1, s2);
+   carry0<21>(s2, s3);
+   carry0<21>(s3, s4);
+   carry0<21>(s4, s5);
+   carry0<21>(s5, s6);
+   carry0<21>(s6, s7);
+   carry0<21>(s7, s8);
+   carry0<21>(s8, s9);
+   carry0<21>(s9, s10);
+   carry0<21>(s10, s11);
 
    s[0] = s0 >> 0;
    s[1] = s0 >> 8;

src/lib/pubkey/ed25519/sc_reduce.cpp
@@ -24,51 +24,32 @@ Output:
 
 void sc_reduce(uint8_t* s)
    {
-   const int64_t X20 = (1 << 20);
-   const int64_t X21 = (1 << 21);
+   const uint32_t MASK = 0x1fffff;
 
-
-   int64_t s0 = 2097151 & load_3(s);
-   int64_t s1 = 2097151 & (load_4(s + 2) >> 5);
-   int64_t s2 = 2097151 & (load_3(s + 5) >> 2);
-   int64_t s3 = 2097151 & (load_4(s + 7) >> 7);
-   int64_t s4 = 2097151 & (load_4(s + 10) >> 4);
-   int64_t s5 = 2097151 & (load_3(s + 13) >> 1);
-   int64_t s6 = 2097151 & (load_4(s + 15) >> 6);
-   int64_t s7 = 2097151 & (load_3(s + 18) >> 3);
-   int64_t s8 = 2097151 & load_3(s + 21);
-   int64_t s9 = 2097151 & (load_4(s + 23) >> 5);
-   int64_t s10 = 2097151 & (load_3(s + 26) >> 2);
-   int64_t s11 = 2097151 & (load_4(s + 28) >> 7);
-   int64_t s12 = 2097151 & (load_4(s + 31) >> 4);
-   int64_t s13 = 2097151 & (load_3(s + 34) >> 1);
-   int64_t s14 = 2097151 & (load_4(s + 36) >> 6);
-   int64_t s15 = 2097151 & (load_3(s + 39) >> 3);
-   int64_t s16 = 2097151 & load_3(s + 42);
-   int64_t s17 = 2097151 & (load_4(s + 44) >> 5);
-   int64_t s18 = 2097151 & (load_3(s + 47) >> 2);
-   int64_t s19 = 2097151 & (load_4(s + 49) >> 7);
-   int64_t s20 = 2097151 & (load_4(s + 52) >> 4);
-   int64_t s21 = 2097151 & (load_3(s + 55) >> 1);
-   int64_t s22 = 2097151 & (load_4(s + 57) >> 6);
+   int64_t s0 = MASK & load_3(s);
+   int64_t s1 = MASK & (load_4(s + 2) >> 5);
+   int64_t s2 = MASK & (load_3(s + 5) >> 2);
+   int64_t s3 = MASK & (load_4(s + 7) >> 7);
+   int64_t s4 = MASK & (load_4(s + 10) >> 4);
+   int64_t s5 = MASK & (load_3(s + 13) >> 1);
+   int64_t s6 = MASK & (load_4(s + 15) >> 6);
+   int64_t s7 = MASK & (load_3(s + 18) >> 3);
+   int64_t s8 = MASK & load_3(s + 21);
+   int64_t s9 = MASK & (load_4(s + 23) >> 5);
+   int64_t s10 = MASK & (load_3(s + 26) >> 2);
+   int64_t s11 = MASK & (load_4(s + 28) >> 7);
+   int64_t s12 = MASK & (load_4(s + 31) >> 4);
+   int64_t s13 = MASK & (load_3(s + 34) >> 1);
+   int64_t s14 = MASK & (load_4(s + 36) >> 6);
+   int64_t s15 = MASK & (load_3(s + 39) >> 3);
+   int64_t s16 = MASK & load_3(s + 42);
+   int64_t s17 = MASK & (load_4(s + 44) >> 5);
+   int64_t s18 = MASK & (load_3(s + 47) >> 2);
+   int64_t s19 = MASK & (load_4(s + 49) >> 7);
+   int64_t s20 = MASK & (load_4(s + 52) >> 4);
+   int64_t s21 = MASK & (load_3(s + 55) >> 1);
+   int64_t s22 = MASK & (load_4(s + 57) >> 6);
    int64_t s23 = (load_4(s + 60) >> 3);
-   int64_t carry0;
-   int64_t carry1;
-   int64_t carry2;
-   int64_t carry3;
-   int64_t carry4;
-   int64_t carry5;
-   int64_t carry6;
-   int64_t carry7;
-   int64_t carry8;
-   int64_t carry9;
-   int64_t carry10;
-   int64_t carry11;
-   int64_t carry12;
-   int64_t carry13;
-   int64_t carry14;
-   int64_t carry15;
-   int64_t carry16;
 
    s11 += s23 * 666643;
    s12 += s23 * 470296;
@@ -118,40 +99,18 @@ void sc_reduce(uint8_t* s)
    s11 -= s18 * 683901;
    s18 = 0;
 
-   carry6 = (s6 + X20) >> 21;
-   s7 += carry6;
-   s6 -= carry6 * X21;
-   carry8 = (s8 + X20) >> 21;
-   s9 += carry8;
-   s8 -= carry8 * X21;
-   carry10 = (s10 + X20) >> 21;
-   s11 += carry10;
-   s10 -= carry10 * X21;
-   carry12 = (s12 + X20) >> 21;
-   s13 += carry12;
-   s12 -= carry12 * X21;
-   carry14 = (s14 + X20) >> 21;
-   s15 += carry14;
-   s14 -= carry14 * X21;
-   carry16 = (s16 + X20) >> 21;
-   s17 += carry16;
-   s16 -= carry16 * X21;
+   carry<21>(s6, s7);
+   carry<21>(s8, s9);
+   carry<21>(s10, s11);
+   carry<21>(s12, s13);
+   carry<21>(s14, s15);
+   carry<21>(s16, s17);
 
-   carry7 = (s7 + X20) >> 21;
-   s8 += carry7;
-   s7 -= carry7 * X21;
-   carry9 = (s9 + X20) >> 21;
-   s10 += carry9;
-   s9 -= carry9 * X21;
-   carry11 = (s11 + X20) >> 21;
-   s12 += carry11;
-   s11 -= carry11 * X21;
-   carry13 = (s13 + X20) >> 21;
-   s14 += carry13;
-   s13 -= carry13 * X21;
-   carry15 = (s15 + X20) >> 21;
-   s16 += carry15;
-   s15 -= carry15 * X21;
+   carry<21>(s7, s8);
+   carry<21>(s9, s10);
+   carry<21>(s11, s12);
+   carry<21>(s13, s14);
+   carry<21>(s15, s16);
 
    s5 += s17 * 666643;
    s6 += s17 * 470296;
@@ -201,43 +160,19 @@ void sc_reduce(uint8_t* s)
    s5 -= s12 * 683901;
    s12 = 0;
 
-   carry0 = (s0 + X20) >> 21;
-   s1 += carry0;
-   s0 -= carry0 * X21;
-   carry2 = (s2 + X20) >> 21;
-   s3 += carry2;
-   s2 -= carry2 * X21;
-   carry4 = (s4 + X20) >> 21;
-   s5 += carry4;
-   s4 -= carry4 * X21;
-   carry6 = (s6 + X20) >> 21;
-   s7 += carry6;
-   s6 -= carry6 * X21;
-   carry8 = (s8 + X20) >> 21;
-   s9 += carry8;
-   s8 -= carry8 * X21;
-   carry10 = (s10 + X20) >> 21;
-   s11 += carry10;
-   s10 -= carry10 * X21;
+   carry<21>(s0, s1);
+   carry<21>(s2, s3);
+   carry<21>(s4, s5);
+   carry<21>(s6, s7);
+   carry<21>(s8, s9);
+   carry<21>(s10, s11);
 
-   carry1 = (s1 + X20) >> 21;
-   s2 += carry1;
-   s1 -= carry1 * X21;
-   carry3 = (s3 + X20) >> 21;
-   s4 += carry3;
-   s3 -= carry3 * X21;
-   carry5 = (s5 + X20) >> 21;
-   s6 += carry5;
-   s5 -= carry5 * X21;
-   carry7 = (s7 + X20) >> 21;
-   s8 += carry7;
-   s7 -= carry7 * X21;
-   carry9 = (s9 + X20) >> 21;
-   s10 += carry9;
-   s9 -= carry9 * X21;
-   carry11 = (s11 + X20) >> 21;
-   s12 += carry11;
-   s11 -= carry11 * X21;
+   carry<21>(s1, s2);
+   carry<21>(s3, s4);
+   carry<21>(s5, s6);
+   carry<21>(s7, s8);
+   carry<21>(s9, s10);
+   carry<21>(s11, s12);
 
    s0 += s12 * 666643;
    s1 += s12 * 470296;
@@ -247,42 +182,18 @@ void sc_reduce(uint8_t* s)
    s5 -= s12 * 683901;
    s12 = 0;
 
-   carry0 = s0 >> 21;
-   s1 += carry0;
-   s0 -= carry0 * X21;
-   carry1 = s1 >> 21;
-   s2 += carry1;
-   s1 -= carry1 * X21;
-   carry2 = s2 >> 21;
-   s3 += carry2;
-   s2 -= carry2 * X21;
-   carry3 = s3 >> 21;
-   s4 += carry3;
-   s3 -= carry3 * X21;
-   carry4 = s4 >> 21;
-   s5 += carry4;
-   s4 -= carry4 * X21;
-   carry5 = s5 >> 21;
-   s6 += carry5;
-   s5 -= carry5 * X21;
-   carry6 = s6 >> 21;
-   s7 += carry6;
-   s6 -= carry6 * X21;
-   carry7 = s7 >> 21;
-   s8 += carry7;
-   s7 -= carry7 * X21;
-   carry8 = s8 >> 21;
-   s9 += carry8;
-   s8 -= carry8 * X21;
-   carry9 = s9 >> 21;
-   s10 += carry9;
-   s9 -= carry9 * X21;
-   carry10 = s10 >> 21;
-   s11 += carry10;
-   s10 -= carry10 * X21;
-   carry11 = s11 >> 21;
-   s12 += carry11;
-   s11 -= carry11 * X21;
+   carry<21>(s0, s1);
+   carry<21>(s1, s2);
+   carry<21>(s2, s3);
+   carry<21>(s3, s4);
+   carry<21>(s4, s5);
+   carry<21>(s5, s6);
+   carry<21>(s6, s7);
+   carry<21>(s7, s8);
+   carry<21>(s8, s9);
+   carry<21>(s9, s10);
+   carry<21>(s10, s11);
+   carry0<21>(s11, s12);
 
    s0 += s12 * 666643;
    s1 += s12 * 470296;
@@ -292,39 +203,17 @@ void sc_reduce(uint8_t* s)
    s5 -= s12 * 683901;
    s12 = 0;
 
-   carry0 = s0 >> 21;
-   s1 += carry0;
-   s0 -= carry0 * X21;
-   carry1 = s1 >> 21;
-   s2 += carry1;
-   s1 -= carry1 * X21;
-   carry2 = s2 >> 21;
-   s3 += carry2;
-   s2 -= carry2 * X21;
-   carry3 = s3 >> 21;
-   s4 += carry3;
-   s3 -= carry3 * X21;
-   carry4 = s4 >> 21;
-   s5 += carry4;
-   s4 -= carry4 * X21;
-   carry5 = s5 >> 21;
-   s6 += carry5;
-   s5 -= carry5 * X21;
-   carry6 = s6 >> 21;
-   s7 += carry6;
-   s6 -= carry6 * X21;
-   carry7 = s7 >> 21;
-   s8 += carry7;
-   s7 -= carry7 * X21;
-   carry8 = s8 >> 21;
-   s9 += carry8;
-   s8 -= carry8 * X21;
-   carry9 = s9 >> 21;
-   s10 += carry9;
-   s9 -= carry9 * X21;
-   carry10 = s10 >> 21;
-   s11 += carry10;
-   s10 -= carry10 * X21;
+   carry0<21>(s0, s1);
+   carry0<21>(s1, s2);
+   carry0<21>(s2, s3);
+   carry0<21>(s3, s4);
+   carry0<21>(s4, s5);
+   carry0<21>(s5, s6);
+   carry0<21>(s6, s7);
+   carry0<21>(s7, s8);
+   carry0<21>(s8, s9);
+   carry0<21>(s9, s10);
+   carry0<21>(s10, s11);
 
    s[0] = s0 >> 0;
    s[1] = s0 >> 8;

src/lib/pubkey/ed25519/ed25519_internal.h
@@ -28,6 +28,40 @@ inline uint64_t load_4(const uint8_t* in)
    return load_le<uint32_t>(in, 0);
    }
 
+template<size_t S, int64_t MUL=1>
+inline void carry(int64_t& h0, int64_t& h1)
+   {
+   static_assert(S > 0 && S < 64, "Shift in range");
+
+   const int64_t X1 = (static_cast<int64_t>(1) << S);
+   const int64_t X2 = (static_cast<int64_t>(1) << (S - 1));
+   int64_t c = (h0 + X2)  >> S;
+   h1 += c * MUL;
+   h0 -= c * X1;
+   }
+
+template<size_t S>
+inline void carry0(int64_t& h0, int64_t& h1)
+   {
+   static_assert(S > 0 && S < 64, "Shift in range");
+
+   const int64_t X1 = (static_cast<int64_t>(1) << S);
+   int64_t c = h0 >> S;
+   h1 += c;
+   h0 -= c * X1;
+   }
+
+template<size_t S>
+inline void carry0(int32_t& h0, int32_t& h1)
+   {
+   static_assert(S > 0 && S < 32, "Shift in range");
+
+   const int32_t X1 = (static_cast<int64_t>(1) << S);
+   int32_t c = h0 >> S;
+   h1 += c;
+   h0 -= c * X1;
+   }
+
 /*
 ge means group element.
 

commit 15478bc16dfbf2ddb6f9a7614039015569c8680d

Fix carry bugs introduced in 8a7559e4f8ad

src/lib/pubkey/ed25519/sc_muladd.cpp
@@ -157,17 +157,17 @@ void sc_muladd(uint8_t* s, const uint8_t* a, const uint8_t* b, const uint8_t* c)
 
    redc_mul(s0, s1, s2, s3, s4, s5, s12);
 
-   carry<21>(s0, s1);
-   carry<21>(s1, s2);
-   carry<21>(s2, s3);
-   carry<21>(s3, s4);
-   carry<21>(s4, s5);
-   carry<21>(s5, s6);
-   carry<21>(s6, s7);
-   carry<21>(s7, s8);
-   carry<21>(s8, s9);
-   carry<21>(s9, s10);
-   carry<21>(s10, s11);
+   carry0<21>(s0, s1);
+   carry0<21>(s1, s2);
+   carry0<21>(s2, s3);
+   carry0<21>(s3, s4);
+   carry0<21>(s4, s5);
+   carry0<21>(s5, s6);
+   carry0<21>(s6, s7);
+   carry0<21>(s7, s8);
+   carry0<21>(s8, s9);
+   carry0<21>(s9, s10);
+   carry0<21>(s10, s11);
    carry0<21>(s11, s12);
 
    redc_mul(s0, s1, s2, s3, s4, s5, s12);

src/lib/pubkey/ed25519/sc_reduce.cpp
@@ -94,17 +94,17 @@ void sc_reduce(uint8_t* s)
 
    redc_mul(s0, s1, s2, s3, s4, s5, s12);
 
-   carry<21>(s0, s1);
-   carry<21>(s1, s2);
-   carry<21>(s2, s3);
-   carry<21>(s3, s4);
-   carry<21>(s4, s5);
-   carry<21>(s5, s6);
-   carry<21>(s6, s7);
-   carry<21>(s7, s8);
-   carry<21>(s8, s9);
-   carry<21>(s9, s10);
-   carry<21>(s10, s11);
+   carry0<21>(s0, s1);
+   carry0<21>(s1, s2);
+   carry0<21>(s2, s3);
+   carry0<21>(s3, s4);
+   carry0<21>(s4, s5);
+   carry0<21>(s5, s6);
+   carry0<21>(s6, s7);
+   carry0<21>(s7, s8);
+   carry0<21>(s8, s9);
+   carry0<21>(s9, s10);
+   carry0<21>(s10, s11);
    carry0<21>(s11, s12);
 
    redc_mul(s0, s1, s2, s3, s4, s5, s12);
@@ -120,6 +120,7 @@ void sc_reduce(uint8_t* s)
    carry0<21>(s8, s9);
    carry0<21>(s9, s10);
    carry0<21>(s10, s11);
+   carry0<21>(s11, s12);
 
    s[0] = s0 >> 0;
    s[1] = s0 >> 8;