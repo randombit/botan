randombit/botan

commit 3657639abf776e78b35a961d646dd410a7fce492

Add RAII versions of get_cipher_mode and get_aead
See also #1526

src/lib/tls/tls_session.cpp
@@ -179,7 +179,7 @@ std::chrono::seconds Session::session_age() const
 std::vector<uint8_t>
 Session::encrypt(const SymmetricKey& key, RandomNumberGenerator& rng) const
    {
-   std::unique_ptr<AEAD_Mode> aead(get_aead("AES-256/GCM", ENCRYPTION));
+   std::unique_ptr<AEAD_Mode> aead = AEAD_Mode::create_or_throw("AES-256/GCM", ENCRYPTION);
    const size_t nonce_len = aead->default_nonce_length();
 
    const secure_vector<uint8_t> nonce = rng.random_vec(nonce_len);
@@ -202,7 +202,7 @@ Session Session::decrypt(const uint8_t in[], size_t in_len, const SymmetricKey& 
    {
    try
       {
-      std::unique_ptr<AEAD_Mode> aead(get_aead("AES-256/GCM", DECRYPTION));
+      std::unique_ptr<AEAD_Mode> aead = AEAD_Mode::create_or_throw("AES-256/GCM", ENCRYPTION);
       const size_t nonce_len = aead->default_nonce_length();
 
       if(in_len < nonce_len + aead->tag_size())

src/cli/encryption.cpp
@@ -44,7 +44,7 @@ do_crypt(const std::string &cipher,
 
    // TODO: implement streaming
 
-   std::unique_ptr<Botan::Cipher_Mode> processor(Botan::get_cipher_mode(cipher, direction));
+   std::unique_ptr<Botan::Cipher_Mode> processor(Botan::Cipher_Mode::create(cipher, direction));
    if(!processor)
       throw CLI_Error("Cipher algorithm not found");
 

src/cli/speed.cpp
@@ -783,9 +783,9 @@ class Speed final : public Command
                }
 #endif
 #if defined(BOTAN_HAS_CIPHER_MODES)
-            else if(auto enc = Botan::get_cipher_mode(algo, Botan::ENCRYPTION))
+            else if(auto enc = Botan::Cipher_Mode::create(algo, Botan::ENCRYPTION))
                {
-               auto dec = Botan::get_cipher_mode(algo, Botan::DECRYPTION);
+               auto dec = Botan::Cipher_Mode::create_or_throw(algo, Botan::DECRYPTION);
                bench_cipher_mode(*enc, *dec, msec, buf_sizes);
                }
 #endif

src/cli/timing_tests.cpp
@@ -217,7 +217,7 @@ std::vector<uint8_t> Lucky13_Timing_Test::prepare_input(std::string input)
    const std::vector<uint8_t> key(16);
    const std::vector<uint8_t> iv(16);
 
-   std::unique_ptr<Botan::Cipher_Mode> enc(Botan::get_cipher_mode("AES-128/CBC/NoPadding", Botan::ENCRYPTION));
+   std::unique_ptr<Botan::Cipher_Mode> enc(Botan::Cipher_Mode::create("AES-128/CBC/NoPadding", Botan::ENCRYPTION));
    enc->set_key(key);
    enc->start(iv);
    Botan::secure_vector<uint8_t> buf(input_vector.begin(), input_vector.end());

src/lib/ffi/ffi_cipher.cpp
@@ -23,7 +23,7 @@ int botan_cipher_init(botan_cipher_t* cipher, const char* cipher_name, uint32_t 
    return ffi_guard_thunk(BOTAN_CURRENT_FUNCTION, [=]() -> int {
       const bool encrypt_p = ((flags & BOTAN_CIPHER_INIT_FLAG_MASK_DIRECTION) == BOTAN_CIPHER_INIT_FLAG_ENCRYPT);
       const Botan::Cipher_Dir dir = encrypt_p ? Botan::ENCRYPTION : Botan::DECRYPTION;
-      std::unique_ptr<Botan::Cipher_Mode> mode(Botan::get_cipher_mode(cipher_name, dir));
+      std::unique_ptr<Botan::Cipher_Mode> mode(Botan::Cipher_Mode::create(cipher_name, dir));
       if(!mode)
          return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
       *cipher = new botan_cipher_struct(mode.release());

src/lib/filters/cipher_filter.h
@@ -23,6 +23,9 @@ class BOTAN_PUBLIC_API(2,0) Cipher_Mode_Filter final : public Keyed_Filter,
    public:
       explicit Cipher_Mode_Filter(Cipher_Mode* t);
 
+      explicit Cipher_Mode_Filter(std::unique_ptr<Cipher_Mode> t) :
+         Cipher_Mode_Filter(t.release()) {}
+
       void set_iv(const InitializationVector& iv) override;
 
       void set_key(const SymmetricKey& key) override;

src/lib/filters/key_filt.cpp
@@ -12,10 +12,8 @@ namespace Botan {
 Keyed_Filter* get_cipher(const std::string& algo_spec,
                          Cipher_Dir direction)
    {
-   std::unique_ptr<Cipher_Mode> c(get_cipher_mode(algo_spec, direction));
-   if(c)
-      return new Cipher_Mode_Filter(c.release());
-   throw Algorithm_Not_Found(algo_spec);
+   std::unique_ptr<Cipher_Mode> c(Cipher_Mode::create_or_throw(algo_spec, direction));
+   return new Cipher_Mode_Filter(c.release());
    }
 
 Keyed_Filter* get_cipher(const std::string& algo_spec,

src/lib/misc/cryptobox/cryptobox.cpp
@@ -76,7 +76,7 @@ std::string encrypt(const uint8_t input[], size_t input_len,
    const uint8_t* iv = mk + CIPHER_KEY_LEN + MAC_KEY_LEN;
 
    // Now encrypt and authenticate
-   std::unique_ptr<Cipher_Mode> ctr(get_cipher_mode("Serpent/CTR-BE", ENCRYPTION));
+   std::unique_ptr<Cipher_Mode> ctr = Cipher_Mode::create_or_throw("Serpent/CTR-BE", ENCRYPTION);
    ctr->set_key(cipher_key, CIPHER_KEY_LEN);
    ctr->start(iv, CIPHER_IV_LEN);
    ctr->finish(out_buf, CRYPTOBOX_HEADER_LEN);
@@ -142,7 +142,7 @@ decrypt_bin(const uint8_t input[], size_t input_len,
    if(!constant_time_compare(computed_mac.data(), box_mac, MAC_OUTPUT_LEN))
       throw Decoding_Error("CryptoBox integrity failure");
 
-   std::unique_ptr<Cipher_Mode> ctr(get_cipher_mode("Serpent/CTR-BE", DECRYPTION));
+   std::unique_ptr<Cipher_Mode> ctr(Cipher_Mode::create_or_throw("Serpent/CTR-BE", DECRYPTION));
    ctr->set_key(cipher_key, CIPHER_KEY_LEN);
    ctr->start(iv, CIPHER_IV_LEN);
    ctr->finish(ciphertext, CRYPTOBOX_HEADER_LEN);

commit 6cfe771a5ced6c87eda98bfdfcd0811490d45baa

Fix bug that broke session decryption (and thus resumption)
Introduced in 3657639ab. Add a test that would have caught this

src/lib/tls/tls_session.cpp
@@ -202,7 +202,7 @@ Session Session::decrypt(const uint8_t in[], size_t in_len, const SymmetricKey& 
    {
    try
       {
-      std::unique_ptr<AEAD_Mode> aead = AEAD_Mode::create_or_throw("AES-256/GCM", ENCRYPTION);
+      std::unique_ptr<AEAD_Mode> aead = AEAD_Mode::create_or_throw("AES-256/GCM", DECRYPTION);
       const size_t nonce_len = aead->default_nonce_length();
 
       if(in_len < nonce_len + aead->tag_size())

src/tests/test_tls.cpp
@@ -11,6 +11,8 @@
 #if defined(BOTAN_HAS_TLS)
   #include <botan/tls_alert.h>
   #include <botan/tls_policy.h>
+  #include <botan/tls_session.h>
+  #include <botan/tls_version.h>
 
   #if defined(BOTAN_HAS_TLS_CBC)
      #include <botan/internal/tls_cbc.h>
@@ -21,6 +23,52 @@
 namespace Botan_Tests {
 
 #if defined(BOTAN_HAS_TLS)
+
+class TLS_Session_Tests final : public Test
+   {
+   public:
+      std::vector<Test::Result> run() override
+         {
+         Test::Result result("TLS::Session");
+
+         Botan::TLS::Session default_session;
+
+         Botan::secure_vector<uint8_t> default_der = default_session.DER_encode();
+
+         result.test_gte("Encoded default session has size", default_der.size(), 0);
+
+         Botan::TLS::Session decoded_default(default_der.data(), default_der.size());
+
+         Botan::TLS::Session session(std::vector<uint8_t>{0xAA, 0xBB},
+                                     Botan::secure_vector<uint8_t>{0xCC, 0xDD},
+                                     Botan::TLS::Protocol_Version::TLS_V12,
+                                     0xFE0F,
+                                     Botan::TLS::CLIENT,
+                                     true,
+                                     false,
+                                     std::vector<Botan::X509_Certificate>(),
+                                     std::vector<uint8_t>(),
+                                     Botan::TLS::Server_Information("server"),
+                                     "SRP username",
+                                     0x0000);
+
+         const Botan::SymmetricKey key("ABCDEF");
+         std::vector<uint8_t> ctext1 = session.encrypt(key, Test::rng());
+         std::vector<uint8_t> ctext2 = session.encrypt(key, Test::rng());
+
+         result.test_ne("TLS session encryption is non-determinsitic",
+                        ctext1.data(), ctext1.size(),
+                        ctext2.data(), ctext2.size());
+
+         Botan::TLS::Session dsession = Botan::TLS::Session::decrypt(ctext1.data(), ctext1.size(), key);
+
+         result.test_eq("Decrypted session access works", dsession.srp_identifier(), "SRP username");
+
+         return {result};
+         }
+   };
+
+BOTAN_REGISTER_TEST("tls_session", TLS_Session_Tests);
 
 #if defined(BOTAN_HAS_TLS_CBC)
 

src/tests/unit_tls.cpp
@@ -704,6 +704,12 @@ class TLS_Unit_Tests final : public Test
                   version, creds, policy, policy, rng, client_ses, server_ses, client_auth);
                test.go();
                results.push_back(test.results());
+
+               TLS_Handshake_Test test_resumption(
+                  version.to_string() + " " + test_descr,
+                  version, creds, policy, policy, rng, client_ses, server_ses, client_auth);
+               test_resumption.go();
+               results.push_back(test_resumption.results());
                }
             }
          catch(std::exception& e)