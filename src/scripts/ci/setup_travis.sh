c303cf1754a41ba4248275eb4141e2fa6f587269
Jack Lloyd <jack@randombit.net>
2018-05-29 12:34:59 -0400

    Workaround for broken Ubuntu roman package

--- src/scripts/ci/setup_travis.sh
+++ src/scripts/ci/setup_travis.sh
@@ -79,9 +79,11 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
         # all C++ features used in the manual. Install python-requests to avoid
         # problem in Ubuntu packaged version, see
         # http://stackoverflow.com/questions/32779919/no-module-named-for-requests
-        sudo apt-get remove python-requests python-openssl
-        sudo pip install requests pyopenssl
-        sudo pip install sphinx==1.7.4
+        # The python-roman package is also broken in Ubuntu
+        # https://github.com/sphinx-doc/sphinx/issues/5022
+        sudo apt-get remove python-requests python-openssl python-roman
+        sudo pip install requests pyopenssl roman
+        sudo pip install sphinx
     fi
 
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
