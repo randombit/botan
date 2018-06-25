f6263f14d3ebed0dbd55eda774226e6c580d8600
Jack Lloyd <jack@randombit.net>
2018-06-25 13:55:53 -0400

    Test all multiplication variants in ECC scalar mult test

--- src/tests/test_ecc_pointmul.cpp
+++ src/tests/test_ecc_pointmul.cpp
@@ -23,17 +23,28 @@ class ECC_Pointmult_Tests final : public Text_Based_Test
 
       Test::Result run_one_test(const std::string& group_id, const VarMap& vars) override
          {
+         Test::Result result("ECC Scalarmult " + group_id);
+
          const Botan::BigInt m = vars.get_req_bn("m");
          const Botan::BigInt X = vars.get_req_bn("X");
          const Botan::BigInt Y = vars.get_req_bn("Y");
 
          Botan::EC_Group group(Botan::OIDS::lookup(group_id));
 
-         const Botan::PointGFp p = group.get_base_point() * m;
+         const Botan::PointGFp& base_point = group.get_base_point();
 
-         Test::Result result("ECC Scalarmult " + group_id);
-         result.test_eq("affine X", p.get_affine_x(), X);
-         result.test_eq("affine Y", p.get_affine_y(), Y);
+         const Botan::PointGFp p1 = base_point * m;
+         result.test_eq("p1 affine X", p1.get_affine_x(), X);
+         result.test_eq("p1 affine Y", p1.get_affine_y(), Y);
+
+         std::vector<Botan::BigInt> ws;
+         const Botan::PointGFp p2 = group.blinded_base_point_multiply(m, Test::rng(), ws);
+         result.test_eq("p2 affine X", p2.get_affine_x(), X);
+         result.test_eq("p2 affine Y", p2.get_affine_y(), Y);
+
+         const Botan::PointGFp p3 = group.blinded_var_point_multiply(base_point, m, Test::rng(), ws);
+         result.test_eq("p3 affine X", p3.get_affine_x(), X);
+         result.test_eq("p3 affine Y", p3.get_affine_y(), Y);
 
          return result;
          }
