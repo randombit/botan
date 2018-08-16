randombit/botan

commit 15e149ac2dab3b22273c166839cfbf1fb947b2d4

Avoid throwing within the FFI layer
No reason given we know the message is going to be thrown away.

src/lib/ffi/ffi_pkey_algs.cpp
@@ -117,7 +117,7 @@ Botan::BigInt pubkey_get_field(const Botan::Public_Key& key,
       else if(field == "e")
          return rsa->get_e();
       else
-         throw Botan::Exception("Field not supported");
+         return BOTAN_FFI_ERROR_BAD_PARAMETER;
       }
 #endif
 
@@ -134,7 +134,7 @@ Botan::BigInt pubkey_get_field(const Botan::Public_Key& key,
       else if(field == "y")
          return dl->get_y();
       else
-         throw Botan::Exception("Field not supported");
+         return BOTAN_FFI_ERROR_BAD_PARAMETER;
       }
 #endif
 
@@ -160,12 +160,12 @@ Botan::BigInt pubkey_get_field(const Botan::Public_Key& key,
       else if(field == "order")
          return ecc->domain().get_order();
       else
-         throw Botan::Exception("Field not supported");
+         return BOTAN_FFI_ERROR_BAD_PARAMETER;
       }
 #endif
 
    // Some other algorithm type not supported by this function
-   throw Botan::Exception("Unsupported algorithm type for botan_pubkey_get_field");
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
    }
 
 Botan::BigInt privkey_get_field(const Botan::Private_Key& key,
@@ -216,7 +216,7 @@ Botan::BigInt privkey_get_field(const Botan::Private_Key& key,
 #endif
 
    // Some other algorithm type not supported by this function
-   throw Botan::Exception("Unsupported algorithm type for botan_privkey_get_field");
+   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
    }
 
 }

src/lib/ffi/ffi_kdf.cpp
@@ -91,7 +91,7 @@ int botan_bcrypt_generate(uint8_t* out, size_t* out_len,
          return BOTAN_FFI_ERROR_BAD_FLAG;
 
       if(wf < 4 || wf > 18)
-         throw FFI_Error("Bad bcrypt work factor " + std::to_string(wf), BOTAN_FFI_ERROR_BAD_PARAMETER);
+         return BOTAN_FFI_ERROR_BAD_PARAMETER;
 
       Botan::RandomNumberGenerator& rng = safe_get(rng_obj);
       const std::string bcrypt = Botan::generate_bcrypt(pass, rng, static_cast<uint16_t>(wf));

src/lib/ffi/ffi_util.h
@@ -63,11 +63,10 @@ T& safe_get(botan_struct<T,M>* p)
    if(p->magic_ok() == false)
       throw FFI_Error("Bad magic in ffi object", BOTAN_FFI_ERROR_INVALID_OBJECT);
 
-   T* t = p->unsafe_get();
-   if(t)
+   if(T* t = p->unsafe_get())
       return *t;
-   else
-      throw FFI_Error("Invalid object pointer", BOTAN_FFI_ERROR_INVALID_OBJECT);
+
+   throw FFI_Error("Invalid object pointer", BOTAN_FFI_ERROR_INVALID_OBJECT);
    }
 
 int ffi_guard_thunk(const char* func_name, std::function<int ()>);

commit 4766fb375ab35026389d39547695be2237f6c1ca

Fix bug introduced in 15e149ac
This caused get_field, when called with a non-existing field, to
return success and set the field to a small negative integer.

src/lib/ffi/ffi_pkey_algs.cpp
@@ -117,7 +117,7 @@ Botan::BigInt pubkey_get_field(const Botan::Public_Key& key,
       else if(field == "e")
          return rsa->get_e();
       else
-         return BOTAN_FFI_ERROR_BAD_PARAMETER;
+         throw Botan_FFI::FFI_Error("Bad field", BOTAN_FFI_ERROR_BAD_PARAMETER);
       }
 #endif
 
@@ -134,7 +134,7 @@ Botan::BigInt pubkey_get_field(const Botan::Public_Key& key,
       else if(field == "y")
          return dl->get_y();
       else
-         return BOTAN_FFI_ERROR_BAD_PARAMETER;
+         throw Botan_FFI::FFI_Error("Bad field", BOTAN_FFI_ERROR_BAD_PARAMETER);
       }
 #endif
 
@@ -160,12 +160,13 @@ Botan::BigInt pubkey_get_field(const Botan::Public_Key& key,
       else if(field == "order")
          return ecc->domain().get_order();
       else
-         return BOTAN_FFI_ERROR_BAD_PARAMETER;
+         throw Botan_FFI::FFI_Error("Bad field", BOTAN_FFI_ERROR_BAD_PARAMETER);
       }
 #endif
 
    // Some other algorithm type not supported by this function
-   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+   throw Botan_FFI::FFI_Error("Field getter not implemented for this algorithm type",
+                              BOTAN_FFI_ERROR_NOT_IMPLEMENTED);
    }
 
 Botan::BigInt privkey_get_field(const Botan::Private_Key& key,
@@ -215,8 +216,7 @@ Botan::BigInt privkey_get_field(const Botan::Private_Key& key,
       }
 #endif
 
-   // Some other algorithm type not supported by this function
-   return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+   return pubkey_get_field(key, field);
    }
 
 }

src/tests/test_ffi.cpp
@@ -1646,6 +1646,9 @@ class FFI_Unit_Tests final : public Test
             botan_mp_init(&n);
             botan_mp_init(&e);
 
+            TEST_FFI_RC(BOTAN_FFI_ERROR_BAD_PARAMETER, botan_privkey_get_field, (p, priv, "quux"));
+            TEST_FFI_RC(BOTAN_FFI_ERROR_BAD_PARAMETER, botan_pubkey_get_field, (p, pub, "quux"));
+
             TEST_FFI_OK(botan_privkey_rsa_get_p, (p, priv));
             TEST_FFI_OK(botan_privkey_rsa_get_q, (q, priv));
             TEST_FFI_OK(botan_privkey_rsa_get_d, (d, priv));
@@ -1799,10 +1802,14 @@ class FFI_Unit_Tests final : public Test
 
             botan_mp_t cmp;
             botan_mp_init(&cmp);
+            TEST_FFI_RC(BOTAN_FFI_ERROR_BAD_PARAMETER, botan_privkey_get_field, (cmp, priv, "quux"));
+
             TEST_FFI_OK(botan_privkey_get_field, (cmp, priv, "x"));
             TEST_FFI_RC(1, botan_mp_equal, (cmp, x));
+
             TEST_FFI_OK(botan_privkey_get_field, (cmp, priv, "y"));
             TEST_FFI_RC(1, botan_mp_equal, (cmp, y));
+
             TEST_FFI_OK(botan_privkey_get_field, (cmp, priv, "p"));
             TEST_FFI_RC(1, botan_mp_equal, (cmp, p));
             botan_mp_destroy(cmp);
@@ -1911,6 +1918,9 @@ class FFI_Unit_Tests final : public Test
          botan_mp_init(&private_scalar);
          botan_mp_init(&public_x);
          botan_mp_init(&public_y);
+
+         TEST_FFI_RC(BOTAN_FFI_ERROR_BAD_PARAMETER, botan_privkey_get_field, (private_scalar, priv, "quux"));
+         TEST_FFI_RC(BOTAN_FFI_ERROR_BAD_PARAMETER, botan_pubkey_get_field, (private_scalar, pub, "quux"));
 
          TEST_FFI_OK(botan_privkey_get_field, (private_scalar, priv, "x"));
          TEST_FFI_OK(botan_pubkey_get_field, (public_x, pub, "public_x"));