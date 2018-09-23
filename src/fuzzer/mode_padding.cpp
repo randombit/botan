681703167cb3b49464aabbb3c335cee29338f202
Jack Lloyd <jack@randombit.net>
2018-09-23 13:04:31 -0400

    Avoid some gcc warnings in mode padding fuzzer [ci skip]

--- src/fuzzer/mode_padding.cpp
+++ src/fuzzer/mode_padding.cpp
@@ -8,6 +8,8 @@
 #include <botan/mode_pad.h>
 #include <botan/internal/tls_cbc.h>
 
+namespace {
+
 size_t ref_pkcs7_unpad(const uint8_t in[], size_t len)
    {
    if(len <= 2)
@@ -58,7 +60,7 @@ size_t ref_oneandzero_unpad(const uint8_t in[], size_t len)
 
    size_t idx = len - 1;
 
-   while(idx >= 0)
+   for(;;)
       {
       if(in[idx] == 0)
          {
@@ -123,6 +125,8 @@ uint16_t ref_tls_cbc_unpad(const uint8_t in[], size_t len)
       }
    return padding_length + 1;
    }
+
+}
 
 void fuzz(const uint8_t in[], size_t len)
    {
