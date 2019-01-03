3b7ac3da76c8a883a48938bf9505b7b27a851a8c
Jack Lloyd <jack@randombit.net>
2019-01-03 09:23:58 -0500

    Remove VC2013 support from AppVeyor script

--- src/scripts/ci/setup_appveyor.bat
+++ src/scripts/ci/setup_appveyor.bat
@@ -1,7 +1,6 @@
 
 echo Current build setup MSVS="%MSVS%" PLATFORM="%PLATFORM%" TARGET="%TARGET%"
 
-if %MSVS% == 2013 call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" %PLATFORM%
 if %MSVS% == 2015 call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %PLATFORM%
 if %MSVS% == 2017 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %PLATFORM%
 if %MSVS% == 2019 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vcvarsall.bat" %PLATFORM%
