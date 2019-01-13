f4246cd7cc1a9f4c4f5ff0cd891c85f5a0784603
Jack Lloyd <jack@randombit.net>
2019-01-13 13:03:02 -0500

    Ignore trailing whitespace in header guards
    This is specifically needed to handle CRLF lines in Python2, otherwise
    the regex for header guards does not match and building the
    amalgamation fails.
    
    This only occured if a header was edited on Windows then later a build
    using amalgamation attempted on Linux.
    
    Closes #1763

--- configure.py
+++ configure.py
@@ -2453,7 +2453,8 @@ class AmalgamationHeader(object):
 class AmalgamationGenerator(object):
     filename_prefix = 'botan_all'
 
-    _header_guard_pattern = re.compile('^#define BOTAN_.*_H_$')
+    _header_guard_pattern = re.compile(r'^#define BOTAN_.*_H_\s*$')
+    _header_endif_pattern = re.compile(r'^#endif\s*$')
 
     @staticmethod
     def read_header(filepath):
@@ -2478,7 +2479,7 @@ class AmalgamationGenerator(object):
 
         end_header_guard_index = None
         for index, line in enumerate(lines):
-            if line == '#endif\n':
+            if AmalgamationGenerator._header_endif_pattern.match(line):
                 end_header_guard_index = index # override with last found
         if end_header_guard_index is None:
             raise InternalError("No header guard end found in " + header_name)
