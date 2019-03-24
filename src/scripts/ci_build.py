381a67df0a2209c81d81830dba8eefb921c0a2c9
Jack Lloyd <jack@randombit.net>
2019-03-24 13:29:15 -0400

    Now getentropy is used by default, remove from CI script

--- src/scripts/ci_build.py
+++ src/scripts/ci_build.py
@@ -192,10 +192,6 @@ def determine_flags(target, target_os, target_cpu, target_cc, cc_bin, ccache, ro
         if target_os == 'osx' or target == 'coverage':
             flags += ['--with-boost']
 
-        if target_os == 'osx':
-            # Travis has 10.12 as default image
-            flags += ['--with-os-features=getentropy']
-
         if target_os == 'linux':
             flags += ['--with-lzma']
 
