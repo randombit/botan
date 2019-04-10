14b1bc0cdce84f1344e367e62ae59772a5739b82
Patrick Schmidt <patrick.schmidt@nexenio.com>
2019-04-10 13:29:08 +0200

    add Flatfile_Certificate_Store

---
+++ src/lib/x509/certstor_flatfile/certstor_flatfile.cpp
@@ -0,0 +1,138 @@
+/*
+* Certificate Store
+* (C) 1999-2019 Jack Lloyd
+* (C) 2019      Patrick Schmidt
+*
+* Botan is released under the Simplified BSD License (see license.txt)
+*/
+
+#include <botan/build.h>
+
+#include <botan/certstor_flatfile.h>
+#include <botan/data_src.h>
+#include <botan/pem.h>
+
+namespace Botan {
+namespace {
+std::vector<secure_vector<uint8_t>> decode_all(DataSource& source)
+   {
+   std::vector<secure_vector<uint8_t>> pems;
+
+   while(!source.end_of_data())
+      {
+      std::string label;
+      try
+         {
+         pems.push_back(PEM_Code::decode(source, label));
+         }
+      catch(const Decoding_Error&) {}
+      }
+
+   return pems;
+   }
+}
+
+Flatfile_Certificate_Store::Flatfile_Certificate_Store()
+   {
+   }
+
+Flatfile_Certificate_Store::Flatfile_Certificate_Store(const std::string& file)
+   {
+   if(file.empty())
+      {
+      return;
+      }
+
+   DataSource_Stream file_stream(file);
+
+   for(const secure_vector<uint8_t> der : decode_all(file_stream))
+      {
+      std::shared_ptr<const X509_Certificate> cert = std::make_shared<const X509_Certificate>(der.data(), der.size());
+
+      m_all_subjects.push_back(cert->subject_dn());
+      m_dn_to_cert.emplace(cert->subject_dn(), cert);
+      m_pubkey_sha1_to_cert.emplace(cert->subject_public_key_bitstring_sha1(), cert);
+      m_subject_dn_sha256_to_cert.emplace(cert->raw_subject_dn_sha256(), cert);
+      }
+   }
+
+std::vector<X509_DN> Flatfile_Certificate_Store::all_subjects() const
+   {
+   return m_all_subjects;
+   }
+
+std::shared_ptr<const X509_Certificate>
+Flatfile_Certificate_Store::find_cert(const X509_DN& subject_dn,
+                                      const std::vector<uint8_t>& key_id) const
+   {
+   std::vector<std::shared_ptr<const X509_Certificate>> found_certs = find_all_certs(subject_dn, key_id);
+
+   return !found_certs.empty() ? found_certs.front() : nullptr;
+   }
+
+std::vector<std::shared_ptr<const X509_Certificate>> Flatfile_Certificate_Store::find_all_certs(
+         const X509_DN& subject_dn,
+         const std::vector<uint8_t>& key_id) const
+   {
+   const auto found_range = m_dn_to_cert.equal_range(subject_dn);
+
+   if(found_range.first == m_dn_to_cert.end())
+      {
+      return {};
+      }
+
+   std::vector<std::shared_ptr<const X509_Certificate>> found_certs;
+
+   for(auto i = found_range.first; i != found_range.second; ++i)
+      {
+      const std::shared_ptr<const X509_Certificate> cert = i->second;
+
+      if(key_id.empty() || key_id == cert->subject_key_id())
+         {
+         found_certs.push_back(cert);
+         }
+      }
+
+   return found_certs;
+   }
+
+std::shared_ptr<const X509_Certificate>
+Flatfile_Certificate_Store::find_cert_by_pubkey_sha1(const std::vector<uint8_t>& key_hash) const
+   {
+   if(key_hash.size() != 20)
+      {
+      throw Invalid_Argument("Flatfile_Certificate_Store::find_cert_by_pubkey_sha1 invalid hash");
+      }
+
+   auto found_cert = m_pubkey_sha1_to_cert.find(key_hash);
+
+   if(found_cert != m_pubkey_sha1_to_cert.end())
+      {
+      return found_cert->second;
+      }
+
+   return nullptr;
+   }
+
+std::shared_ptr<const X509_Certificate>
+Flatfile_Certificate_Store::find_cert_by_raw_subject_dn_sha256(const std::vector<uint8_t>& subject_hash) const
+   {
+   if(subject_hash.size() != 32)
+      { throw Invalid_Argument("Flatfile_Certificate_Store::find_cert_by_raw_subject_dn_sha256 invalid hash"); }
+
+   auto found_cert = m_subject_dn_sha256_to_cert.find(subject_hash);
+
+   if(found_cert != m_subject_dn_sha256_to_cert.end())
+      {
+      return found_cert->second;
+      }
+
+   return nullptr;
+   }
+
+std::shared_ptr<const X509_CRL> Flatfile_Certificate_Store::find_crl_for(const X509_Certificate& subject) const
+   {
+   BOTAN_UNUSED(subject);
+   return {};
+   }
+}
