cd2805db7d7117bae25da0abad6e730b013d89a0
Hannes Rantzsch <hannes.rantzsch@nexenio.com>
2019-04-25 13:38:10 +0200

    require boost 1.68 for asio stream unit test

--- src/tests/unit_asio_stream.cpp
+++ src/tests/unit_asio_stream.cpp
@@ -10,8 +10,9 @@
 
 #if defined(BOTAN_HAS_TLS) && defined(BOTAN_HAS_BOOST_ASIO)
 
+// first boost version to include boost/beast/experimental/test/stream.hpp
 #include <boost/version.hpp>
-#if BOOST_VERSION >= 106600
+#if BOOST_VERSION >= 106800
 
 #include <botan/asio_stream.h>
 #include <botan/tls_callbacks.h>
