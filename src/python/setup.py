727dec9430123b2aa03f8a421cef67b27edd9d18
Richard Musil <risa2000x@gmail.com>
2019-08-08 13:39:04 +0200

    Removed unnecessary path mangling for the readme file.

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup
 
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'readme.rst'), encoding='utf-8') as f:
-#with open('readme.rst', encoding='utf-8') as f:
+with open('readme.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
