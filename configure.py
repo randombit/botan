e9c552d99d1fcf43b624ab436da1bfc6e00e8543
Jack Lloyd <jack@randombit.net>
2019-10-31 20:48:51 -0400

    Avoid using .PHONY when generating Makefile for Windows
    Since most Windows make tools such as jom and nmake don't support it.
    And a GCC build should be going for MinGW instead.
    
    Closes #2160

--- configure.py
+++ configure.py
@@ -2078,7 +2078,7 @@ def create_template_vars(source_paths, build_paths, options, modules, cc, arch, 
         'cxx': (options.compiler_binary or cc.binary_name),
         'cxx_abi_flags': cc.mach_abi_link_flags(options),
         'linker': cc.linker_name or '$(CXX)',
-        'make_supports_phony': cc.basename != 'msvc',
+        'make_supports_phony': osinfo.basename != 'windows',
 
         'sanitizer_types' : sorted(cc.sanitizer_types),
 
