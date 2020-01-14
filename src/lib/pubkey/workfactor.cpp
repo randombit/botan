71e3d907653002078d7cbe039211a242d76334ba
Jack Lloyd <jack@randombit.net>
2020-01-13 20:16:59 -0500

    Avoid using std::log2 in workfactor.cpp
    If a libc is missing any of the C99 math functions, old versions of
    GCC disable *all* the std:: versions of the C99 functions, even those
    that do exist.
    
    GH #2235

--- src/lib/pubkey/workfactor.cpp
+++ src/lib/pubkey/workfactor.cpp
@@ -18,10 +18,10 @@ size_t ecp_work_factor(size_t bits)
 
 namespace {
 
-size_t nfs_workfactor(size_t bits, double k)
+size_t nfs_workfactor(size_t bits, double log2_k)
    {
-   // approximates natural logarithm of integer of given bitsize
-   const double log2_e = std::log2(std::exp(1));
+   // approximates natural logarithm of an integer of given bitsize
+   const double log2_e = 1.44269504088896340736;
    const double log_p = bits / log2_e;
 
    const double log_log_p = std::log(log_p);
@@ -30,7 +30,7 @@ size_t nfs_workfactor(size_t bits, double k)
    const double est = 1.92 * std::pow(log_p * log_log_p * log_log_p, 1.0/3.0);
 
    // return log2 of the workfactor
-   return static_cast<size_t>(std::log2(k) + log2_e * est);
+   return static_cast<size_t>(log2_k + log2_e * est);
    }
 
 }
@@ -39,7 +39,8 @@ size_t if_work_factor(size_t bits)
    {
    // RFC 3766 estimates k at .02 and o(1) to be effectively zero for sizes of interest
 
-   return nfs_workfactor(bits, .02);
+   const double log2_k = -5.6438; // log2(.02)
+   return nfs_workfactor(bits, log2_k);
    }
 
 size_t dl_work_factor(size_t bits)
@@ -56,9 +57,10 @@ size_t dl_exponent_size(size_t bits)
    of the prime group by 5-6 bits), and always returns at least 128 bits
    (this only matters for very small primes).
    */
-   const size_t MIN_WORKFACTOR = 64;
+   const size_t min_workfactor = 64;
+   const double log2_k = 0;
 
-   return 2 * std::max<size_t>(MIN_WORKFACTOR, nfs_workfactor(bits, 1));
+   return 2 * std::max<size_t>(min_workfactor, nfs_workfactor(bits, log2_k));
    }
 
 }
