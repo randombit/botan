2a3731c912ae6cc2d2c5be6188d15a3002ee2095
Jack Lloyd <jack@randombit.net>
2021-01-26 03:36:44 -0500

    Use an if/else in the batch script

--- src/scripts/ci/setup_appveyor.bat
+++ src/scripts/ci/setup_appveyor.bat
@@ -6,8 +6,11 @@ if %CC% == VC2019p call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Previe
 if %CC% == MinGW set PATH=%PATH%;C:\msys64\mingw64\bin
 
 rem check compiler version
-if %CC% == MinGW g++ -v
-if not %CC% == MinGW cl
+if %CC% == MinGW (
+   g++ -v
+) else (
+   cl
+)
 
 appveyor DownloadFile https://github.com/mozilla/sccache/releases/download/%SCCACHE_VERSION%/sccache-%SCCACHE_VERSION%-x86_64-pc-windows-msvc.tar.gz
 tar -xf sccache-%SCCACHE_VERSION%-x86_64-pc-windows-msvc.tar.gz
