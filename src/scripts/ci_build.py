5204a4deea32c9fa35df9683feea0de5f2447bff
Jack Lloyd <jack@randombit.net>
2021-12-18 11:24:08 -0500

    Add back a GCC 4.8 CI build for release-2 branch

--- src/scripts/ci_build.py
+++ src/scripts/ci_build.py
@@ -132,6 +132,9 @@ def determine_flags(target, target_os, target_cpu, target_cc, cc_bin,
     if target == 'fuzzers':
         flags += ['--unsafe-fuzzer-mode']
 
+    if target == 'gcc4.8':
+        cc_bin = 'g++-4.8'
+
     if target in ['fuzzers', 'coverage']:
         flags += ['--build-fuzzers=test']
 
@@ -550,7 +553,7 @@ def main(args=None):
                          os.path.join(root_dir, 'fuzzer_corpus'),
                          os.path.join(root_dir, 'build/fuzzer')])
 
-        if target in ['shared', 'coverage'] and options.os != 'windows':
+        if target in ['shared', 'gcc4.8', 'coverage'] and options.os != 'windows':
             botan_exe = os.path.join(root_dir, 'botan-cli.exe' if options.os == 'windows' else 'botan')
 
             args = ['--threads=%d' % (options.build_jobs)]
@@ -571,7 +574,7 @@ def main(args=None):
                 if use_python3:
                     cmds.append(['python3', '-b', python_tests])
 
-        if target in ['shared', 'static', 'bsi', 'nist']:
+        if target in ['shared', 'static', 'gcc4.8', 'bsi', 'nist']:
             cmds.append(make_cmd + ['install'])
             build_config = os.path.join(root_dir, 'build', 'build_config.json')
             cmds.append([py_interp, os.path.join(root_dir, 'src/scripts/ci_check_install.py'), build_config])
