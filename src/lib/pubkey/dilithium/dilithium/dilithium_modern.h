951ef1a04356b535b0aeafdc9a78a825944934bf
Michael Boric <michael.boric@rohde-schwarz.com>
2022-05-05 10:38:05 +0200

    Add Dilithium deterministic/randomized with Keccak or AES
    Compatible with: pq-crystals/dilithium 3e9b9f1412f6c7435dbeb4e10692ea58f181ee51
    
    This uses OIDs that are registered under Botan's private arc.
    
    Co-authored-by: Manuel Glaser <Manuel.Glaser@rohde-schwarz.com>
    Co-authored-by: René Meusel <Rene.Meusel@rohde-schwarz.com>

---
+++ src/lib/pubkey/dilithium/dilithium/dilithium_modern.h
@@ -0,0 +1,55 @@
+/*
+* Asymmetric primitives for dilithium
+* (C) 2022 Jack Lloyd
+* (C) 2022 Manuel Glaser, Michael Boric, René Meusel - Rohde & Schwarz Cybersecurity
+*
+* Botan is released under the Simplified BSD License (see license.txt)
+*/
+
+#ifndef BOTAN_DILITHIUM_COMMON_SYM_PRIMITIVES_H_
+#define BOTAN_DILITHIUM_COMMON_SYM_PRIMITIVES_H_
+
+#include <botan/internal/dilithium_symmetric_primitives.h>
+
+#include <botan/internal/shake.h>
+#include <botan/internal/shake_cipher.h>
+
+#include <array>
+#include <memory>
+#include <vector>
+
+namespace Botan {
+
+class Dilithium_Common_Symmetric_Primitives : public Dilithium_Symmetric_Primitives
+   {
+   public:
+      std::unique_ptr<StreamCipher> XOF(const XofType type, std::span<const uint8_t> seed,
+                                        uint16_t nonce) const override
+         {
+         // Input is a concatination of seed | nonce used as input for shake128
+         std::vector<uint8_t> input;
+         input.reserve(seed.size() + 2);
+         input.insert(input.end(), seed.begin(), seed.end());
+         input.push_back(static_cast<uint8_t>(nonce));
+         input.push_back(static_cast<uint8_t>(nonce >> 8));
+
+         std::unique_ptr<StreamCipher> cipher;
+         switch(type)
+            {
+            case XofType::k128:
+               cipher = std::make_unique<SHAKE_128_Cipher>();
+               break;
+            case XofType::k256:
+               cipher = std::make_unique<SHAKE_256_Cipher>();
+               break;
+            }
+
+         cipher->set_key(input);
+
+         return cipher;
+         }
+   };
+
+} // namespace Botan
+
+#endif
---
+++ src/lib/pubkey/dilithium/dilithium_aes/dilithium_aes.h
@@ -0,0 +1,51 @@
+/*
+* Asymmetric primitives for dilithium AES
+* (C) 2022 Jack Lloyd
+* (C) 2022 Manuel Glaser, Michael Boric, René Meusel - Rohde & Schwarz Cybersecurity
+*
+* Botan is released under the Simplified BSD License (see license.txt)
+*/
+
+#ifndef BOTAN_DILITHIUM_AES_SYM_PRIMITIVES_H_
+#define BOTAN_DILITHIUM_AES_SYM_PRIMITIVES_H_
+
+#include <botan/internal/dilithium_symmetric_primitives.h>
+
+#include <botan/stream_cipher.h>
+#include <botan/internal/loadstor.h>
+
+#include <array>
+#include <memory>
+#include <vector>
+
+namespace Botan {
+
+class Dilithium_AES_Symmetric_Primitives : public Dilithium_Symmetric_Primitives
+   {
+   public:
+      // AES mode always uses AES-256, regardless of the XofType
+      std::unique_ptr<StreamCipher> XOF(const XofType /* type */, std::span<const uint8_t> seed,
+                                        uint16_t nonce) const final
+         {
+         auto cipher = StreamCipher::create_or_throw("CTR(AES-256)");
+
+         // Algorithm Spec V. 3.1 Section 5.3
+         //    In the AES variant, the first 32 bytes of rhoprime are used as
+         //    the key and i is extended to a 12 byte nonce for AES-256 in
+         //    counter mode.
+         //
+         // I.e. when the XOF is used in "ExpandS" `seed` (aka rhoprime) will be
+         // 64 bytes long and must be truncated to the 32 most significant bytes.
+         BOTAN_ASSERT_NOMSG(seed.size() >= cipher->key_spec().minimum_keylength());
+         cipher->set_key(seed.data(), cipher->key_spec().minimum_keylength());
+
+         // The nonce is padded as needed by the CTR_BE mode
+         std::array<uint8_t, 2> iv { get_byte<1>(nonce), get_byte<0>(nonce) };
+         cipher->set_iv(iv.data(), iv.size());
+         return cipher;
+         }
+   };
+
+} // namespace Botan
+
+#endif
