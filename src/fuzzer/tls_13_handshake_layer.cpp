64863501519a17a3e25ad50ce39a1c6562c7582a
René Meusel <rene.meusel@nexenio.com>
2022-05-19 11:47:53 +0200

    add a simple fuzz target for TLS 1.3 handshake messages
    Co-Authored-By: Lukas Zeller <lukas.zeller@nexenio.com>

---
+++ src/fuzzer/tls_13_handshake_layer.cpp
@@ -0,0 +1,40 @@
+/*
+* (C) 2022 Jack Lloyd
+* (C) 2002 René Meusel - neXenio GmbH
+*
+* Botan is released under the Simplified BSD License (see license.txt)
+*/
+
+#include "fuzzers.h"
+#include <botan/internal/tls_handshake_layer_13.h>
+#include <botan/internal/tls_transcript_hash_13.h>
+
+
+namespace {
+
+Botan::TLS::Handshake_Layer prepare(const std::vector<uint8_t>& data)
+   {
+   Botan::TLS::Handshake_Layer hl(Botan::TLS::Connection_Side::CLIENT);
+   hl.copy_data(data);
+   return hl;
+   }
+
+}  // namespace;
+
+
+void fuzz(const uint8_t in[], size_t len)
+   {
+   static Botan::TLS::Default_Policy policy;
+
+   try
+      {
+      std::vector<uint8_t> v(in, in + len);
+      auto hl1 = prepare(v);
+      Botan::TLS::Transcript_Hash_State ths("SHA-256");
+      while (hl1.next_message(policy, ths).has_value()) {};
+
+      auto hl2 = prepare(v);
+      while (hl2.next_post_handshake_message(policy).has_value()) {};
+      }
+   catch(Botan::Exception& e) {}
+   }
