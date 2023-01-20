2c8a8561fa777176a9161b991d54e2ad1f9e6330
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2023-01-20 16:07:59 -0500

    Make the ZFEC C API conditional on BOTAN_HAS_ZFEC.
    Return a not implemented error code if it is unavailable.

--- src/lib/ffi/ffi_zfec.cpp
+++ src/lib/ffi/ffi_zfec.cpp
@@ -5,20 +5,28 @@
 */
 
 #include <botan/ffi.h>
-#include <botan/zfec.h>
 #include <botan/internal/ffi_util.h>
+
+#if defined(BOTAN_HAS_ZFEC)
+#include <botan/zfec.h>
+#endif
 
 extern "C" {
   int botan_zfec_encode(size_t K, size_t N, const uint8_t *input, size_t size, uint8_t **outputs) {
+#if defined(BOTAN_HAS_ZFEC)
     return Botan_FFI::ffi_guard_thunk(__func__, [=]() -> int {
       Botan::ZFEC(K, N).encode(input, size, [=](size_t index, const uint8_t block[], size_t blockSize) -> void {
 	  std::copy(block, block + blockSize, outputs[index]);
 	});
       return BOTAN_FFI_SUCCESS;
     });
+#else
+    return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
   }
 
   int botan_zfec_decode(size_t K, size_t N, const size_t *indexes, uint8_t *const*const inputs, size_t shareSize, uint8_t **outputs) {
+#if defined(BOTAN_HAS_ZFEC)
     return Botan_FFI::ffi_guard_thunk(__func__, [=]() -> int {
       std::map<size_t, const uint8_t*> shares;
       for (size_t k = 0; k < K; ++k) {
@@ -29,5 +37,8 @@ extern "C" {
 	});
       return BOTAN_FFI_SUCCESS;
     });
+#else
+    return BOTAN_FFI_ERROR_NOT_IMPLEMENTED;
+#endif
   }
 }
