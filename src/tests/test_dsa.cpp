4a21edba982a2d54ba71312896b9030ec21e659b
Jack Lloyd <jack@randombit.net>
2023-02-22 02:19:50 -0500

    Test DSA/ECDSA verification both ways
    Previously if RFC 6979 was enabled, we would test only that file.
    And if it was disabled, we would test only the CAVS file.
    
    Now test signature *generation* using whichever method is enabled,
    but also test verification of the signatures from the other method.

--- src/tests/test_dsa.cpp
+++ src/tests/test_dsa.cpp
@@ -54,6 +54,45 @@ class DSA_KAT_Tests final : public PK_Signature_Generation_Test
          }
    };
 
+class DSA_KAT_Verification_Tests final : public PK_Signature_Verification_Test
+   {
+   public:
+      DSA_KAT_Verification_Tests() : PK_Signature_Verification_Test(
+            "DSA",
+#if !defined(BOTAN_HAS_RFC6979_GENERATOR)
+            "pubkey/dsa_rfc6979.vec",
+            "P,Q,G,X,Hash,Msg,Signature",
+#else
+            "pubkey/dsa_prob.vec",
+            "P,Q,G,X,Hash,Msg,Nonce,Signature",
+#endif
+            "") {}
+
+      bool clear_between_callbacks() const override
+         {
+         return false;
+         }
+
+      std::unique_ptr<Botan::Public_Key> load_public_key(const VarMap& vars) override
+         {
+         const Botan::BigInt p = vars.get_req_bn("P");
+         const Botan::BigInt q = vars.get_req_bn("Q");
+         const Botan::BigInt g = vars.get_req_bn("G");
+         const Botan::BigInt x = vars.get_req_bn("X");
+
+         const Botan::DL_Group grp(p, q, g);
+
+         const Botan::DSA_PrivateKey priv_key(Test::rng(), grp, x);
+
+         return priv_key.public_key();
+         }
+
+      std::string default_padding(const VarMap& vars) const override
+         {
+         return "EMSA1(" + vars.get_req_str("Hash") + ")";
+         }
+   };
+
 class DSA_Verification_Tests final : public PK_Signature_Verification_Test
    {
    public:
@@ -98,8 +137,9 @@ class DSA_Keygen_Tests final : public PK_Key_Generation_Test
          }
    };
 
-BOTAN_REGISTER_TEST("pubkey", "dsa_sign", DSA_KAT_Tests);
-BOTAN_REGISTER_TEST("pubkey", "dsa_verify", DSA_Verification_Tests);
+BOTAN_REGISTER_TEST("pubkey", "dsa_kat_sign", DSA_KAT_Tests);
+BOTAN_REGISTER_TEST("pubkey", "dsa_kat_verify", DSA_KAT_Verification_Tests);
+BOTAN_REGISTER_TEST("pubkey", "dsa_misc_verify", DSA_Verification_Tests);
 BOTAN_REGISTER_TEST("pubkey", "dsa_keygen", DSA_Keygen_Tests);
 
 #endif
