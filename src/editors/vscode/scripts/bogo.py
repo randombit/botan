7fd1edae62352606abad51e752b343908d64ee0f
Rene Meusel <rene.meusel@rohde-schwarz.com>
2023-03-13 15:51:52 +0100

    Chore: Update BoringSSL's test suite to latest master
    Note that a few tests needed to be disabled, but no additional failures
    were found. In a future release, we might consider to build explicit
    support for a FIPS 202205 policy.

--- src/editors/vscode/scripts/bogo.py
+++ src/editors/vscode/scripts/bogo.py
@@ -6,7 +6,7 @@ from common import run_cmd, get_concurrency
 
 
 BORING_REPO = "https://github.com/reneme/boringssl.git"
-BORING_BRANCH = "rene/runner-20220322"
+BORING_BRANCH = "rene/runner-20230313"
 
 BORING_PATH = "build_deps/boringssl"
 BOGO_PATH = os.path.join(BORING_PATH, "ssl", "test", "runner")
