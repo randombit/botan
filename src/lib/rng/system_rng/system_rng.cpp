caf0aec7aa708e387da79e607003ef7f994b1234
Oliver Collyer <ovcollyer@mac.com>
2023-03-26 13:35:31 +0100

    Fixed bcrypt WinRT build

--- src/lib/rng/system_rng/system_rng.cpp
+++ src/lib/rng/system_rng/system_rng.cpp
@@ -17,7 +17,9 @@
 #if defined(BOTAN_TARGET_OS_HAS_RTLGENRANDOM)
   #include <botan/internal/dyn_load.h>
 #elif defined(BOTAN_TARGET_OS_HAS_CRYPTO_NG)
+  #include <windows.h>
   #include <bcrypt.h>
+  #define STATUS_SUCCESS 0x00000000
 #elif defined(BOTAN_TARGET_OS_HAS_CCRANDOM)
   #include <CommonCrypto/CommonRandom.h>
 #elif defined(BOTAN_TARGET_OS_HAS_ARC4RANDOM)
