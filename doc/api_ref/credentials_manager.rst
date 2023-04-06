38c284ee4d04c998f85cc168cca097acf007b27b
Rene Meusel <rene.meusel@rohde-schwarz.com>
2023-04-06 09:33:05 +0200

    credentials_manager.rst

--- doc/api_ref/credentials_manager.rst
+++ doc/api_ref/credentials_manager.rst
@@ -5,193 +5,34 @@ Credentials Manager
 A ``Credentials_Manager`` is a way to abstract how the application
 stores credentials. The main user is the :doc:`tls` implementation.
 
-.. cpp:class:: Credentials_Manager
+Certificate-based Authentication
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-   .. cpp:function:: std::vector<Certificate_Store*> \
-         trusted_certificate_authorities( \
-         const std::string& type, \
-         const std::string& context)
+The ``Credentials_Manager`` provides hooks for applications to control the
+certificates used in TLS. Both for authenticating themselves and to define the
+trusted certificate authorities.
 
-      Return the list of certificate stores, each of which is assumed
-      to contain (only) trusted certificate authorities. The
-      ``Credentials_Manager`` retains ownership of the
-      Certificate_Store pointers.
+API Overview
+~~~~~~~~~~~~
 
-      .. note::
+.. container:: toggle
 
-         It would have been a better API to return a vector of
-         ``shared_ptr`` here.  This may change in a future major release.
-
-      When *type* is "tls-client", *context* will be the hostname of
-      the server, or empty if the hostname is not known. This allows
-      using a different set of certificate stores in different contexts,
-      for example using the system certificate store unless contacting
-      one particular server which uses a cert issued by an internal CA.
-
-      When *type* is "tls-server", the *context* will again be the
-      hostname of the server, or empty if the client did not send a
-      server name indicator. For TLS servers, these CAs are the ones
-      trusted for signing of client certificates. If you do not want
-      the TLS server to ask for a client cert,
-      ``trusted_certificate_authorities`` should return an empty list
-      for *type* "tls-server".
-
-      The default implementation returns an empty list.
-
-   .. cpp:function:: std::vector<X509_Certificate> find_cert_chain( \
-                     const std::vector<std::string>& cert_key_types, \
-                     const std::vector<X509_DN>& acceptable_CAs, \
-                     const std::string& type, \
-                     const std::string& context)
-
-      Return the certificate chain to use to identify ourselves. The
-      ``acceptable_CAs`` parameter gives a list of CAs the peer trusts.
-      This may be empty.
-
-      .. warning::
-         If this function returns a certificate that is not one of the
-         types given in ``cert_key_types`` confusing handshake
-         failures will result.
-
-   .. cpp:function:: std::vector<X509_Certificate> cert_chain( \
-         const std::vector<std::string>& cert_key_types, \
-         const std::string& type, \
-         const std::string& context)
-
-      Return the certificate chain to use to identify ourselves. Starting in
-      2.5, prefer ``find_cert_chain`` which additionally provides the CA list.
-
-   .. cpp:function:: std::vector<X509_Certificate> cert_chain_single_type( \
-         const std::string& cert_key_type, \
-         const std::string& type, \
-         const std::string& context)
-
-      Return the certificate chain to use to identifier ourselves, if
-      we have one of type *cert_key_type* and we would like to use a
-      certificate in this *type*/*context*.
-
-      For servers *type* will be "tls-server" and the *context* will
-      be the server name that the client requested via SNI (or empty,
-      if the client did not send SNI).
-
-      .. warning::
-
-         To avoid cross-protocol attacks it is recommended that if a server
-         receives an SNI request for a name it does not expect, it should close
-         the connection with an alert. This can be done by throwing an exception
-         from the implementation of this function.
-
-   .. cpp:function:: std::shared_ptr<Private_Key> private_key_for(const X509_Certificate& cert, \
-                                                  const std::string& type, \
-                                                  const std::string& context)
-
-      Return a shared pointer to the private key for this certificate. The
-      *cert* will be the leaf cert of a chain returned previously by
-      ``cert_chain`` or ``cert_chain_single_type``.
-
-In versions before 1.11.34, there was an additional function on `Credentials_Manager`
-
-   .. cpp::function:: void verify_certificate_chain( \
-         const std::string& type, \
-         const std::string& hostname, \
-         const std::vector<X509_Certificate>& cert_chain)
-
-This function has been replaced by `TLS::Callbacks::tls_verify_cert_chain`.
-
-SRP Authentication
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-``Credentials_Manager`` contains the hooks used by TLS clients and
-servers for SRP authentication.
-
-.. note::
-
-   Support for TLS-SRP is deprecated, and will be removed in a future
-   major release. When that occurs these APIs will be removed. Prefer
-   instead performing a standard TLS handshake, then perform a PAKE
-   authentication inside of (and cryptographically bound to) the TLS
-   channel.
-
-.. cpp:function:: bool attempt_srp(const std::string& type, \
-                                   const std::string& context)
-
-   Returns if we should consider using SRP for authentication
-
-.. cpp:function:: std::string srp_identifier(const std::string& type, \
-                                             const std::string& context)
-
-   Returns the SRP identifier we'd like to use (used by client)
-
-.. cpp:function:: std::string srp_password(const std::string& type, \
-                                           const std::string& context, \
-                                           const std::string& identifier)
-
-   Returns the password for *identifier* (used by client)
-
-.. cpp:function:: bool srp_verifier(const std::string& type, \
-                                    const std::string& context, \
-                                    const std::string& identifier, \
-                                    std::string& group_name, \
-                                    BigInt& verifier, \
-                                    std::vector<uint8_t>& salt, \
-                                    bool generate_fake_on_unknown)
-
-    Returns the SRP verifier information for *identifier* (used by server)
+   .. doxygenclass:: Botan::Credentials_Manager
+      :members: trusted_certificate_authorities,find_cert_chain,cert_chain,cert_chain_single_type,private_key_for
 
 Preshared Keys
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-TLS supports the use of pre shared keys for authentication.
+TLS supports the use of pre shared keys for authentication. Note that in
+Botan 3.0 we support PSK based authentication in TLS 1.2 only. Support for
+TLS 1.3 might come in a later release. Using the ``Credentials_Manager``
+applications can define the PSK secret to be used.
 
-.. cpp:function:: SymmetricKey psk(const std::string& type, \
-                                   const std::string& context, \
-                                   const std::string& identity)
+API Overview
+~~~~~~~~~~~~
 
-    Return a symmetric key for use with *identity*
+.. container:: toggle
 
-    One important special case for ``psk`` is where *type* is
-    "tls-server", *context* is "session-ticket" and *identity* is an
-    empty string. If a key is returned for this case, a TLS server
-    will offer session tickets to clients who can use them, and the
-    returned key will be used to encrypt the ticket. The server is
-    allowed to change the key at any time (though changing the key
-    means old session tickets can no longer be used for resumption,
-    forcing a full re-handshake when the client next connects). One
-    simple approach to add support for session tickets in your server
-    is to generate a random key the first time ``psk`` is called to
-    retrieve the session ticket key, cache it for later use in the
-    ``Credentials_Manager``, and simply let it be thrown away when the
-    process terminates. See :rfc:`4507` for more information about TLS
-    session tickets.
-
-    A similar special case exists for DTLS cookie verification. In
-    this case *type* will be "tls-server" and *context* is
-    "dtls-cookie-secret". If no key is returned, then DTLS cookies are
-    not used. Similar to the session ticket key, the DTLS cookie
-    secret can be chosen during server startup and rotated at any time
-    with no ill effect.
-
-    .. warning::
-
-       If DTLS cookies are not used then the server is prone to be
-       abused as a DoS amplifier, where the attacker sends a
-       relatively small client hello in a UDP packet with a forged
-       return address, and then the server replies to the victim with
-       several messages that are larger. This not only hides the
-       attackers address from the victim, but increases their
-       effective bandwidth. This is not an issue when using DTLS over
-       SCTP or TCP.
-
-.. cpp:function:: std::string psk_identity_hint(const std::string& type, \
-                                                const std::string& context)
-
-    Returns an identity hint which may be provided to the client. This
-    can help a client understand what PSK to use.
-
-.. cpp:function:: std::string psk_identity(const std::string& type, \
-                                           const std::string& context, \
-                                           const std::string& identity_hint)
-
-    Returns the identity we would like to use given this *type* and
-    *context* and the optional *identity_hint*. Not all servers or
-    protocols will provide a hint.
+   .. doxygenclass:: Botan::Credentials_Manager
+      :members: psk,psk_identity_hint,psk_identity
+      :no-link:
--- src/lib/tls/credentials_manager.h
+++ src/lib/tls/credentials_manager.h
@@ -35,34 +35,55 @@ class BOTAN_PUBLIC_API(2,0) Credentials_Manager
 
       /**
       * Return a list of the certificates of CAs that we trust in this
-      * type/context.
+      * type/context. The ``Credentials_Manager`` retains ownership of the
+      * Certificate_Store pointers.
+      *
+      * @note It would have been a better API to return a vector of `shared_ptr`
+      *       here.  This may change in a future major release.
+      *
+      * When @p type is "tls-client", @p context will be the hostname of the
+      * server, or empty if the hostname is not known. This allows using a
+      * different set of certificate stores in different contexts, for example
+      * using the system certificate store unless contacting one particular
+      * server which uses a cert issued by an internal CA.
+      *
+      * When @p type is "tls-server", the @p context will again be the hostname
+      * of the server, or empty if the client did not send a server name
+      * indicator. For TLS servers, these CAs are the ones trusted for signing
+      * of client certificates. If you do not want the TLS server to ask for a
+      * client cert, trusted_certificate_authorities() should return an empty
+      * list for @p type "tls-server".
+      *
+      * The default implementation returns an empty list.
       *
       * @param type specifies the type of operation occurring
-      *
-      * @param context specifies a context relative to type. For instance
-      *        for type "tls-client", context specifies the servers name.
+      * @param context specifies a context relative to type. For instance for
+      *        type "tls-client", context specifies the servers name.
       */
       virtual std::vector<Certificate_Store*> trusted_certificate_authorities(
          const std::string& type,
          const std::string& context);
 
       /**
-      * Return a cert chain we can use, ordered from leaf to root,
-      * or else an empty vector.
+      * Return a certificate chain we can use to identify ourselves, ordered
+      * from leaf to root, or else an empty vector.
       *
-      * It is assumed that the caller can get the private key of the
-      * leaf with private_key_for
+      * It is assumed that the caller can get the private key of the leaf with
+      * private_key_for()
+      *
+      * @warning  If this function returns a certificate that is not one of the
+      *           types given in @p cert_key_types confusing handshake failures
+      *           will result.
       *
       * For a comprehensive write-up of how to select certificates for TLS
       * CertificateVerify messages, see RFC 8446 Sections 4.4.2.2 and 4.4.2.3.
       *
-      * @param cert_key_types specifies the key types desired ("RSA",
-      *                       "DSA", "ECDSA", etc), or empty if there
-      *                       is no preference by the caller.
-      * @param cert_signature_schemes specifies the signature types desired
-      *                               as signatures in the certificate(s) itself,
+      * @param cert_key_types specifies the key types desired ("RSA", "DSA",
+      *                       "ECDSA", etc), or empty if there is no preference
+      *                       by the caller.
+      * @param cert_signature_schemes specifies the signature types desired as
+      *                               signatures in the certificate(s) itself,
       *                               or empty for no preference by the caller.
-      *
       * @param acceptable_CAs the CAs the requestor will accept (possibly empty)
       * @param type specifies the type of operation occurring
       * @param context specifies a context relative to type.
@@ -75,24 +96,23 @@ class BOTAN_PUBLIC_API(2,0) Credentials_Manager
          const std::string& context);
 
       /**
-      * Return a cert chain we can use, ordered from leaf to root,
-      * or else an empty vector.
+      * Return a certificate chain we can use to identify ourselves, ordered
+      * from leaf to root, or else an empty vector.
       *
-      * This virtual function is deprecated, and will be removed in a
-      * future release. Use (and override) find_cert_chain instead.
+      * @deprecated This virtual function is deprecated, and will be removed in
+      *             a future release. Use (and override) find_cert_chain()
+      *             instead.
       *
-      * It is assumed that the caller can get the private key of the
-      * leaf with private_key_for
+      * It is assumed that the caller can get the private key of the leaf with
+      * private_key_for()
       *
-      * @param cert_key_types specifies the key types desired ("RSA",
-      *                       "DSA", "ECDSA", etc), or empty if there
-      *                       is no preference by the caller.
-      * @param cert_signature_schemes specifies the signature types desired
-      *                               as signatures in the certificate(s) itself,
+      * @param cert_key_types specifies the key types desired ("RSA", "DSA",
+      *                       "ECDSA", etc), or empty if there is no preference
+      *                       by the caller.
+      * @param cert_signature_schemes specifies the signature types desired as
+      *                               signatures in the certificate(s) itself,
       *                               or empty for no preference by the caller.
-      *
       * @param type specifies the type of operation occurring
-      *
       * @param context specifies a context relative to type.
       */
       virtual std::vector<X509_Certificate> cert_chain(
@@ -102,20 +122,30 @@ class BOTAN_PUBLIC_API(2,0) Credentials_Manager
          const std::string& context);
 
       /**
-      * Return a cert chain we can use, ordered from leaf to root,
-      * or else an empty vector.
+      * Return a certificate chain we can use to identify ourselves, ordered
+      * from leaf to root, or else an empty vector. Override this if we have one
+      * certificate of type @p cert_key_type and we would like to use a
+      * certificate in this type and context.
       *
-      * It is assumed that the caller can get the private key of the
-      * leaf with private_key_for
+      * For servers @p type will be "tls-server" and the @p context will be the
+      * server name that the client requested via SNI (or empty, if the client
+      * did not send SNI).
       *
-      * @param cert_key_type specifies the type of key requested
-      *                      ("RSA", "DSA", "ECDSA", etc)
-      * @param cert_signature_schemes specifies the signature types desired
-      *                               as signatures in the certificate(s) itself,
+      * @warning To avoid cross-protocol attacks it is recommended that if a
+      *          server receives an SNI request for a name it does not expect,
+      *          it should close the connection with an alert. This can be done
+      *          by throwing an exception from the implementation of this
+      *          function.
+      *
+      * It is assumed that the caller can get the private key of the leaf with
+      * private_key_for()
+      *
+      * @param cert_key_type specifies the type of key requested ("RSA", "DSA",
+      *                      "ECDSA", etc)
+      * @param cert_signature_schemes specifies the signature types desired as
+      *                               signatures in the certificate(s) itself,
       *                               or empty for no preference by the caller.
-      *
       * @param type specifies the type of operation occurring
-      *
       * @param context specifies a context relative to type.
       */
       std::vector<X509_Certificate> cert_chain_single_type(
@@ -125,10 +155,14 @@ class BOTAN_PUBLIC_API(2,0) Credentials_Manager
          const std::string& context);
 
       /**
-      * @return private key associated with this certificate if we should
-      *         use it with this context. cert was returned by cert_chain
-      * This function should either return null or throw an exception if
-      * the key is unavailable.
+      * Return a `shared_ptr` to the private key for this certificate. The
+      * @p cert will be the leaf cert of a chain returned previously by
+      * find_cert_chain() or cert_chain_single_type().
+      *
+      * @return private key associated with this certificate if we should use it
+      *         with this context. cert was returned by cert_chain This function
+      *         should either return null or throw an exception if the key is
+      *         unavailable.
       */
       virtual std::shared_ptr<Private_Key>
          private_key_for(const X509_Certificate& cert,
@@ -136,6 +170,9 @@ class BOTAN_PUBLIC_API(2,0) Credentials_Manager
                          const std::string& context);
 
       /**
+      * Returns an identity hint which may be provided to the client. This can
+      * help a client understand what PSK to use.
+      *
       * @param type specifies the type of operation occurring
       * @param context specifies a context relative to type.
       * @return the PSK identity hint for this type/context
@@ -144,6 +181,10 @@ class BOTAN_PUBLIC_API(2,0) Credentials_Manager
                                             const std::string& context);
 
       /**
+      * Returns the identity we would like to use given this @p type and
+      * @p context and the optional @p identity_hint. Not all servers or
+      * protocols will provide a hint.
+      *
       * @param type specifies the type of operation occurring
       * @param context specifies a context relative to type.
       * @param identity_hint was passed by the server (but may be empty)
@@ -154,12 +195,43 @@ class BOTAN_PUBLIC_API(2,0) Credentials_Manager
                                        const std::string& identity_hint);
 
       /**
+      * Return a symmetric key for use with @p identity
+      *
+      * One important special case for this method is where @p type is
+      * "tls-server", @p context is "session-ticket" and @p identity is an empty
+      * string. If a key is returned for this case, a TLS server will offer
+      * session tickets to clients who can use them, and the returned key will
+      * be used to encrypt the ticket. The server is allowed to change the key
+      * at any time (though changing the key means old session tickets can no
+      * longer be used for resumption, forcing a full re-handshake when the
+      * client next connects). One simple approach to add support for session
+      * tickets in your server is to generate a random key the first time psk()
+      * is called to retrieve the session ticket key, cache it for later use in
+      * the Credentials_Manager, and simply let it be thrown away when the
+      * process terminates. See RFC 4507 or RFC 8446 for more information about
+      * TLS session tickets.
+      *
+      * A similar special case exists for DTLS cookie verification. In this case
+      * @p type will be "tls-server" and @p context is "dtls-cookie-secret". If
+      * no key is returned, then DTLS cookies are not used. Similar to the
+      * session ticket key, the DTLS cookie secret can be chosen during server
+      * startup and rotated at any time with no ill effect.
+      *
+      * @warning If DTLS cookies are not used then the server is prone to be
+      *          abused as a DoS amplifier, where the attacker sends a
+      *          relatively small client hello in a UDP packet with a forged
+      *          return address, and then the server replies to the victim with
+      *          several messages that are larger. This not only hides the
+      *          attackers address from the victim, but increases their
+      *          effective bandwidth. This is not an issue when using DTLS over
+      *          SCTP or TCP.
+      *
       * @param type specifies the type of operation occurring
       * @param context specifies a context relative to type.
-      * @param identity is a PSK identity previously returned by
-               psk_identity for the same type and context.
-      * @return the PSK used for identity, or throw an exception if no
-      * key exists
+      * @param identity is a PSK identity previously returned by psk_identity
+      *                 for the same type and context.
+      * @return the PSK used for identity, or throw an exception if no key
+      *         exists
       */
       virtual SymmetricKey psk(const std::string& type,
                                const std::string& context,
