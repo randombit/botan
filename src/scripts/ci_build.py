9cb6dbc16f28ef7c07fc924264a214be7c0bb8c4
Jack Lloyd <jack@randombit.net>
2023-06-01 09:39:55 -0400

    Disable some more tests in the standard valgrind run [ci skip]
    This is a lot more palatable since now we do a full run every night.

--- src/scripts/ci_build.py
+++ src/scripts/ci_build.py
@@ -193,15 +193,17 @@ def determine_flags(target, target_os, target_cpu, target_cc, cc_bin, ccache,
         test_cmd += ['--test-threads=1']
 
         if target != 'valgrind-full':
-            # valgrind is slow
+            # valgrind is slow, so some tests only run in the nightly check
             slow_tests = [
-                'argon2', 'bcrypt', 'bcrypt_pbkdf', 'compression', 'cryptobox',
+                'argon2', 'bcrypt', 'bcrypt_pbkdf', 'compression_tests', 'cryptobox',
                 'dh_invalid', 'dh_kat', 'dh_keygen', 'dl_group_gen', 'dlies',
                 'dsa_kat_verify', 'dsa_param', 'ecc_basemul', 'ecdsa_verify_wycheproof',
                 'ed25519_sign', 'elgamal_decrypt', 'elgamal_encrypt', 'elgamal_keygen',
-                'ffi_dsa', 'ffi_elgamal', 'mce_keygen', 'passhash9', 'pbkdf', 'rsa_encrypt',
-                'rsa_pss', 'rsa_pss_raw', 'scrypt', 'srp6_kat', 'x509_path_bsi',
-                'x509_path_rsa_pss', 'xmss_keygen', 'xmss_keygen_reference', 'xmss_sign']
+                'ffi_dsa', 'ffi_elgamal', 'hash_nist_mc', 'mce_keygen',
+                'passhash9', 'pbkdf', 'pwdhash', 'rsa_encrypt', 'rsa_pss', 'rsa_pss_raw',
+                'scrypt', 'srp6_kat', 'srp6_rt', 'unit_tls', 'x509_path_bsi',
+                'x509_path_rsa_pss', 'xmss_keygen', 'xmss_keygen_reference',
+                'xmss_sign', 'xmss_verify', 'xmss_verify_invalid' ]
 
             disabled_tests += slow_tests
 
