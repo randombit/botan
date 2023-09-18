3e61eba4ea6af316607339e8ad006f2a06a3dfbb
Rene Meusel <rene.meusel@rohde-schwarz.com>
2023-09-18 16:38:34 +0200

    Remove MDx_HashFunction base class

--- src/lib/hash/mdx_hash/mdx_hash.h
+++ src/lib/hash/mdx_hash/mdx_hash.h
@@ -127,52 +127,6 @@ class MerkleDamgard_Hash final {
       AlignmentBuffer<uint8_t, MD::block_bytes> m_buffer;
 };
 
-/**
-* MDx Hash Function Base Class
-*/
-class MDx_HashFunction : public HashFunction {
-   public:
-      /**
-      * @param block_length is the number of bytes per block, which must
-      *        be a power of 2 and at least 8.
-      * @param big_byte_endian specifies if the hash uses big-endian bytes
-      * @param big_bit_endian specifies if the hash uses big-endian bits
-      * @param counter_size specifies the size of the counter var in bytes
-      */
-      MDx_HashFunction(size_t block_length, bool big_byte_endian, bool big_bit_endian, uint8_t counter_size = 8);
-
-      size_t hash_block_size() const final { return m_buffer.size(); }
-
-   protected:
-      void add_data(std::span<const uint8_t> input) final;
-      void final_result(std::span<uint8_t> output) final;
-
-      /**
-      * Run the hash's compression function over a set of blocks
-      * @param blocks the input
-      * @param block_n the number of blocks
-      */
-      virtual void compress_n(const uint8_t blocks[], size_t block_n) = 0;
-
-      void clear() override;
-
-      /**
-      * Copy the output to the buffer
-      * @param buffer to put the output into
-      */
-      virtual void copy_out(uint8_t buffer[]) = 0;
-
-   private:
-      const uint8_t m_pad_char;
-      const uint8_t m_counter_size;
-      const uint8_t m_block_bits;
-      const bool m_count_big_endian;
-
-      uint64_t m_count;
-      secure_vector<uint8_t> m_buffer;
-      size_t m_position;
-};
-
 }  // namespace Botan
 
 #endif
