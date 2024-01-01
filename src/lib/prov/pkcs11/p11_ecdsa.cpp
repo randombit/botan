7b0c59f0c426b8ba3add35fcad00adb15fe2174c
Lars Silvén <lars@pkimodules.com>
2024-01-01 17:54:31 +0100

    Changing from local to public for some parts of the API.
    This is done to make it possible to use none Botan private keys when signing certificates.

--- src/lib/prov/pkcs11/p11_ecdsa.cpp
+++ src/lib/prov/pkcs11/p11_ecdsa.cpp
@@ -10,10 +10,10 @@
 
 #if defined(BOTAN_HAS_ECDSA)
 
+   #include <botan/p11_mechanism.h>
    #include <botan/pk_ops.h>
    #include <botan/rng.h>
    #include <botan/internal/keypair.h>
-   #include <botan/internal/p11_mechanism.h>
 
 namespace Botan::PKCS11 {
 
