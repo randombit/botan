3aa242a870c14a2ad7d678598de5ca2c8c8e7b14
Rene Meusel <rene.meusel@rohde-schwarz.com>
2024-03-25 17:14:23 +0100

    Introduce BufferTransformer

--- src/lib/utils/stl_util.h
+++ src/lib/utils/stl_util.h
@@ -250,6 +250,43 @@ class BufferStuffer {
 };
 
 /**
+ * @brief Helper class combining the BufferStuffer and BufferSlicer
+ *
+ * Use this class to transform data from one buffer to another in a streaming
+ * fashion. The input and output buffers must be of the same size.
+ */
+class BufferTransformer {
+   public:
+      template <size_t count = std::dynamic_extent>
+      using ispan = std::span<const uint8_t, count>;
+
+      template <size_t count = std::dynamic_extent>
+      using ospan = std::span<uint8_t, count>;
+
+   public:
+      BufferTransformer(ispan<> in, ospan<> out) : m_in(in), m_out(out) {
+         BOTAN_ARG_CHECK(in.size() == out.size(), "Input and output buffers must be the same size");
+      }
+
+      std::pair<ispan<>, ospan<>> next(size_t count) { return {m_in.take(count), m_out.next(count)}; }
+
+      template <size_t count>
+      std::pair<ispan<count>, ospan<count>> next() {
+         return {m_in.take<count>(), m_out.next<count>()};
+      }
+
+      void skip(const size_t count) { next(count); }
+
+      size_t remaining() const { return m_in.remaining(); }
+
+      bool empty() const { return m_in.empty(); }
+
+   private:
+      BufferSlicer m_in;
+      BufferStuffer m_out;
+};
+
+/**
  * Concatenate an arbitrary number of buffers.
  * @return the concatenation of \p buffers as the container type of the first buffer
  */
