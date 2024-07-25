302c8c6caba3981f3e6d123fa6d6ad200782f764
Jack Lloyd <jack@randombit.net>
2024-07-25 06:48:37 -0400

    Remove Homebrew workaround for broken Emscripten

--- src/scripts/ci/setup_gh_actions.sh
+++ src/scripts/ci/setup_gh_actions.sh
@@ -140,11 +140,9 @@ else
         boostincdir=$(brew --prefix boost)/include
         echo "BOOST_INCLUDEDIR=$boostincdir" >> "$GITHUB_ENV"
     elif [ "$TARGET" = "emscripten" ]; then
-        # Workaround: emscripten 3.1.63 is broken, install an older one...
-        brew tap-new botan/local-emscripten
-        brew tap --force homebrew/core
-        brew extract --version=3.1.61 emscripten botan/local-emscripten
-        brew install emscripten@3.1.61
+        # Disabled until GH image picks up Emscripten 3.1.64
+        export HOMEBREW_NO_AUTO_UPDATE=0
+        brew install emscripten
     fi
 
     if [ -d '/Applications/Xcode_15.3.app/Contents/Developer' ]; then
