54ef29e5da3303ececc194931e167b62bedb597a
Philipp Weber <philipp.weber.ext@rohde-schwarz.com>
2024-12-16 13:41:38 +0100

    fix formatting

--- src/lib/prov/tpm2/tpm2_rsa/tpm2_rsa.h
+++ src/lib/prov/tpm2/tpm2_rsa/tpm2_rsa.h
@@ -12,10 +12,10 @@
 #include <botan/tpm2_key.h>
 
 namespace Botan::TPM2 {
-    
+
 BOTAN_DIAGNOSTIC_PUSH
 BOTAN_DIAGNOSTIC_IGNORE_INHERITED_VIA_DOMINANCE
-    
+
 class BOTAN_PUBLIC_API(3, 6) RSA_PublicKey final : public virtual Botan::TPM2::PublicKey,
                                                    public virtual Botan::RSA_PublicKey {
    public:
