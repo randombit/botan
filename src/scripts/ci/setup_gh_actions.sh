5c5e91bcb46d5920bf9ba161150ced95a9301745
Jack Lloyd <jack@randombit.net>
2025-01-03 08:39:01 -0500

    Switch to XCode 16 for GH Actions macos-14 build

--- src/scripts/ci/setup_gh_actions.sh
+++ src/scripts/ci/setup_gh_actions.sh
@@ -207,8 +207,8 @@ else
         brew install emscripten@3.1.61
     fi
 
-    if [ -d '/Applications/Xcode_15.3.app/Contents/Developer' ]; then
-        sudo xcrun xcode-select --switch '/Applications/Xcode_15.4.app/Contents/Developer'
+    if [ -d '/Applications/Xcode_16.1.app/Contents/Developer' ]; then
+        sudo xcrun xcode-select --switch '/Applications/Xcode_16.1.app/Contents/Developer'
     else
         sudo xcrun xcode-select --switch '/Applications/Xcode_15.2.app/Contents/Developer'
     fi
