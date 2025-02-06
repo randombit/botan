1b55d750390a5e0c027975c86d1f00b1cebfdfcf
Jack Lloyd <jack@randombit.net>
2025-02-06 07:26:36 -0500

    Remove bogus MSVC test for run_cpu_instruction_probe
    This test looked like it was doing something, but the macros here were the wrong
    ones. During initial implementation of run_cpu_instruction_probe it was thought
    Windows SEH could be used to perform runtime tests, but it cannot. (Or at least
    I could not get it to work.)

--- src/tests/test_os_utils.cpp
+++ src/tests/test_os_utils.cpp
@@ -11,11 +11,6 @@
    #include <botan/internal/os_utils.h>
 #endif
 
-// For __ud2 intrinsic
-#if defined(BOTAN_TARGET_COMPILER_IS_MSVC)
-   #include <intrin.h>
-#endif
-
 namespace Botan_Tests {
 
 #if defined(BOTAN_HAS_OS_UTILS)
@@ -162,13 +157,7 @@ class OS_Utils_Tests final : public Test {
 
          std::function<int()> crash_probe;
 
-   #if defined(BOTAN_TARGET_COMPILER_IS_MSVC)
-         crash_probe = []() noexcept -> int {
-            __ud2();
-            return 3;
-         };
-
-   #elif defined(BOTAN_USE_GCC_INLINE_ASM)
+   #if defined(BOTAN_USE_GCC_INLINE_ASM)
 
       #if defined(BOTAN_TARGET_CPU_IS_X86_FAMILY)
          crash_probe = []() noexcept -> int {
