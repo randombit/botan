b139aafbf74858755771177dbbb8b3a12d682a2a
Jack Lloyd <jack@randombit.net>
2025-02-06 20:40:46 -0500

    Remove BOTAN_TARGET_CPU_HAS_NATIVE_64BIT

--- src/lib/utils/mul128.h
+++ src/lib/utils/mul128.h
@@ -11,7 +11,7 @@
 #include <botan/types.h>
 #include <type_traits>
 
-#if defined(BOTAN_BUILD_COMPILER_IS_MSVC) && defined(BOTAN_TARGET_CPU_HAS_NATIVE_64BIT)
+#if defined(BOTAN_BUILD_COMPILER_IS_MSVC)
    #include <intrin.h>
 #endif
 
