291de8dec3a4503bef3ba80d8e18815802fe612e
Jack Lloyd <jack@randombit.net>
2025-02-14 07:02:01 -0500

    Fix ec_named.cpp generation
    Apparently it was forgotten along the line that this file is supposed to be
    generated by a script, not edited by hand.
    
    Update the script to handle the new macro checks needed for known_named_groups,
    and regenerate the file.

--- src/scripts/dev_tools/gen_ec_groups.py
+++ src/scripts/dev_tools/gen_ec_groups.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-(C) 2021 Jack Lloyd
+(C) 2021,2025 Jack Lloyd
 
 Botan is released under the Simplified BSD License (see license.txt)
 """
@@ -11,7 +11,7 @@ import re
 import datetime
 
 def curve_info(src):
-    re_kv = re.compile('([A-Za-z]+) = ([0-9A-Za-z-_\. ]+)')
+    re_kv = re.compile('([A-Za-z]+) = ([0-9A-Za-z-_\\. ]+)')
 
     current = {}
 
@@ -29,7 +29,7 @@ def curve_info(src):
 
         if key in ['Name']:
             current[key] = val
-        elif key in ['OID']:
+        elif key in ['OID', 'Impl']:
             current[key] = val.split(' ')
         elif key in ['A']:
             if val == '-3':
@@ -44,8 +44,6 @@ def curve_info(src):
             current = {}
 
 def format_int(x):
-    if x.bit_length() <= 5:
-        return str(x)
     return hex(x).upper().replace('0X', '0x')
 
 def print_curve(curve):
@@ -78,16 +76,52 @@ def print_curve(curve):
     return template_str % (name, oid_match, p, a, b, x, y, n, pref_oid)
 
 def format_names(names):
-    for nm in sorted(names):
+    legacy = []
+    generic = []
+    pcurves = []
+    pcurves_no_generic = []
+
+    for (nm, impl) in names:
+        if 'pcurve' in impl:
+            pcurves.append(nm)
+            if 'generic' not in impl:
+                pcurves_no_generic.append(nm)
+        elif 'generic' in impl:
+            generic.append(nm)
+        else:
+            assert 'legacy' in impl
+            legacy.append(nm)
+
+    legacy_macro = "defined(BOTAN_HAS_LEGACY_EC_POINT)"
+    generic_macro = "defined(BOTAN_HAS_PCURVES_GENERIC)"
+    for nm in sorted(pcurves):
+        nm_macro = "BOTAN_HAS_PCURVES_%s" % (nm.upper())
+
+        if nm in pcurves_no_generic:
+            yield "#if defined(%s) || %s" % (nm_macro, legacy_macro)
+            yield "      // Not supported by pcurves_generic"
+        else:
+            yield "#if defined(%s) || %s || %s" % (nm_macro, legacy_macro, generic_macro)
         yield '      \"%s\",' % (nm)
+        yield "#endif\n"
+
+    yield "#if %s || %s" % (legacy_macro, generic_macro)
+    for nm in sorted(generic):
+        yield '      \"%s\",' % (nm)
+    yield "#endif\n"
+
+    yield "#if %s" % (legacy_macro)
+    for nm in sorted(legacy):
+        yield '      \"%s\",' % (nm)
+    yield "#endif\n"
 
 def format_orders(orders):
-    template_str = """   if(low_bits == %s && order == BigInt("%s")) {\n      return OID{%s};\n   }\n""";
+    template_str = """   if(low_bits == 0x%08X && order == BigInt("%s")) {\n      return OID{%s};\n   }\n""";
 
     orders_seen = set([])
 
     for (order,oid) in orders:
-        low_bits = hex(order & 0xFFFFFFFF).upper().replace('0X', '0x')
+        low_bits = order & 0xFFFFFFFF
         order = format_int(order)
         if order in orders_seen:
             raise Exception("Duplicate EC group order %s" % (order))
@@ -100,13 +134,13 @@ def main():
 
     template_str = open('./src/build-data/ec_named.cpp.in').read()
 
-    names = "\n".join(format_names([c['Name'] for c in curves]))
+    names = "\n".join(format_names([(c['Name'], c['Impl']) for c in curves]))
     orders = "\n".join(format_orders([(c['N'], c['OID']) for c in curves]))
     curves = '\n'.join([print_curve(curve) for curve in curves])
     this_script = sys.argv[0]
     today = datetime.date.today().strftime("%Y-%m-%d")
 
-    print(template_str % (this_script, today, curves, orders, names), end='')
+    print(template_str % (this_script, today, curves, orders, names.strip()), end='')
     return 0
 
 if __name__ == '__main__':
