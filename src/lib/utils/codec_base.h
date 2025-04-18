874720a56ede1670d4aa6ea30014d9503de50350
Chaosvex <ch@osvex.com>
2025-04-18 18:15:36 +0100

    Old Clang workaround

--- src/lib/utils/codec_base.h
+++ src/lib/utils/codec_base.h
@@ -35,8 +35,8 @@ size_t base_encode(
    Base&& base, char output[], const uint8_t input[], size_t input_length, size_t& input_consumed, bool final_inputs) {
    input_consumed = 0;
 
-   constexpr size_t encoding_bytes_in = base.encoding_bytes_in();
-   constexpr size_t encoding_bytes_out = base.encoding_bytes_out();
+   constexpr size_t encoding_bytes_in = Base::encoding_bytes_in();
+   constexpr size_t encoding_bytes_out = Base::encoding_bytes_out();
 
    size_t input_remaining = input_length;
    size_t output_produced = 0;
@@ -116,8 +116,8 @@ size_t base_decode(Base&& base,
                    size_t& input_consumed,
                    bool final_inputs,
                    bool ignore_ws = true) {
-   constexpr size_t decoding_bytes_in = base.decoding_bytes_in();
-   constexpr size_t decoding_bytes_out = base.decoding_bytes_out();
+   constexpr size_t decoding_bytes_in = Base::decoding_bytes_in();
+   constexpr size_t decoding_bytes_out = Base::decoding_bytes_out();
 
    uint8_t* out_ptr = output;
    std::array<uint8_t, decoding_bytes_in> decode_buf{};
