9c78206f18cad0e179124865e38a4645ed831895
Jack Lloyd <jack@randombit.net>
2025-04-27 20:06:36 +0000

    Add SHA-512 using AVX-512 for message expansion
    On a Tiger Lake processor, approximately 8% faster than the version using AVX2/BMI2

--- src/lib/hash/sha2_64/sha2_64.h
+++ src/lib/hash/sha2_64/sha2_64.h
@@ -90,6 +90,10 @@ class SHA_512 final : public HashFunction {
       static void compress_digest_x86_avx2(digest_type& digest, std::span<const uint8_t> input, size_t blocks);
 #endif
 
+#if defined(BOTAN_HAS_SHA2_64_X86_AVX512)
+      static void compress_digest_x86_avx512(digest_type& digest, std::span<const uint8_t> input, size_t blocks);
+#endif
+
 #if defined(BOTAN_HAS_SHA2_64_X86)
       static void compress_digest_x86(digest_type& digest, std::span<const uint8_t> input, size_t blocks);
 #endif
