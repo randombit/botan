75b4bddd0b94d289e7fb08d2b95c823b58868440
René Meusel <github@renemeusel.de>
2025-06-22 10:26:50 +0200

    Run strub tests according to target architecture

--- src/scripts/test_strubbed_symbols.py
+++ src/scripts/test_strubbed_symbols.py
@@ -11,17 +11,35 @@ import os
 import sys
 import argparse
 import tempfile
+import platform
 
 from textwrap import indent
+from enum import Enum
 
 SCRIPT_LOCATION = os.path.dirname(os.path.abspath(__file__))
 GDB_EXTENSION = os.path.join(SCRIPT_LOCATION, "gdb", "strubtest.py")
 
+class CPU(Enum):
+    ANY = "any"
+    X86_64 = "x86_64"
+    AARCH64 = "aarch64"
+
+    @staticmethod
+    def current():
+        cpu = platform.machine().lower()
+        if cpu in ['x86_64', 'amd64']:
+            return CPU.X86_64
+        elif cpu in ['aarch64', 'arm64']:
+            return CPU.AARCH64
+        else:
+            raise ValueError(f"Unsupported or unknown CPU architecture: {cpu}")
+
 class StrubTest:
-    def __init__(self, symbol, inferior_cmdline, masked_cpuid_bits = None, expect_fail = False):
+    def __init__(self, symbol, inferior_cmdline, masked_cpuid_bits = None, cpu = CPU.ANY, expect_fail = False):
         self.symbol = symbol
         self.inferior_cmdline = inferior_cmdline
         self.masked_cpuid_bits = masked_cpuid_bits
+        self.cpu = cpu
         self.expect_fail = expect_fail
 
     @property
@@ -46,8 +64,15 @@ class StrubTest:
         full_env.update(self.environment)
         return full_env
 
+    @property
+    def runnable(self):
+        return True if self.cpu == CPU.ANY else self.cpu == CPU.current()
+
     def run(self):
         print(f"Checking {self.symbol}... ", end="")
+        if not self.runnable:
+            return self._skip("incompatible platform")
+
         try:
             proc = subprocess.run(self.gdb_command, capture_output=True, check=False, env=self.rendered_environment)
         except subprocess.SubprocessError as ex:
@@ -98,6 +123,10 @@ class StrubTest:
             self._print_debug_report(proc_result=proc_result)
         return not self.expect_fail
 
+    def _skip(self, reason):
+        print(f"skipped ({reason})")
+        return True
+
 def dummy_file(size = 1024):
     with tempfile.NamedTemporaryFile(delete=False) as temp_file:
         temp_file.write(os.urandom(size))
@@ -116,15 +145,15 @@ def main():
         # information is naturally not annotated for stack scrubbing.
         StrubTest("Botan::version_string", [cli, "version", "--full"], expect_fail=True),
 
-        # Below is a list of all strub-annotated symbols. Note that we currently run
-        # this on x86_64 only. So platform specific symbols on other architectures are
-        # commented out for completeness and easier future extension.
+        # Below is a list of all strub-annotated symbols. Each of these
+        # symbols is tested by running the Botan CLI with GDB and checking
+        # that the stack scrubbing was performed correctly.
 
         StrubTest("Botan::SHA_256::compress_digest",          [cli, "hash", "--algo=SHA-256", myfile]),
-        StrubTest("Botan::SHA_256::compress_digest_x86",      [cli, "hash", "--algo=SHA-256", myfile]),
-        #StrubTest("Botan::SHA_256::compress_digest_armv8",   [cli, "hash", "--algo=SHA-256", myfile]),
-        StrubTest("Botan::SHA_256::compress_digest_x86_avx2", [cli, "hash", "--algo=SHA-256", myfile], masked_cpuid_bits=["intel_sha"]),
-        StrubTest("Botan::SHA_256::compress_digest_x86_simd", [cli, "hash", "--algo=SHA-256", myfile], masked_cpuid_bits=["intel_sha", "avx2"]),
+        StrubTest("Botan::SHA_256::compress_digest_x86",      [cli, "hash", "--algo=SHA-256", myfile], cpu=CPU.X86_64),
+        StrubTest("Botan::SHA_256::compress_digest_armv8",    [cli, "hash", "--algo=SHA-256", myfile], cpu=CPU.AARCH64),
+        StrubTest("Botan::SHA_256::compress_digest_x86_avx2", [cli, "hash", "--algo=SHA-256", myfile], cpu=CPU.X86_64, masked_cpuid_bits=["intel_sha"]),
+        StrubTest("Botan::SHA_256::compress_digest_x86_simd", [cli, "hash", "--algo=SHA-256", myfile], cpu=CPU.X86_64, masked_cpuid_bits=["intel_sha", "avx2"]),
     ]
 
     results = [test.run() for test in tests]
