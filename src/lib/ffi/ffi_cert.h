2df6d4a166f8248d1176bdf3b26aa0cd5a9e425a
arckoor <33837362+arckoor@users.noreply.github.com>
2025-07-16 23:30:20 +0200

    use certificate params builder

--- src/lib/ffi/ffi_cert.h
+++ src/lib/ffi/ffi_cert.h
@@ -15,7 +15,6 @@
    #include <botan/x509_ca.h>
    #include <botan/x509_crl.h>
    #include <botan/x509_ext.h>
-   #include <botan/x509cert.h>
    #include <botan/x509path.h>
    #include <botan/x509self.h>
 #endif
@@ -23,7 +22,7 @@
 extern "C" {
 #if defined(BOTAN_HAS_X509_CERTIFICATES)
 
-BOTAN_FFI_DECLARE_STRUCT(botan_x509_cert_params_builder_struct, Botan::X509_Cert_Options, 0x92597C7D);
+BOTAN_FFI_DECLARE_STRUCT(botan_x509_cert_params_builder_struct, Botan::CertificateParametersBuilder, 0x92597C7D);
 BOTAN_FFI_DECLARE_STRUCT(botan_x509_pkcs10_req_struct, Botan::PKCS10_Request, 0x87F0690A);
 BOTAN_FFI_DECLARE_STRUCT(botan_x509_cert_struct, Botan::X509_Certificate, 0x8F628937);
 BOTAN_FFI_DECLARE_STRUCT(botan_x509_crl_struct, Botan::X509_CRL, 0x2C628910);
