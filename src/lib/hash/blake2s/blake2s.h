def077b113d56d6ea60247a4a934e2c20d20fbea
Kağan Can Şit <kagancansit@hotmail.com>
2025-08-17 13:26:18 +0300

    Modernize blake2s performance (eliminating per-byte loops) and solve clang-tidy suppress (todo message etc.)
    Optimization for performance was performed by 'Co-authored-by: Richard Huveneers <https:://github.com/huven>'

--- src/lib/hash/blake2s/blake2s.h
+++ src/lib/hash/blake2s/blake2s.h
@@ -1,6 +1,7 @@
 /*
  * BLAKE2s
- * (C) 2023           Richard Huveneers
+ * (C) 2023, 2025       Richard Huveneers
+ * (C) 2025             Kagan Can Sit
  *
  * Botan is released under the Simplified BSD License (see license.txt)
  */
@@ -10,21 +11,28 @@
 
 #include <botan/hash.h>
 
+#include <array>
+
 namespace Botan {
 
 /**
  * BLAKE2s
  */
-class BLAKE2s final : public HashFunction /* NOLINT(*-special-member-functions) */ {
+class BLAKE2s final : public HashFunction {
    public:
-      explicit BLAKE2s(size_t output_bits = 256);
+      explicit BLAKE2s(std::size_t output_bits = 256);
       ~BLAKE2s() override;
+
+      BLAKE2s(const BLAKE2s&) = default;
+      BLAKE2s& operator=(const BLAKE2s&) = delete;
+      BLAKE2s(BLAKE2s&&) = delete;
+      BLAKE2s& operator=(BLAKE2s&&) = delete;
 
       std::string name() const override;
 
-      size_t output_length() const override { return m_outlen; }
+      std::size_t output_length() const override { return m_outlen; }
 
-      size_t hash_block_size() const override { return 64; }
+      std::size_t hash_block_size() const override { return block_size; }
 
       std::unique_ptr<HashFunction> copy_state() const override;
 
@@ -35,15 +43,16 @@ class BLAKE2s final : public HashFunction /* NOLINT(*-special-member-functions) 
    private:
       void add_data(std::span<const uint8_t> input) override;
       void final_result(std::span<uint8_t> output) override;
-      void state_init(size_t outlen, const uint8_t* key, size_t keylen);
-      void compress(bool last);
+      void state_init(std::size_t outlen, const uint8_t* key, std::size_t keylen);
+      void compress(bool last, std::span<const uint8_t> buf);
 
-      // TODO use secure_vector here
-      uint8_t m_b[64]{};    // input buffer
-      uint32_t m_h[8]{};    // chained state
-      uint32_t m_t[2]{};    // total number of bytes
-      uint8_t m_c = 0;      // pointer for b[]
-      size_t m_outlen = 0;  // digest size
+      std::array<uint8_t, 64> m_b{};  // input buffer
+      std::array<uint32_t, 8> m_h{};  // chained state
+      std::array<uint32_t, 2> m_t{};  // total number of bytes
+      uint8_t m_c = 0;                // pointer for b[]
+      std::size_t m_outlen = 0;       // digest size
+
+      static inline constexpr std::size_t block_size = 64;
 };
 
 }  // namespace Botan
