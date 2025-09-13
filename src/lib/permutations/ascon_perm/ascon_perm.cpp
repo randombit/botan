05dff3b60d23db5d4794052ff868e061183e025f
René Meusel <github@renemeusel.de>
2025-09-13 16:42:34 +0200

    Extend Ascon-p in preparation for Ascon-AEAD128
    ... particularly add percolate_in and percolate_out for data encryption
    and decryption. Also, introduce an intermediate_finish that is called when
    the associated data was fully absorbed into the sponge.

--- src/lib/permutations/ascon_perm/ascon_perm.cpp
+++ src/lib/permutations/ascon_perm/ascon_perm.cpp
@@ -24,7 +24,35 @@ void Ascon_p::squeeze(std::span<uint8_t> output) {
    squeeze_from_sponge(*this, output);
 }
 
-void Ascon_p::finish() {
+void Ascon_p::percolate_in(std::span<uint8_t> data) {
+   BufferSlicer input_slicer(data);
+   BufferStuffer output_stuffer(data);
+
+   process_bytes_in_sponge(*this, data.size(), [&](uint64_t state_word, auto bounds) {
+      state_word ^= bounds.read_from(input_slicer);
+      bounds.write_into(output_stuffer, state_word);
+      return state_word;
+   });
+
+   BOTAN_ASSERT_NOMSG(input_slicer.empty());
+   BOTAN_ASSERT_NOMSG(output_stuffer.full());
+}
+
+void Ascon_p::percolate_out(std::span<uint8_t> data) {
+   BufferSlicer input_slicer(data);
+   BufferStuffer output_stuffer(data);
+
+   process_bytes_in_sponge(*this, data.size(), [&](uint64_t state_word, auto bounds) {
+      const auto input_word = bounds.read_from(input_slicer);
+      bounds.write_into(output_stuffer, state_word ^ input_word);
+      return bounds.masked_assignment(state_word, input_word);
+   });
+
+   BOTAN_ASSERT_NOMSG(input_slicer.empty());
+   BOTAN_ASSERT_NOMSG(output_stuffer.full());
+}
+
+void Ascon_p::finish(uint8_t rounds) {
    // NIST SP.800-232, Section 2.1 (Algorithm 2 "pad()")
 
    // The padding is defined as:
@@ -36,7 +64,7 @@ void Ascon_p::finish() {
    // input block aligned with the byte rate, the final block may be just
    // padding bytes, otherwise the final block is padded as needed.
 
-   absorb(std::span{padding}.first(byte_rate() - cursor()), m_init_final_rounds);
+   absorb(std::span{padding}.first(byte_rate() - cursor()), rounds);
    BOTAN_ASSERT_NOMSG(cursor() == 0);
 }
 
