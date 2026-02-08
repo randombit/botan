5d542632df2312e70ffbac93c366269941a6b3f3
Jack Lloyd <jack@randombit.net>
2026-02-08 11:48:24 -0500

    fix

--- src/lib/pch/pch_lib.h
+++ src/lib/pch/pch_lib.h
@@ -37,7 +37,11 @@
 #include <variant>
 #include <vector>
 
-#if (defined(__x86_64__) || defined(_M_X64)) && __has_include(<immintrin.h>)
+/*
+Some cross compilers running on x86 will "have" immintrin.h and then error if
+you include it, so check for being plausibly actually x86 before including
+*/
+#if __has_include(<immintrin.h>) && (defined(__x86_64__) || defined(_M_X6))
    #include <immintrin.h>
 #endif
 
