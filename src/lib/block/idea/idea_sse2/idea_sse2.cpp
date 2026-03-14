ba1b59fbb6ac52d1666d0053187d9c50fc8be280
Jack Lloyd <jack@randombit.net>
2026-03-14 09:00:07 -0400

    Fix inefficiency in IDEA SSE2 transpose step

--- src/lib/block/idea/idea_sse2/idea_sse2.cpp
+++ src/lib/block/idea/idea_sse2/idea_sse2.cpp
@@ -53,37 +53,27 @@ BOTAN_FN_ISA_SSE2 inline __m128i mul(__m128i X, uint16_t K_16) {
 
 /*
 * 4x8 matrix transpose
-*
-* FIXME: why do I need the extra set of unpack_epi32 here? Inverse in
-* transpose_out doesn't need it. Something with the shuffle? Removing
-* that extra unpack could easily save 3-4 cycles per block, and would
-* also help a lot with register pressure on 32-bit x86
 */
 BOTAN_FN_ISA_SSE2 void transpose_in(__m128i& B0, __m128i& B1, __m128i& B2, __m128i& B3) {
-   __m128i T0 = _mm_unpackhi_epi32(B0, B1);
-   __m128i T1 = _mm_unpacklo_epi32(B0, B1);
-   __m128i T2 = _mm_unpackhi_epi32(B2, B3);
-   __m128i T3 = _mm_unpacklo_epi32(B2, B3);
+   B0 = _mm_shuffle_epi32(B0, _MM_SHUFFLE(3, 1, 2, 0));
+   B1 = _mm_shuffle_epi32(B1, _MM_SHUFFLE(3, 1, 2, 0));
+   B2 = _mm_shuffle_epi32(B2, _MM_SHUFFLE(3, 1, 2, 0));
+   B3 = _mm_shuffle_epi32(B3, _MM_SHUFFLE(3, 1, 2, 0));
 
-   const __m128i T4 = _mm_unpacklo_epi32(T0, T1);
-   const __m128i T5 = _mm_unpackhi_epi32(T0, T1);
-   const __m128i T6 = _mm_unpacklo_epi32(T2, T3);
-   const __m128i T7 = _mm_unpackhi_epi32(T2, T3);
+   B0 = _mm_shufflelo_epi16(B0, _MM_SHUFFLE(3, 1, 2, 0));
+   B1 = _mm_shufflelo_epi16(B1, _MM_SHUFFLE(3, 1, 2, 0));
+   B2 = _mm_shufflelo_epi16(B2, _MM_SHUFFLE(3, 1, 2, 0));
+   B3 = _mm_shufflelo_epi16(B3, _MM_SHUFFLE(3, 1, 2, 0));
 
-   T0 = _mm_shufflehi_epi16(T4, _MM_SHUFFLE(1, 3, 0, 2));
-   T1 = _mm_shufflehi_epi16(T5, _MM_SHUFFLE(1, 3, 0, 2));
-   T2 = _mm_shufflehi_epi16(T6, _MM_SHUFFLE(1, 3, 0, 2));
-   T3 = _mm_shufflehi_epi16(T7, _MM_SHUFFLE(1, 3, 0, 2));
+   B0 = _mm_shufflehi_epi16(B0, _MM_SHUFFLE(3, 1, 2, 0));
+   B1 = _mm_shufflehi_epi16(B1, _MM_SHUFFLE(3, 1, 2, 0));
+   B2 = _mm_shufflehi_epi16(B2, _MM_SHUFFLE(3, 1, 2, 0));
+   B3 = _mm_shufflehi_epi16(B3, _MM_SHUFFLE(3, 1, 2, 0));
 
-   T0 = _mm_shufflelo_epi16(T0, _MM_SHUFFLE(1, 3, 0, 2));
-   T1 = _mm_shufflelo_epi16(T1, _MM_SHUFFLE(1, 3, 0, 2));
-   T2 = _mm_shufflelo_epi16(T2, _MM_SHUFFLE(1, 3, 0, 2));
-   T3 = _mm_shufflelo_epi16(T3, _MM_SHUFFLE(1, 3, 0, 2));
-
-   T0 = _mm_shuffle_epi32(T0, _MM_SHUFFLE(3, 1, 2, 0));
-   T1 = _mm_shuffle_epi32(T1, _MM_SHUFFLE(3, 1, 2, 0));
-   T2 = _mm_shuffle_epi32(T2, _MM_SHUFFLE(3, 1, 2, 0));
-   T3 = _mm_shuffle_epi32(T3, _MM_SHUFFLE(3, 1, 2, 0));
+   const __m128i T0 = _mm_unpacklo_epi32(B0, B1);
+   const __m128i T1 = _mm_unpackhi_epi32(B0, B1);
+   const __m128i T2 = _mm_unpacklo_epi32(B2, B3);
+   const __m128i T3 = _mm_unpackhi_epi32(B2, B3);
 
    B0 = _mm_unpacklo_epi64(T0, T2);
    B1 = _mm_unpackhi_epi64(T0, T2);
