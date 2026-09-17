a326376f225cfcc310390a24a55904a917e93a8a
arckoor <33837362+arckoor@users.noreply.github.com>
2026-09-17 19:29:11 +0200

    Add certificate creation to FFI

--- src/lib/ffi/ffi_cert.h
+++ src/lib/ffi/ffi_cert.h
@@ -10,6 +10,8 @@
 #include <botan/internal/ffi_util.h>
 
 #if defined(BOTAN_HAS_X509_CERTIFICATES)
+   #include <botan/pkcs10.h>
+   #include <botan/x509_builder.h>
    #include <botan/x509_ca.h>
    #include <botan/x509_crl.h>
    #include <botan/x509cert.h>
@@ -23,6 +25,8 @@ BOTAN_FFI_DECLARE_STRUCT(botan_x509_cert_struct, Botan::X509_Certificate, 0x8F62
 BOTAN_FFI_DECLARE_STRUCT(botan_x509_crl_struct, Botan::X509_CRL, 0x2C628910);
 BOTAN_FFI_DECLARE_STRUCT(botan_x509_crl_entry_struct, Botan::CRL_Entry, 0x4EAA5346);
 BOTAN_FFI_DECLARE_STRUCT(botan_x509_general_name_struct, Botan::GeneralName, 0x563654FD);
+BOTAN_FFI_DECLARE_STRUCT(botan_x509_cert_builder_struct, Botan::CertificateParametersBuilder, 0x8A79DAB7);
+BOTAN_FFI_DECLARE_STRUCT(botan_x509_pkcs10_req_struct, Botan::PKCS10_Request, 0x94C112B6);
 
 #endif
 }
